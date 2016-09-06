0562af68b5a0b8351a3c92ec1223c793701cb0ea
Yuriy Yefremov <megadethov@gmail.com>
2016-09-06 08:25:02 +0300

    generic interface

---
+++ src/full_guide/part_01/chapter_14/_11_GenericInterface/MyClass.java
@@ -0,0 +1,32 @@
+package full_guide.part_01.chapter_14._11_GenericInterface;
+
+/**
+ * Created by mega on 06.09.2016.
+ */
+// Реализация интерфейса
+public class MyClass<T extends Comparable<T>> implements MinMax<T> {
+
+    T[] arrObj;
+
+    public MyClass (T[] arrObj) {
+        this.arrObj = arrObj;
+    }
+
+    @Override
+    public T min() {
+        T result = arrObj[0];
+        for (int i = 1; i < arrObj.length; i++) {
+            if (arrObj[i].compareTo(result) < 0) result = arrObj[i];
+        }
+        return result;
+    }
+
+    @Override
+    public T max() {
+       T result = arrObj[0];
+        for (int i = 0; i < arrObj.length; i++) {
+            if(arrObj[i].compareTo(result) > 0) result = arrObj[i];
+        }
+        return result;
+    }
+}
