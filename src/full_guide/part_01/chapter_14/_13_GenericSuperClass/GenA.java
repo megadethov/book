aae29fba44e0dabb9b47253506c820441a793af6
Yuriy Yefremov <megadethov@gmail.com>
2016-09-06 10:45:45 +0300

    generic super class

---
+++ src/full_guide/part_01/chapter_14/_13_GenericSuperClass/GenA.java
@@ -0,0 +1,24 @@
+package full_guide.part_01.chapter_14._13_GenericSuperClass;
+
+/**
+ * Created by mega on 06.09.2016.
+ */
+// Простая иерархия обобщенных классов
+public class GenA<T> {
+    T obj;
+
+    public GenA(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+}
+
+class GenB<T> extends GenA<T> {
+
+    public GenB(T obj) {
+        super(obj);
+    }
+}
