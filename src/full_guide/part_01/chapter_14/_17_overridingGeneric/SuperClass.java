9996ef1d9815b08b897d6be3adf958e90a9534d1
Yuriy Yefremov <megadethov@gmail.com>
2016-09-06 14:10:04 +0300

    overriding generic

---
+++ src/full_guide/part_01/chapter_14/_17_overridingGeneric/SuperClass.java
@@ -0,0 +1,40 @@
+package full_guide.part_01.chapter_14._17_overridingGeneric;
+
+import full_guide.part_01.chapter_09._02_Access.PackSub.SubC;
+
+/**
+ * Created by mega on 06.09.2016.
+ */
+public class SuperClass<T> {
+    T superObject;
+
+    public SuperClass(T superObject) {
+        this.superObject = superObject;
+    }
+
+    void print() {
+        System.out.println(superObject);
+    }
+}
+class SubClass<T> extends SuperClass {
+
+
+    public SubClass(T superObject) {
+        super(superObject);
+    }
+
+    @Override
+    void print() {
+        System.out.println(superObject);
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        SuperClass<String> superClass = new SuperClass<>("Super from super");
+        SubClass<String> subClass = new SubClass("Super from sub");
+
+        subClass.print();
+        superClass.print();
+    }
+}
