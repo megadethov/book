d518eef67d0788e269f029a4c58a755cc8b4cce9
Yuriy Yefremov <megadethov@gmail.com>
2016-09-09 11:15:21 +0300

    MyComparator

---
+++ src/full_guide/part_01/chapter_18_Collections/_17_MyComparator/Demo.java
@@ -0,0 +1,24 @@
+package full_guide.part_01.chapter_18_Collections._17_MyComparator;
+
+import java.util.TreeSet;
+
+/**
+ * Created by mega on 09.09.2016.
+ */
+public class Demo {
+    public static void main(String[] args) {
+//        TreeSet<String> ts = new TreeSet<>(new MyComparator()); // обычный компаратор
+        TreeSet<String> ts = new TreeSet<>(new MyComparator().reversed()); // естественный реверс JDK8
+        ts.add("F");
+        ts.add("C");
+        ts.add("G");
+        ts.add("D");
+        ts.add("H");
+        ts.add("E");
+        ts.add("B");
+        ts.add("A");
+        for (String s : ts) {
+            System.out.print(s + " ");
+        }
+    }
+}
---
+++ src/full_guide/part_01/chapter_18_Collections/_17_MyComparator/MyComparator.java
@@ -0,0 +1,19 @@
+package full_guide.part_01.chapter_18_Collections._17_MyComparator;
+
+import java.util.Comparator;
+
+/**
+ * Created by mega on 09.09.2016.
+ */
+// Comparator для реверсного порядка
+public class MyComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+
+        // для реверса порядка меняем местами объекты
+//        return o1.compareTo(o2); // для String compareTo() реализован
+        return o2.compareTo(o1); // для String compareTo() реализован
+    }
+    // переопределять equals() не требуется
+}
