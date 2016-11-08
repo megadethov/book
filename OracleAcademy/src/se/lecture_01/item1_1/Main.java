af7e086b7eccc4aad7d433cd4d86e23449292275
Yuriy Yefremov <megadethov@gmail.com>
2016-11-08 22:21:56 +0200

    OA project

---
+++ OracleAcademy/src/se/lecture_01/item1_1/Main.java
@@ -0,0 +1,21 @@
+package se.lecture_01.item1_1;
+
+/**
+ * Created by Yefremov Yuriy
+ */
+class Parent {
+    int p1;
+
+    public Parent() {
+        System.out.println("Construct Parent");
+        p1 = 1;
+    }
+    void Print() {
+        System.out.println(p1);
+    }
+}
+
+
+
+public class Main {
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# library
+# my training
