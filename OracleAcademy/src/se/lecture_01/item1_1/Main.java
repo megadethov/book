ad97ba1130e7b05f6205549d09f91356f7f387d8
Yuriy Yefremov <megadethov@gmail.com>
2016-11-09 09:31:12 +0200

    abstract, polymorphism

--- OracleAcademy/src/se/lecture_01/item1_1/Main.java
+++ OracleAcademy/src/se/lecture_01/item1_1/Main.java
@@ -3,19 +3,33 @@ package se.lecture_01.item1_1;
 /**
  * Created by Yefremov Yuriy
  */
-class Parent {
+//В этом примере показано, что абстрактный класс заставляет всех своих наследников реализовать все свои абстрактные методы
+//или тоже объявить себя абстрактными
+abstract class Parent {
     int p1;
-
     public Parent() {
         System.out.println("Construct Parent");
         p1 = 1;
     }
-    void Print() {
-        System.out.println(p1);
-    }
+    void Print() {System.out.println(p1);}
+    abstract void test(); // у абстрактного метода нет тела
 }
 
+class Child extends Parent {
+    public Child() {System.out.println("Construct Child");}
 
+    @Override
+    void test() {}
+
+//    abstract void method(); // нельзя сделать абстрактный метод в не абстрактном классе
+}
 
 public class Main {
+    public static void main(String[] args) {
+        Parent parent; // такая переменная может ссылаться как на Parent так и на всех его детей
+//        parent = new Parent(); // ошибка компиляции при попытке создать инстанс абстрактного класса
+        Parent child = new Child(); // сначала будет построен Parent, а затем Child.
+        child.Print(); // Child доступен метод Parent
+
+    }
 }
---
+++ OracleAcademy/src/se/lecture_01/item1_2/MyAbstractMethod.java
@@ -0,0 +1,37 @@
+package se.lecture_01.item1_2;
+
+/**
+ * Created by Yefremov Yuriy
+ */
+//Этот пример показывает полиморфизм в действии. Конкретный метод выбирается в рантайм.
+abstract class Parent {
+    void print() {
+        System.out.println("Parent print");
+    }
+}
+
+class LittleChild extends Parent {
+    @Override
+    void print() {
+        System.out.println("LittleChild print");
+    }
+}
+
+class BigChild extends Parent {
+    @Override
+    void print() {
+        System.out.println("BigChild print");
+    }
+}
+
+public class MyAbstractMethod {
+    public static void main(String[] args) {
+        // обе ссылки родительские, а инстансы конкретных детей
+        Parent littleChild = new LittleChild();
+        Parent bigChild = new BigChild();
+        littleChild.print(); // будет вызван переопределенный метод LittleChild
+        bigChild.print(); // будет вызван переопределенный метод BigChild
+
+
+    }
+}
