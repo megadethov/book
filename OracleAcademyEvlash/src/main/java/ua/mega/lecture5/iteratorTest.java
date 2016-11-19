2bf59ad1b4cd645d73fe3cafaac793db8cab5ba9
Yuriy Yefremov <megadethov@gmail.com>
2016-11-19 11:05:45 +0200

    Iterator

---
+++ OracleAcademyEvlash/src/main/java/ua/mega/lecture5/iteratorTest.java
@@ -0,0 +1,24 @@
+package ua.mega.lecture5;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Yefremov Yuriy
+ */
+public class iteratorTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Five");
+        list.add("Four");
+        list.add("Two");
+
+        Iterator<String> iter = list.iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+
+    }
+}
