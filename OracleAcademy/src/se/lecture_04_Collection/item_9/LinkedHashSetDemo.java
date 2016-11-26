fb1c1b494af16f2503d7377e048ba2ccde304c9f
Yuriy Yefremov <megadethov@gmail.com>
2016-11-26 17:28:18 +0200

    LinkedHashSet

---
+++ OracleAcademy/src/se/lecture_04_Collection/item_9/LinkedHashSetDemo.java
@@ -0,0 +1,21 @@
+package se.lecture_04_Collection.item_9;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetDemo {
+    public static void main(String[] args) {
+        // дубликатов нет, порядок вставки ПОДДЕРЖИВАЕТСЯ, доступа по индексу нет
+        Set<String> set = new LinkedHashSet<>();
+        set.add("BBB");
+        set.add("AAA");
+        set.add("BBB");
+        set.add("CCC");
+        set.add("BBB");
+        set.add("BBB");
+        set.add("AAA");
+        set.add("BBB");
+        System.out.println(set);
+    }
+}
