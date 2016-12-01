4a643b73fda00bfe8c2b3e92cd4a6844547db548
Yuriy Yefremov <megadethov@gmail.com>
2016-12-01 15:28:35 +0200

    MyConnection

---
+++ OracleAcademy/OracleAcademyKov/JSON/src/main/java/ua/mega/Lecture_10/MyConnection.java
@@ -0,0 +1,45 @@
+package ua.mega.Lecture_10;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+
+    private static String driverName = "com.mysql.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/test"; // test - имя базы
+    private static String user = "root";
+    private static String password = "toor";
+
+    // первый способ создать connection
+    public static Connection createConnection() {
+        Connection conn = null;
+
+        try {
+            Class.forName(driverName); // загружаем драйвер в ClassPath
+            // получаем от DriverManager - Connection
+            conn = DriverManager.getConnection(url, user, password); // можно также указать в конструкторе properties
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    // второй способ создать connection
+    public static Connection createConnectionWay2() {
+        Connection conn = null;
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+
+    public static void main(String[] args) {
+        Connection connection = createConnection();
+        System.out.println("Connection is created");
+    }
+}
