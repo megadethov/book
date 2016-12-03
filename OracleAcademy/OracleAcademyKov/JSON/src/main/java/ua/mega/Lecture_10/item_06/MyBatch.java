c3b564a13b9c99b2bb77a75c5b4dbdd1ab15ccf2
Yuriy Yefremov <megadethov@gmail.com>
2016-12-03 20:57:24 +0200

    Butch

---
+++ OracleAcademy/OracleAcademyKov/JSON/src/main/java/ua/mega/Lecture_10/item_06/MyBatch.java
@@ -0,0 +1,36 @@
+package ua.mega.Lecture_10.item_06;
+
+import ua.mega.Lecture_10.MyConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MyBatch {
+
+    public static final String INSERT_USER = "INSERT INTO user (name, age) VALUES (?, ?)";
+
+    public static void main(String[] args) throws SQLException {
+        batchUpdate(INSERT_USER);
+    }
+
+    private static void batchUpdate(String sql) throws SQLException {
+        Connection connection = MyConnection.createConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        connection.setAutoCommit(false); // отключаем авто-коммит
+
+        for (int i = 0; i < 5; i++) {
+            preparedStatement.setString(1, "name" + i);
+            preparedStatement.setInt(2, i);
+            preparedStatement.addBatch();
+        }
+
+        int[] counts = preparedStatement.executeBatch(); // возвращает кол-во измененных строк каждым запросом
+
+        connection.commit(); // теперь коммит
+
+        if (preparedStatement != null) preparedStatement.close();
+        if (connection != null) connection.close();
+    }
+}
