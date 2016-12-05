7c59dc623e7f23bdca7340e5e8aa34cc568c41f9
Yuriy Yefremov <megadethov@gmail.com>
2016-12-05 09:38:52 +0200

    Generics wildcards - extends

--- goit/GoJavaOnline/src/main/java/ua/mega/module_02_Generics/GenericsExtendsSuper.java
+++ goit/GoJavaOnline/src/main/java/ua/mega/module_02_Generics/GenericsExtendsSuper.java
@@ -12,11 +12,16 @@ public class GenericsExtendsSuper {
     @Test
     public void testPersonList() {
         List<Person> persons = Arrays.asList(new Person("Pasha", "Pavlov"), new Person("Lena", "Lenina"), new Person("Sasha", ""));
+        List<Citizen> citizens = Arrays.asList(new Citizen("Pasha", "Pavlov", "Lviv"), new Citizen("Lena", "Lenina", ""), new Citizen("Sasha", "", ""));
         System.out.println(isValidList(persons, new PersonValidator())); // false - Sasha не валидный
+//        System.out.println(isValidList(citizens, new PersonValidator())); // компилятор не запрещает - Citizen вместо Person
+        System.out.println(isValidList(citizens, new PersonValidator())); // компилятор не запрещает - citizens
 
     }
 
-    public boolean isValidList(List<Person> persons, Validator<Person> personValidator) {
+    // Метод - проверяет валидность List
+//    public boolean isValidList(List<Person> persons, Validator<Person> personValidator) { // компилятор не запрещает - Citizen вместо Person
+    public boolean isValidList(List<? extends Person> persons, Validator<Person> personValidator) { // так все ок
         for (Person person : persons) {
             if (!personValidator.isValid(person)) {
                 return false;
@@ -25,10 +30,12 @@ public class GenericsExtendsSuper {
         return true;
     }
 
+    // Интерфейс Validator
     public interface Validator<T> {
         boolean isValid(T value);
     }
 
+    // Класс реализующий валидацию
     public static class PersonValidator implements Validator<Person> {
 
         public boolean isValid(Person value) {
@@ -36,6 +43,7 @@ public class GenericsExtendsSuper {
         }
     }
 
+    // класс Person
     public static class Person {
         public String name;
         public String surname;
@@ -50,6 +58,25 @@ public class GenericsExtendsSuper {
             return "Person{" +
                     "name='" + name + '\'' +
                     ", surname='" + surname + '\'' +
+                    '}';
+        }
+    }
+
+    // класс - наследник Person
+    public static class Citizen extends Person {
+        public String address;
+
+        public Citizen(String name, String surname, String address) {
+            super(name, surname);
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return "Citizen{" +
+                    "name='" + name + '\'' +
+                    ", surname='" + surname + '\'' +
+                    ", address='" + address + '\'' +
                     '}';
         }
     }
