ee68903a6fd9c4e14c799f974037ced7e77722c7
Yuriy Yefremov <megadethov@gmail.com>
2016-12-05 07:50:04 +0200

    Generics

--- goit/GoJavaOnline/src/main/java/ua/mega/module_02_Generics/Generics.java
+++ goit/GoJavaOnline/src/main/java/ua/mega/module_02_Generics/Generics.java
@@ -9,10 +9,17 @@ public class Generics {
 
     @Test
    public void testGenericsTypeSafe() {
-       List<Integer> ints = Arrays.asList(10, 20, 3);
+       List<Integer> ints = Arrays.asList(10, 20, 3); // компилятор следит за типами
         for (Object anInt : ints) {
-            System.out.println(anInt);
+            System.out.println(anInt); // кастовать не нужно
         }
 
+    }
+
+    @Test
+    public void yestGenericSubtypes() {
+        List<Number> numbers;
+        List<Integer> integers = Arrays.asList(10, 20, 30);
+//        numbers = integers; // полиморфизм в Generics не работает
     }
 }
