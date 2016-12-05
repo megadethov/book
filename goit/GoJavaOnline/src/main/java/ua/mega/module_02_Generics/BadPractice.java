2c8d6650ddfa7661f41ba5e86050836270ffc785
Yuriy Yefremov <megadethov@gmail.com>
2016-12-05 11:27:28 +0200

    Generics bad practice

--- goit/GoJavaOnline/src/main/java/ua/mega/module_02_Generics/BadPractice.java
+++ goit/GoJavaOnline/src/main/java/ua/mega/module_02_Generics/BadPractice.java
@@ -1,9 +1,27 @@
 package ua.mega.module_02_Generics;
 
+import org.testng.annotations.Test;
+
 public class BadPractice {
+
+    @Test
+    public void testPairTriple() {
+        Triple<String, Pair<String, String>, Triple<String, Long, Long>> result = new Triple<String, Pair<String, String>, Triple<String, Long, Long>>();
+        result.first = "This is a bad idea";
+        result.second = new Pair<>();
+        result.second.first = "Nightmare";
+        result.second.second = "Looks awful";
+        result.third = new Triple<>();
+        result.third.first = "Ohh man!";
+        result.third.second = 10L;
+        result.third.third = 100L;
+
+        // a lot of code here
+    }
 
 
 // обертки, могут применяться, когда вам нужно вернуть из метода несколько значений
+    // названия типа first, second - антипаттерн
     public static class Pair<F, S> {
         public F first;
         public S second;
