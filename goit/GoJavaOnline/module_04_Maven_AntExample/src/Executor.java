b2c68eb55f0181abfadb663716d661661bd423f7
Yuriy Yefremov <megadethov@gmail.com>
2016-12-12 18:20:24 +0200

    interface Executor

---
+++ goit/GoJavaOnline/module_04_Maven_AntExample/src/Executor.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public interface Executor<T> {
+
+    void addTask(Task<? extends T> task);
+
+    void addTask(Task<? extends T> task, Validator<? super T> validator);
+
+    void execute();
+
+    List<T> getValidResults();
+
+    List<T> getInvalidResults();
+}
