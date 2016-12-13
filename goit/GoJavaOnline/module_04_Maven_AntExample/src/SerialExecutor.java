241e5ce125d59b60d3388792b979dfd73992ef6c
Yuriy Yefremov <megadethov@gmail.com>
2016-12-13 08:29:39 +0200

    testExecuteWithValidator

--- goit/GoJavaOnline/module_04_Maven_AntExample/src/SerialExecutor.java
+++ goit/GoJavaOnline/module_04_Maven_AntExample/src/SerialExecutor.java
@@ -1,27 +1,42 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SerialExecutor<T> implements Executor<T> {
 
-    private List<Task<? extends T>> tasks = new ArrayList<>();
+    // эта лямбда заменяет имплементацию метода интерфкйса Validator - метда isValid(), к-ый всегда возвращает - true
+    // избавляет от необходимости создавать класс, имплементирующий Validator
+    private static final Validator<Object> DEFAULT_VALIDATOR = value -> true; // перередаем в лямбду - value, возвращаем - true
+
+    private List<TaskAndValidator<T>> tasks = new ArrayList<>();
     private List<T> validResults = new ArrayList<>();
+    private List<T> invalidResults = new ArrayList<T>();
+
+    private Map<Task<? extends T>, Validator<? super T>> map = new HashMap<>();
 
 
     @Override
     public void addTask(Task<? extends T> task) {
-        tasks.add(task);
+        addTask(task, DEFAULT_VALIDATOR);
     }
 
     @Override
     public void addTask(Task<? extends T> task, Validator<? super T> validator) {
-
+        tasks.add(new TaskAndValidator<T>(task, validator));
+        map.put(task, validator);
     }
 
     @Override
     public void execute() {
-        for (Task<? extends T> task : tasks) {
+        for (TaskAndValidator<T> tTaskAndValidator : tasks) {
+            Task<? extends T> task = tTaskAndValidator.task;
             task.execute();
-            validResults.add(task.getResult());
+            if (tTaskAndValidator.validator.isValid(task.getResult())) {
+                validResults.add(task.getResult());
+            } else {
+                invalidResults.add(task.getResult());
+            }
         }
     }
 
@@ -32,6 +47,17 @@ public class SerialExecutor<T> implements Executor<T> {
 
     @Override
     public List<T> getInvalidResults() {
-        return null;
+        return invalidResults;
+    }
+
+    private static class TaskAndValidator<T> {
+
+        public TaskAndValidator(Task<? extends T> task, Validator<? super T> validator) {
+            this.task = task;
+            this.validator = validator;
+        }
+
+        private Task<? extends T> task;
+        private Validator<? super T> validator;
     }
 }
--- goit/GoJavaOnline/module_04_Maven_AntExample/test/SerialExecutorTest.java
+++ goit/GoJavaOnline/module_04_Maven_AntExample/test/SerialExecutorTest.java
@@ -9,12 +9,19 @@ public class SerialExecutorTest {
         SerialExecutor<Integer> executor = new SerialExecutor<Integer>();
         executor.addTask(new AddTask(1, 2));
         executor.execute();
+        assertEquals(executor.getValidResults().size(), 1, "Wrong valid results size");
+        assertEquals(executor.getInvalidResults().size(), 0, "Wrong invalid results size");
         assertEquals(executor.getValidResults().get(0), Integer.valueOf(3), "Wrong execution result");
     }
 
     @Test
     public void testExecuteWithValidator() throws Exception {
-
+        SerialExecutor<Integer> executor = new SerialExecutor<>();
+        executor.addTask(new AddTask(1, -2), result -> result >= 0);
+        executor.execute();
+        assertEquals(executor.getValidResults().size(), 0, "Wrong valid results size");
+        assertEquals(executor.getInvalidResults().size(), 1, "Wrong invalid results size");
+        assertEquals(executor.getInvalidResults().get(0), Integer.valueOf(-1), "Wrong execution result");
     }
 
     // по хорошему на Task нужно писать отдельный тест, но он очень простой и мы его лепим в этом тесте
