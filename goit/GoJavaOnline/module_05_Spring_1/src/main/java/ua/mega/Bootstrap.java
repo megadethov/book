2bc52e911f31d590a0259ef083a46171214595c6
Yuriy Yefremov <megadethov@gmail.com>
2016-12-14 20:26:57 +0200

    refactor AddTask

--- goit/GoJavaOnline/module_05_Spring_1/src/main/java/ua/mega/Bootstrap.java
+++ goit/GoJavaOnline/module_05_Spring_1/src/main/java/ua/mega/Bootstrap.java
@@ -26,25 +26,4 @@ public class Bootstrap {
         executor.getValidResults().forEach(System.out::println);
     }
 
-    private static class AddTask implements Task<Integer> {
-
-        private int value1;
-        private int value2;
-        private int result;
-
-        public AddTask(int value1, int value2) {
-            this.value1 = value1;
-            this.value2 = value2;
-        }
-
-        @Override
-        public void execute() {
-            result = value1 + value2;
-        }
-
-        @Override
-        public Integer getResult() {
-            return result;
-        }
-    }
 }
