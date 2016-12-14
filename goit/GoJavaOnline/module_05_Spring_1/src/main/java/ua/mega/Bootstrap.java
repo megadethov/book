4c808cbabc9ec5d1e323367a9b6d04fb65e7525d
Yuriy Yefremov <megadethov@gmail.com>
2016-12-14 18:19:50 +0200

    Spring_1

--- goit/GoJavaOnline/module_05_Spring_1/src/main/java/ua/mega/Bootstrap.java
+++ goit/GoJavaOnline/module_05_Spring_1/src/main/java/ua/mega/Bootstrap.java
@@ -1,7 +1,18 @@
 package ua.mega;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Bootstrap {
     public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml"); // создаем appContext
+        Bootstrap bootstrap = applicationContext.getBean("bootstrap", Bootstrap.class); // берем у контекста, созданный Контейнером инстанс
+
+        bootstrap.execute();
+    }
+
+    public void execute() {
         SerialExecutor<Integer> executor = new SerialExecutor<>();
         executor.addTask(new AddTask(1, -2));
         executor.addTask(new AddTask(1, 2), result -> result >= 0);
@@ -13,9 +24,8 @@ public class Bootstrap {
         executor.getInvalidResults().forEach(System.out::println);
         System.out.println("Invalid results:");
         executor.getValidResults().forEach(System.out::println);
-
-
     }
+
     private static class AddTask implements Task<Integer> {
 
         private int value1;
