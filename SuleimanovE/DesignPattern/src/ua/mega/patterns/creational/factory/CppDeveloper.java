43392a95f49e4ac9e0a8303cee728d0686c1a713
Yuriy Yefremov <megadethov@gmail.com>
2016-12-15 13:10:06 +0200

    patterns - factory - Developer impl

---
+++ SuleimanovE/DesignPattern/src/ua/mega/patterns/creational/factory/CppDeveloper.java
@@ -0,0 +1,8 @@
+package ua.mega.patterns.creational.factory;
+
+public class CppDeveloper implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("C++ Dev write code...");
+    }
+}
---
+++ SuleimanovE/DesignPattern/src/ua/mega/patterns/creational/factory/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package ua.mega.patterns.creational.factory;
+
+public class JavaDeveloper implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("Java Dev write code...");
+    }
+}
