19c77c5859d1a267502f33c8fca1b2e2c3a13d65
Yuriy Yefremov <megadethov@gmail.com>
2016-12-15 13:25:48 +0200

    patterns - factory - Program with polymorph

--- SuleimanovE/DesignPattern/src/ua/mega/patterns/creational/factory/Program.java
+++ SuleimanovE/DesignPattern/src/ua/mega/patterns/creational/factory/Program.java
@@ -2,6 +2,7 @@ package ua.mega.patterns.creational.factory;
 
 public class Program {
     public static void main(String[] args) {
-
+        Developer developer = new CppDeveloper();
+        developer.writeCode();
     }
 }
