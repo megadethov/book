52cd26415f836a7f25d50ada1f77a22c0a854a10
Yuriy Yefremov <megadethov@gmail.com>
2016-12-15 12:11:57 +0200

    patterns - factory - JavaDeveloper

--- SuleimanovE/DesignPattern/src/ua/mega/patterns/creational/factory/JavaDeveloper.java
+++ SuleimanovE/DesignPattern/src/ua/mega/patterns/creational/factory/JavaDeveloper.java
@@ -2,6 +2,6 @@ package ua.mega.patterns.creational.factory;
 
 public class JavaDeveloper {
     public void writeJavaCode() {
-
+        System.out.println("Java developer writes Java code...");
     }
 }
