b3d28f3775807bd4da200b18e2089b8997f48623
Yuriy Yefremov <megadethov@gmail.com>
2016-12-16 17:13:21 +0200

    Builder

--- itvdn/DesignPatterns/src/ua/mega/creational/Builder/Product.java
+++ itvdn/DesignPatterns/src/ua/mega/creational/Builder/Product.java
@@ -1,4 +1,18 @@
 package ua.mega.creational.Builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
+    List<String> parts = new ArrayList<>();
+
+    public boolean add(String part) {
+        return  parts.add(part);
+    }
+
+    public void show() {
+        for (String s : parts) {
+            System.out.println(s);
+        }
+    }
 }
