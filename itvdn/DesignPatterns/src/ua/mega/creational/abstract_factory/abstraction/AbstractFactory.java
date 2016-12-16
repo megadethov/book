249121ee055eefdbd380b0c651ba6ce08a2e3ca1
Yuriy Yefremov <megadethov@gmail.com>
2016-12-16 10:38:40 +0200

    AbstractFactory

---
+++ itvdn/DesignPatterns/src/ua/mega/creational/abstract_factory/abstraction/AbstractFactory.java
@@ -0,0 +1,5 @@
+package ua.mega.creational.abstract_factory.abstraction;
+
+public interface AbstractFactory {
+   AbstractBottle createBottle();
+}
---
+++ itvdn/DesignPatterns/src/ua/mega/creational/abstract_factory/abstraction/AbstractWater.java
@@ -0,0 +1,4 @@
+package ua.mega.creational.abstract_factory.abstraction;
+
+public interface AbstractWater {
+}
