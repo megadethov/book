dc37ecac057cc9886b3c5f1d666a8d8912a12667
Yuriy Yefremov <megadethov@gmail.com>
2016-12-17 08:42:20 +0200

    Prototype

---
+++ itvdn/DesignPatterns/src/ua/mega/creational/prototype/Prototype.java
@@ -0,0 +1,11 @@
+package ua.mega.creational.prototype;
+
+public abstract class Prototype {
+    private int id;
+
+    public Prototype(int id) {
+        this.id = id;
+    }
+
+    public abstract Prototype cloning();
+}
