fbbcfca131fb05cea029f8aef0ad2711055557d7
Yuriy Yefremov <megadethov@gmail.com>
2016-12-17 14:27:15 +0200

    bridge

---
+++ itvdn/DesignPatterns/src/ua/mega/structural/bridge/ConcreteImplementorA.java
@@ -0,0 +1,8 @@
+package ua.mega.structural.bridge;
+
+public class ConcreteImplementorA implements Implementor {
+    @Override
+    public void operationImp() {
+        System.out.println("ImplementorA");
+    }
+}
---
+++ itvdn/DesignPatterns/src/ua/mega/structural/bridge/ConcreteImplementorB.java
@@ -0,0 +1,8 @@
+package ua.mega.structural.bridge;
+
+public class ConcreteImplementorB implements Implementor {
+    @Override
+    public void operationImp() {
+        System.out.println("ImplementorB");
+    }
+}
