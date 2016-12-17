4f31fbc2b908c09de1865009053a58ecd66a3745
Yuriy Yefremov <megadethov@gmail.com>
2016-12-17 14:41:40 +0200

    bridge

---
+++ itvdn/DesignPatterns/src/ua/mega/structural/bridge/RefinedAbstraction.java
@@ -0,0 +1,12 @@
+package ua.mega.structural.bridge;
+
+public class RefinedAbstraction extends Abstraction  {
+    public RefinedAbstraction(Implementor implementor) {
+        super(implementor);
+    }
+
+    @Override
+    public void operation() {
+        super.operation();
+    }
+}
