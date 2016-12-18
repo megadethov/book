54d28f742dfc908dd1d94699d1dde57b061c58b3
Yuriy Yefremov <megadethov@gmail.com>
2016-12-18 21:36:15 +0200

    Proxy

---
+++ itvdn/DesignPatterns/src/ua/mega/structural/proxy/Proxy.java
@@ -0,0 +1,13 @@
+package ua.mega.structural.proxy;
+
+public class Proxy implements Subject {
+    RealSubject realSubject;
+
+    @Override
+    public void request() {
+        if (realSubject == null) {
+            realSubject = new RealSubject();
+        }
+        realSubject.request();
+    }
+}
