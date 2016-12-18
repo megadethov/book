ac05a8177560904e393d81caecc552b6ce10e559
Yuriy Yefremov <megadethov@gmail.com>
2016-12-18 11:18:16 +0200

    decorator

---
+++ itvdn/DesignPatterns/src/ua/mega/structural/decorator/ConcreteDecoratorA.java
@@ -0,0 +1,9 @@
+package ua.mega.structural.decorator;
+
+public class ConcreteDecoratorA implements Decorator {
+    private String addedState = "Some state";
+    @Override
+    public void operation() {
+
+    }
+}
--- itvdn/DesignPatterns/src/ua/mega/structural/decorator/Decorator.java
+++ itvdn/DesignPatterns/src/ua/mega/structural/decorator/Decorator.java
@@ -1,4 +1,12 @@
 package ua.mega.structural.decorator;
 
-public interface Decorator extends Component {
+public abstract class Decorator implements Component {
+    Component component; // компонент, к-ый необходимо декорировать
+
+    @Override
+    public void operation() {
+        if (component != null) { // проверка, что компонент для декора существует
+            component.operation(); // компонент для декора выполняет свою функциональность
+        }
+    }
 }
