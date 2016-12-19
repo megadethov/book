eee6d64718b0530826fd34dcdbb12cb3794cbb89
Yuriy Yefremov <megadethov@gmail.com>
2016-12-19 22:06:07 +0200

    covariant

--- mitropol/lecture_03/src/generics/MaxExample.java
+++ mitropol/lecture_03/src/generics/MaxExample.java
@@ -1,5 +1,7 @@
 package generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MaxExample {
@@ -12,5 +14,32 @@ public class MaxExample {
             }
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+
+//        List<String> list = new ArrayList<>(Arrays.asList("AA", "BB"));
+//        List<Object> list2 = list; // Incompatible types
+
+
+//        List<String> list = new ArrayList<>(Arrays.asList("AA", "BB"));
+//        List<CharSequence> list2 = list; // Incompatible types
+
+//----------КОВАРИАНТНОСТЬ--------------------------------------------
+//        List<String> list = new ArrayList<>(Arrays.asList("AA", "BB"));
+//        List<? extends CharSequence> list2 = list; // ковариантность ПРЕДОК - НАСЛЕДНИК
+//        for (CharSequence cs : list2 ) {
+//            System.out.println(cs);
+//        }
+//        list2.add("12"); // положить в эту коллекцию ничего нельзя, тк <? extends CharSequence> <какой-то extends CharSequence> а какой неизвестно
+
+//----------КОНТРВАРИАНТНОСТЬ--------------------------------------------
+        List<String> list = new ArrayList<>(Arrays.asList("AA", "BB"));
+        List<? super String> list2 = list; // контрвариантность НАСЛЕДНИК - ПРЕДОК
+        for (Object cs : list2 ) {
+            System.out.println(cs);
+        }
+        list2.add("СС"); // теперь в эту коллекцию можно добавлять <? super String> <какой-то super String> стринга и все ее предки
+
     }
 }
