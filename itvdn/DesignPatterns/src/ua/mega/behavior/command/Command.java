e4e93a483648e8158fc2ea1081fa321c27312bf2
Yuriy Yefremov <megadethov@gmail.com>
2016-12-19 11:41:18 +0200

    command

--- itvdn/DesignPatterns/src/ua/mega/behavior/command/Command.java
+++ itvdn/DesignPatterns/src/ua/mega/behavior/command/Command.java
@@ -3,6 +3,10 @@ package ua.mega.behavior.command;
 public abstract class Command {
     private Receiver receiver;
 
+    public Receiver getReceiver() {
+        return receiver;
+    }
+
     public Command(Receiver receiver) {
         this.receiver = receiver;
     }
---
+++ itvdn/DesignPatterns/src/ua/mega/behavior/command/ConcreteCommand.java
@@ -0,0 +1,13 @@
+package ua.mega.behavior.command;
+
+public class ConcreteCommand extends Command {
+
+    public ConcreteCommand(Receiver receiver) {
+        super(receiver);
+    }
+
+    @Override
+    public void execute() {
+        getReceiver().action();
+    }
+}
