4c4b44136c85f479d34ad297412675e876124311
Yuriy Yefremov <megadethov@gmail.com>
2016-12-19 13:30:59 +0200

    interpreter

---
+++ itvdn/DesignPatterns/src/ua/mega/behavior/interpreter/AbstractExpression.java
@@ -0,0 +1,5 @@
+package ua.mega.behavior.interpreter;
+
+public interface AbstractExpression {
+    void interpret(Context context);
+}
---
+++ itvdn/DesignPatterns/src/ua/mega/behavior/interpreter/Context.java
@@ -0,0 +1,4 @@
+package ua.mega.behavior.interpreter;
+
+public class Context {
+}
