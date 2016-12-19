cc92c71bb8b4b24ba0b6c490d151f090930e6d52
Yuriy Yefremov <megadethov@gmail.com>
2016-12-19 13:36:50 +0200

    interpreter

--- itvdn/DesignPatterns/src/ua/mega/behavior/interpreter/Context.java
+++ itvdn/DesignPatterns/src/ua/mega/behavior/interpreter/Context.java
@@ -1,4 +1,14 @@
 package ua.mega.behavior.interpreter;
 
 public class Context {
+
+// Контекст распознающей грамматики языка:
+// V = {a}; L = V*;
+// Правильные цепочки: а, аа, ааа, ...
+// Неправильные цепочки: b, ab, aba, ...
+
+    public String source;
+    public char vocabulary;
+    public boolean result;
+    public int position;
 }
