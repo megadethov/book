38171d8418e46edea93444c5e2b5549c2cce7fcb
Yuriy Yefremov <megadethov@gmail.com>
2016-12-20 20:31:07 +0200

    Monitor

--- mitropol/lecture_05/src/Monitors.java
+++ mitropol/lecture_05/src/Monitors.java
@@ -1,13 +1,50 @@
 public class Monitors {
     public static void main(String[] args) throws InterruptedException {
-        Object ob = new Object();
+        Resource resource = new Resource();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    resource.push(i);
+                    System.out.println("pushing " + i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t1.start();
 
-        synchronized (ob) {
-            ob.wait();
-        }
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Object res = resource.pool();
+                    System.out.println("pooling " + res);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t2.start();
+    }
+}
 
-        synchronized (ob) {
-            ob.notify();
+class Resource {
+    Object ob = null;
+
+    synchronized void push(Object ob) throws InterruptedException {
+        if (this.ob != null) {
+            wait();
         }
+        this.ob = ob;
+        notifyAll();
+    }
+
+    synchronized Object pool() throws InterruptedException {
+        if (this.ob == null) {
+            wait();
+        }
+        Object res = ob;
+        ob = null;
+        notifyAll();
+        return res;
     }
 }
