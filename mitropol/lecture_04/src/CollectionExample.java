f308c592bd910779e3ee763727d128bd7492538d
Yuriy Yefremov <megadethov@gmail.com>
2016-12-20 11:53:14 +0200

    stream

--- mitropol/lecture_04/src/CollectionExample.java
+++ mitropol/lecture_04/src/CollectionExample.java
@@ -9,7 +9,7 @@ public class CollectionExample {
         while (iter.hasNext()) {
             System.out.print(iter.next() + " ");
         }
-//        System.out.println();
+        System.out.println();
 
         Collection<Integer> unmod = Collections.unmodifiableCollection(collection);
 //        unmod.add(100); // java.lang.UnsupportedOperationException
@@ -26,6 +26,14 @@ public class CollectionExample {
 //        s2.filter((str) -> str.length() > 3).forEach((i) -> System.out.println("Hello " + i));
 //        s2.filter((str) -> str.length() > 3).map(str -> str.toUpperCase()).forEach((i) -> System.out.println("Hello " + i));
 //        s2.filter((str) -> str.length() > 3).map(str -> str.length()).forEach((i) -> System.out.println("Hello " + i));
-        Stream<Integer> is = s2.filter((str) -> str.length() > 3).map(str -> str.length());
+//        Stream<Integer> is = s2.filter((str) -> str.length() > 3).map(str -> str.length());
+
+//        Stream<Integer s = collection.stream();
+//        s.forEach((i) -> System.out.print(i));
+
+        Stream<Integer> s = Stream.iterate(1, i -> i + 1);
+//         s.forEach(System.out::println);
+//         s.filter(i -> i % 2 == 0).forEach(System.out::println);
+         s.filter(i -> i % 2 == 0).limit(10).forEach(System.out::println);
     }
 }
