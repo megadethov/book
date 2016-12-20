aeed97a6c0791781c83c7e42f2ddb9a3d26c1658
Yuriy Yefremov <megadethov@gmail.com>
2016-12-20 16:46:24 +0200

    Thread

--- mitropol/lecture_05/src/Multithreading2.java
+++ mitropol/lecture_05/src/Multithreading2.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Multithreading2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 5, 3, 4, 2));
         List<Integer> list2 = new ArrayList<>(Arrays.asList(10, 50, 30, 40, 20));
 
@@ -13,14 +13,19 @@ public class Multithreading2 {
         Thread t1 = new Thread(() -> {
            Integer max1 = Collections.max(list1, Integer::compare);
             result.add(max1);
+            System.out.println(max1);
         });
         Thread t2 = new Thread(() -> {
           Integer max2 = Collections.max(list2, Integer::compare);
             result.add(max2);
+            System.out.println(max2);
         });
 
         t1.start();
         t2.start();
+
+        t1.join();
+        t2.join();
 
         System.out.println(result);
 
