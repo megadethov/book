d50b78ec3eb3a9c1207a4f0fd3505c33c7c4d4ba
Yuriy Yefremov <megadethov@gmail.com>
2016-12-20 11:23:07 +0200

    stream

--- mitropol/lecture_04/src/CollectionExample.java
+++ mitropol/lecture_04/src/CollectionExample.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Stream;
 
 public class CollectionExample {
     public static void main(String[] args) {
@@ -8,8 +9,23 @@ public class CollectionExample {
         while (iter.hasNext()) {
             System.out.print(iter.next() + " ");
         }
+//        System.out.println();
 
-      Collection<Integer> unmod = Collections.unmodifiableCollection(collection);
-        unmod.add(100); // java.lang.UnsupportedOperationException
+        Collection<Integer> unmod = Collections.unmodifiableCollection(collection);
+//        unmod.add(100); // java.lang.UnsupportedOperationException
+
+//        Stream<Integer> stream = Stream.generate(() -> 100); // стрим содержащий бесконечное кол-во 100
+//        stream.forEach(i -> System.out.println(i)); // the same
+//        stream.forEach(System.out::println); // the same
+// -------------------------------------------------------------
+
+        Scanner scanner = new Scanner(System.in);
+        Stream<String> s2 = Stream.generate(() -> scanner.nextLine());
+
+//        s2.forEach((i) -> System.out.println("Hello " + i));
+//        s2.filter((str) -> str.length() > 3).forEach((i) -> System.out.println("Hello " + i));
+//        s2.filter((str) -> str.length() > 3).map(str -> str.toUpperCase()).forEach((i) -> System.out.println("Hello " + i));
+//        s2.filter((str) -> str.length() > 3).map(str -> str.length()).forEach((i) -> System.out.println("Hello " + i));
+        Stream<Integer> is = s2.filter((str) -> str.length() > 3).map(str -> str.length());
     }
 }
