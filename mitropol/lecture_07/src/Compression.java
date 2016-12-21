e0c9d7a1b363bcaa6284e12d9ec9489bb54747c0
Yuriy Yefremov <megadethov@gmail.com>
2016-12-21 12:32:03 +0200

    IO

--- mitropol/lecture_07/src/Compression.java
+++ mitropol/lecture_07/src/Compression.java
@@ -4,9 +4,12 @@ import java.util.zip.GZIPOutputStream;
 public class Compression {
     public static void main(String[] args) throws IOException {
 
-        FileOutputStream fos = new FileOutputStream("out.zip");
-        GZIPOutputStream gzos = new GZIPOutputStream(fos);
-        OutputStreamWriter osw = new OutputStreamWriter(gzos, "UTF-8");
-        PrintWriter pw = new PrintWriter(osw);
+       try( FileOutputStream fos = new FileOutputStream("out.zip");
+            GZIPOutputStream gzos = new GZIPOutputStream(fos);
+            OutputStreamWriter osw = new OutputStreamWriter(gzos, "UTF-8"); // позволяет задать кодировку, тк PrintWriter - в системной кодировке
+            PrintWriter pw = new PrintWriter(osw)) {
+
+           pw.println("Hello World");
+       }
     }
 }
