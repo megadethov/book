223409bd178947e231bd912383ff61521e0e1641
Yuriy Yefremov <megadethov@gmail.com>
2016-12-21 14:46:05 +0200

    IO Cipher

--- mitropol/lecture_07/src/CipherTwoKey.java
+++ mitropol/lecture_07/src/CipherTwoKey.java
@@ -4,18 +4,30 @@ import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 
-public class CipherOneKey {
+// Ассиметричное шифрование - 2 ключа. Еще называют - шифрование с открытым ключем
+// Public ключ распространяется в открытом виде - для шифрования
+// Private ключ есть только у вас для расшифровки
+public class CipherTwoKey {
     public static void main(String[] args) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
 
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        SecretKeySpec key = new SecretKeySpec("qhsjdjwieikdnghc".getBytes(), "AES"); // генерация ключа - из массива байтов
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        KeyPairGenerator keyPG = KeyPairGenerator.getInstance("RSA");
+        KeyPair pair = keyPG.genKeyPair();
+        PublicKey aPublic = pair.getPublic();
+        PrivateKey aPrivate = pair.getPrivate();
+
+
+
+
+
+
+//        SecretKeySpec key = new SecretKeySpec("qhsjdjwieikdnghc".getBytes(), "AES"); // это нам уже не нужно
 
 
 //        Пишем данные в зашифрованном виде в файл
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.init(Cipher.ENCRYPT_MODE, aPublic); // шифруем публичным ключем
         try (FileOutputStream fos = new FileOutputStream("cipher.txt");
              CipherOutputStream cis = new CipherOutputStream(fos, cipher);
              OutputStreamWriter osw = new OutputStreamWriter(cis);
@@ -24,7 +36,7 @@ public class CipherOneKey {
         }
 
         //        Считываем зашифрованные данные
-        cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(Cipher.DECRYPT_MODE, aPrivate); // расшифровываем приватным ключем
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new CipherInputStream(new FileInputStream("cipher.txt"), cipher)));
         System.out.println(bufferedReader.readLine());
     }
