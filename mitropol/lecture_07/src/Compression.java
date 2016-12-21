96286501d0078d3a3ecc8f8d77e911fd893e7e93
Yuriy Yefremov <megadethov@gmail.com>
2016-12-21 14:04:26 +0200

    IO Cipher

--- mitropol/lecture_07/src/Compression.java
+++ mitropol/lecture_07/src/Compression.java
@@ -1,15 +1,34 @@
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.zip.GZIPOutputStream;
 
 public class Compression {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
 
-       try( FileOutputStream fos = new FileOutputStream("out.zip");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        SecretKeySpec key = new SecretKeySpec("qhsjdjwieikdnghc".getBytes(), "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+//        CipherOutputStream cis = new CipherOutputStream(null, null);
+        try (FileOutputStream fos = new FileOutputStream("cipher.txt");
+             CipherOutputStream cis = new CipherOutputStream(fos, cipher);
+             OutputStreamWriter osw = new OutputStreamWriter(cis);
+             PrintWriter pw = new PrintWriter(osw))
+
+
+
+       /*try( FileOutputStream fos = new FileOutputStream("out.zip");
             GZIPOutputStream gzos = new GZIPOutputStream(fos);
             OutputStreamWriter osw = new OutputStreamWriter(gzos, "UTF-8"); // позволяет задать кодировку, тк PrintWriter - в системной кодировке
-            PrintWriter pw = new PrintWriter(osw)) {
+            PrintWriter pw = new PrintWriter(osw))*/ {
 
-           pw.println("Hello World");
-       }
+            pw.println("Hello World");
+        }
     }
 }
