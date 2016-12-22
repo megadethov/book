1b3beac04aa81183071c0675be6dc298c29e4e51
Yuriy Yefremov <megadethov@gmail.com>
2016-12-22 16:39:52 +0200

    NIO - FileChannel

--- mitropol/lecture_11/src/ua/mega/channel/FileChannelSimple.java
+++ mitropol/lecture_11/src/ua/mega/channel/FileChannelSimple.java
@@ -1,18 +1,32 @@
 package ua.mega.channel;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class FileChannelSimple {
     public static void main(String[] args) throws IOException {
-         FileChannel input = FileChannel.open(Paths.get("file.txt"), StandardOpenOption.READ);
+
+         FileChannel input = FileChannel.open(Paths.get("lecture_11/src/file.txt"), StandardOpenOption.READ);
 
         FileChannel output = FileChannel.open(Paths.get("file2.txt"),
                 StandardOpenOption.WRITE,
                 StandardOpenOption.CREATE,
                 StandardOpenOption.TRUNCATE_EXISTING);
 
+        ByteBuffer buffer = ByteBuffer.allocate(1024 * 16);
+        while (input.read(buffer) != -1) {
+            buffer.flip();
+            output.write(buffer);
+            buffer.compact();
+//            buffer.clear();
+
+
+            // TODO: AsynchronousFileChannel
+
+        }
     }
 }
