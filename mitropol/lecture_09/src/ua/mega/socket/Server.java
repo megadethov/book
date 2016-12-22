5d1659413820cbecf7c7fbeeffd5d14926cb7c18
Yuriy Yefremov <megadethov@gmail.com>
2016-12-22 09:12:38 +0200

    socket

--- mitropol/lecture_09/src/ua/mega/socket/Server.java
+++ mitropol/lecture_09/src/ua/mega/socket/Server.java
@@ -5,26 +5,33 @@ import ua.mega.serialization.Person;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 // сокет - это пара = домен+порт
 public class Server {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 
         // Серверный сокет с портом 3000 (висит на нем и слушает), находится на локальной машине
         ServerSocket serverSocket = new ServerSocket(3000);
+        ExecutorService tp = Executors.newFixedThreadPool(2);
 
+        Semaphore semaphore = new Semaphore(2); // тот же мьтекс, но для (2)
         while (true) {
+            semaphore.acquire();
             Socket accept = serverSocket.accept();
-           new Thread(() -> {
+//           new Thread(() -> {
+           tp.execute(() -> {
                try (Socket accept2 = accept) // программа залипает пока не получит accept Теперь после подключения с ним можно работать
                {
                    serve(accept);
-               } catch (ClassNotFoundException e) {
+               } catch (ClassNotFoundException | IOException e) {
                    e.printStackTrace();
-               } catch (IOException e) {
-                   e.printStackTrace();
+               } finally {
+                   semaphore.release(); // снимает блокировку
                }
-           }).start();
+           })/*.start()*/;
         }
 
     } // end main
---
+++ mitropol/lecture_09/src/ua/mega/socket/UdpServer.java
@@ -0,0 +1,4 @@
+package ua.mega.socket;
+
+public class UdpServer {
+}
