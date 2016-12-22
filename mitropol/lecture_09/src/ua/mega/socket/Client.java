e2e2c1ccb2a94ee5e29f0c36568851d66d1e38fc
Yuriy Yefremov <megadethov@gmail.com>
2016-12-22 08:50:16 +0200

    socket

--- mitropol/lecture_09/src/ua/mega/socket/Client.java
+++ mitropol/lecture_09/src/ua/mega/socket/Client.java
@@ -7,9 +7,18 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Socket socket = new Socket("localhost", 3000); // подключение к Серверу на сокет localhost:3000
+//        socket.setSoTimeout(10000); // время допустимого простоя до закрытия сокета
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream()); // будем гнать на сервер объект
+
         oos.writeObject(new Person("Вася", "Пупкин", 33));
+        Thread.sleep(10000);
+        oos.writeObject(new Person("Петя", "Ложкин", 42));
+        oos.writeObject(new Person("Вова", "Соскин", 15));
+
+        oos.close();
+        socket.close();
     }
+
 }
--- mitropol/lecture_09/src/ua/mega/socket/Server.java
+++ mitropol/lecture_09/src/ua/mega/socket/Server.java
@@ -1,33 +1,43 @@
 package ua.mega.socket;
 
+import ua.mega.serialization.Person;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+// сокет - это пара = домен+порт
 public class Server {
-    public static void main(String[] args) throws IOException {
-        // сокет - это пара = домен+порт
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         // Серверный сокет с портом 3000 (висит на нем и слушает), находится на локальной машине
         ServerSocket serverSocket = new ServerSocket(3000);
 
-        // клиентский сокет подкл. к сокету Сервера (у клиента тоже открывается свой порт)
-//        Socket socket = new Socket("localhost", 3000);
+        while (true) {
+            Socket accept = serverSocket.accept();
+           new Thread(() -> {
+               try (Socket accept2 = accept) // программа залипает пока не получит accept Теперь после подключения с ним можно работать
+               {
+                   serve(accept);
+               } catch (ClassNotFoundException e) {
+                   e.printStackTrace();
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }).start();
+        }
 
-        Socket accept = serverSocket.accept(); // программа залипает пока не получит accept
-        // Теперь после подключения с ним можно работать
+    } // end main
+
+    private static void serve(Socket accept) throws IOException, ClassNotFoundException {
         InputStream inputStream = accept.getInputStream(); // источник для чтения
         OutputStream outputStream = accept.getOutputStream(); // цель для записи
 
-//        ObjectInputStream ois = new ObjectInputStream(inputStream); // читаем из серверного сокета
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream)); // читаем из серверного сокета
-        for (int i = 0; i < 10; i++) {
-            String str = br.readLine();
-            System.out.println(str);
-        }
-        accept.close(); // закрытие сокета гарантированно закроет все IS-OS
-
-
+        ObjectInputStream ois = new ObjectInputStream(inputStream); // читаем из серверного сокета объекты
+        while (true) { // выход по исключению EOF
+//                Object readObj = ois.readObject();
+            Person readObj = (Person) ois.readObject();
+            System.out.println(readObj);
+        } // end while
     }
 }
