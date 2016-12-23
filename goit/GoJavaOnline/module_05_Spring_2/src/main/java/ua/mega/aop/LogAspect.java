ccd32fd758482740ba201e353084eb1b8e980bda
Yuriy Yefremov <megadethov@gmail.com>
2016-12-23 15:43:44 +0200

    Spring AOP - AspectJ

--- goit/GoJavaOnline/module_05_Spring_2/src/main/java/ua/mega/aop/LogAspect.java
+++ goit/GoJavaOnline/module_05_Spring_2/src/main/java/ua/mega/aop/LogAspect.java
@@ -6,9 +6,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
  * Created by mega on 23.12.2016.
  */
 public class LogAspect {
-    public void onExecute(ProceedingJoinPoint pgp) throws Throwable {
+    public Object onExecute(ProceedingJoinPoint pgp) throws Throwable {
         System.out.println("LogAspect Before execution of: " + pgp.getSignature().getName());
-        pgp.proceed();
+       Object result =  pgp.proceed();
         System.out.println("LogAspect After execution of: " + pgp.getSignature().getName());
+        return result;
     }
 }
