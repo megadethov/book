d43813ef9423be2a69f09a7658a53572523a277c
Yuriy Yefremov <megadethov@gmail.com>
2016-12-23 16:26:15 +0200

    Spring AOP - AspectJ annotation

--- goit/GoJavaOnline/module_05_Spring_2_2/src/main/java/ua/mega/aop/LogAspect2.java
+++ goit/GoJavaOnline/module_05_Spring_2_2/src/main/java/ua/mega/aop/LogAspect2.java
@@ -7,11 +7,20 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class LogAspect2 {
 
-    @Around("execution( * Executor.*(..))")
+   /* @Around("execution( * Executor.*(..))")
     public Object onExecute(ProceedingJoinPoint pgp) throws Throwable {
         System.out.println("LogAspect2 Before execution of: " + pgp.getSignature().getName());
        Object result =  pgp.proceed();
         System.out.println("LogAspect2 After execution of: " + pgp.getSignature().getName());
         return result;
+    }*/
+
+    @Around("execution( * Executor.addTask(Task)) && args(task))")
+    public void onExecute(Task task) throws Throwable {
+//        System.out.println("LogAspect2 Before execution of: " + pgp.getSignature().getName());
+//        Object result =  pgp.proceed();
+//        System.out.println("LogAspect2 After execution of: " + pgp.getSignature().getName());
+//        return result;
+        System.out.println(task.toString());
     }
 }
