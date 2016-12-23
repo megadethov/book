1c5b4b336420621f80a37a817f477bab6f7d748b
Yuriy Yefremov <megadethov@gmail.com>
2016-12-23 15:27:26 +0200

    Spring AOP - AspectJ

--- goit/GoJavaOnline/module_05_Spring_2/src/main/java/ua/mega/aop/Bootstrap.java
+++ goit/GoJavaOnline/module_05_Spring_2/src/main/java/ua/mega/aop/Bootstrap.java
@@ -10,7 +10,7 @@ public class Bootstrap {
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml"); // создаем appContext
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-context.xml", "aop-context.xml"); // создаем appContext
 
         Bootstrap bootstrap = applicationContext.getBean("bootstrap", Bootstrap.class); // не требует кастинга
 
--- goit/GoJavaOnline/module_05_Spring_2/src/main/java/ua/mega/aop/LogAspect.java
+++ goit/GoJavaOnline/module_05_Spring_2/src/main/java/ua/mega/aop/LogAspect.java
@@ -1,7 +1,14 @@
 package ua.mega.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * Created by mega on 23.12.2016.
  */
 public class LogAspect {
+    public void onExecute(ProceedingJoinPoint pgp) throws Throwable {
+        System.out.println("LogAspect Before execution of: " + pgp.getSignature().getName());
+        pgp.proceed();
+        System.out.println("LogAspect After execution of: " + pgp.getSignature().getName());
+    }
 }
