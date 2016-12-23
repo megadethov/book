e78c6f7923403910701105a934dcdbc14fb1ec93
Yuriy Yefremov <megadethov@gmail.com>
2016-12-23 17:47:21 +0200

    Spring AOP - AspectJ annotation

--- goit/GoJavaOnline/module_05_Spring_2_2/src/main/java/ua/mega/aop/LogAspect2.java
+++ goit/GoJavaOnline/module_05_Spring_2_2/src/main/java/ua/mega/aop/LogAspect2.java
@@ -1,8 +1,11 @@
 package ua.mega.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+
+import java.util.List;
 
 @Aspect
 public class LogAspect2 {
@@ -15,12 +18,18 @@ public class LogAspect2 {
         return result;
     }*/
 
-    @Around("execution( * Executor.addTask(Task)) && args(task))")
+   /* @Around("execution( * Executor.addTask(Task)) && args(task)")
     public void onExecute(Task task) throws Throwable {
-//        System.out.println("LogAspect2 Before execution of: " + pgp.getSignature().getName());
-//        Object result =  pgp.proceed();
-//        System.out.println("LogAspect2 After execution of: " + pgp.getSignature().getName());
-//        return result;
         System.out.println(task.toString());
+    }*/
+
+   /* @AfterReturning(pointcut = "execution( * Executor.getValidResults())", returning = "result")
+    public void onExecute(List result) throws Throwable {
+        System.out.println("AfterReturning result: " + result.toString());
+    }*/
+
+    @Around("execution( * Executor.addTask(Task)) && args(task)")
+    public void onExecute(ProceedingJoinPoint pjp, Task task) throws Throwable {
+        ((Executor<Integer>) pjp.getTarget()).addTask(task, (t) -> t > 0);
     }
 }
