bf051eb52c4139f56af0ee1cc495ed2df393d863
Yuriy Yefremov <megadethov@gmail.com>
2016-12-26 21:49:31 +0200

    JDBC connecting to DB

--- goit/GoJavaOnline/module_06_JDBC/src/main/java/Main.java
+++ goit/GoJavaOnline/module_06_JDBC/src/main/java/Main.java
@@ -1,21 +1,38 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+
         loadDriver();
+        LOGGER.info("Connecting to DB");
+
+
+        String url = "jdbc:postgresql://localhost:5432/company";
+        String user = "postgres";
+        String password = "postgres";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);) {
+            LOGGER.info("Successfully connected to DB");
+        } catch (SQLException e) {
+            LOGGER.error("Exception occurred while connecting to DB: " + url, e);
+        }
     }
 
     private static void loadDriver() {
         try {
             LOGGER.info("Loading JDBC driver: org.postgresql.Driver");
             Class.forName("org.postgresql.Driver");
-            LOGGER.info("Driver loaded successful");
+            LOGGER.info("Driver loaded successfully");
         } catch (ClassNotFoundException e) {
-           LOGGER.error("Can't find driver: org.postgresql.Driver");
+            LOGGER.error("Can't find driver: org.postgresql.Driver");
             throw new RuntimeException(e);
         }
     }
