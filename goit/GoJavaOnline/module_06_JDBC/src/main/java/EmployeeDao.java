1f39234b56c2d9c7784b8293c0683e58e2692d1b
Yuriy Yefremov <megadethov@gmail.com>
2016-12-27 10:00:36 +0200

    JDBC DAO

---
+++ goit/GoJavaOnline/module_06_JDBC/src/main/java/EmployeeDao.java
@@ -0,0 +1,55 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDao {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeDao.class);
+    private String url = "jdbc:postgresql://localhost:5432/company";
+    private String user = "postgres";
+    private String password = "postgres";
+
+    public EmployeeDao() {
+        loadDriver();
+    }
+
+    public List<Employee> getAll() {
+
+        List<Employee> employees = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement();) {
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee");
+            while (resultSet.next()) {
+                Employee employee = new Employee();
+                employee.setId(resultSet.getInt("id"));
+                employee.setName(resultSet.getString("name"));
+                employee.setAge(resultSet.getInt("age"));
+                employee.setAddress(resultSet.getString("address"));
+                employee.setSalary(resultSet.getFloat("salary"));
+                employee.setJoinDate(resultSet.getString("join_date"));
+                employees.add(employee);
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Exception occurred while connecting to DB: " + url, e);
+        }
+        return employees;
+    }
+
+
+    private void loadDriver() {
+        try {
+            LOGGER.info("Loading JDBC driver: org.postgresql.Driver");
+            Class.forName("org.postgresql.Driver");
+            LOGGER.info("Driver loaded successfully");
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("Can't find driver: org.postgresql.Driver");
+            throw new RuntimeException(e);
+        }
+    }
+}
+
--- goit/GoJavaOnline/module_06_JDBC/src/main/java/Main.java
+++ goit/GoJavaOnline/module_06_JDBC/src/main/java/Main.java
@@ -6,47 +6,10 @@ import java.time.LocalDateTime;
 
 public class Main {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
 
     public static void main(String[] args) {
 
-        loadDriver();
-        LOGGER.info("Connecting to DB");
 
-
-        String url = "jdbc:postgresql://localhost:5432/company";
-        String user = "postgres";
-        String password = "postgres";
-
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             Statement statement = connection.createStatement();) {
-            LOGGER.info("Successfully connected to DB");
-            String sql = "SELECT * FROM employee";
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                Employee employee = new Employee();
-                employee.setId(resultSet.getInt("id"));
-                employee.setName(resultSet.getString("name"));
-                employee.setAge(resultSet.getInt("age"));
-                employee.setAddress(resultSet.getString("address"));
-                employee.setSalary(resultSet.getFloat("salary"));
-                employee.setJoinDate(resultSet.getString("join_date"));
-                System.out.println(employee);
-            }
-        } catch (SQLException e) {
-            LOGGER.error("Exception occurred while connecting to DB: " + url, e);
-        }
     }
-
-    private static void loadDriver() {
-        try {
-            LOGGER.info("Loading JDBC driver: org.postgresql.Driver");
-            Class.forName("org.postgresql.Driver");
-            LOGGER.info("Driver loaded successfully");
-        } catch (ClassNotFoundException e) {
-            LOGGER.error("Can't find driver: org.postgresql.Driver");
-            throw new RuntimeException(e);
-        }
-    }
-
 }
