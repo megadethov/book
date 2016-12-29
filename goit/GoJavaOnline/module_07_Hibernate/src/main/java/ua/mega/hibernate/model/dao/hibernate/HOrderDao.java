286a655c8bdf282c1343465d2886feb0bcd883e5
Yuriy Yefremov <megadethov@gmail.com>
2016-12-29 16:54:30 +0200

    Hibernate

--- goit/GoJavaOnline/module_07_Hibernate/src/main/java/ua/mega/hibernate/model/dao/hibernate/HOrderDao.java
+++ goit/GoJavaOnline/module_07_Hibernate/src/main/java/ua/mega/hibernate/model/dao/hibernate/HOrderDao.java
@@ -1,11 +1,19 @@
 package ua.mega.hibernate.model.dao.hibernate;
 
+import org.hibernate.SessionFactory;
 import ua.mega.hibernate.model.Order;
 import ua.mega.hibernate.model.dao.OrderDao;
 
 public class HOrderDao implements OrderDao {
+
+    private SessionFactory sessionFactory;
+
     @Override
     public void save(Order order) {
+        sessionFactory.getCurrentSession().save(order);
+    }
 
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 }
