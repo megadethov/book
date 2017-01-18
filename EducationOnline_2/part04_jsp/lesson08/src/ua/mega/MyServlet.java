4aa8eb3c3fc199f777d79e23b7c378f7b578211e
Yuriy Yefremov <megadethov@gmail.com>
2017-01-18 11:08:52 +0200

    GET

---
+++ EducationOnline_2/part04_jsp/lesson08/src/ua/mega/MyServlet.java
@@ -0,0 +1,29 @@
+package ua.mega;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int a = Integer.parseInt(req.getParameter("a"));
+        int b = Integer.parseInt(req.getParameter("b"));
+        int res = a + b;
+
+        req.setAttribute("a", a);
+        req.setAttribute("b", b);
+        req.setAttribute("result", res);
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/result.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ EducationOnline_2/part04_jsp/lesson08/web/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index Page</title>
+</head>
+<body>
+<h1><a href="myServlet?a=10&b=20">a + b</a></h1>
+</body>
+</html>
