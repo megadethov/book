004db4e06d4b00d1be3277ec3898d05d5664dbea
Yuriy Yefremov <megadethov@gmail.com>
2017-01-19 10:20:08 +0200

    Authorization - AuthFilter

--- EducationOnline_2/part04_jsp/lesson12/src/ua/mega/AuthFilter.java
+++ EducationOnline_2/part04_jsp/lesson12/src/ua/mega/AuthFilter.java
@@ -3,6 +3,9 @@ package ua.mega;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,7 +22,24 @@ public class AuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        StringUtils.substringAfterLast("", "/");
+        String uri = ((HttpServletRequest) servletRequest).getRequestURI();
+        String path = StringUtils.substringAfterLast(uri, "/");
+
+        if (!pathFilters.contains(path)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        HttpSession session = ((HttpServletRequest) servletRequest).getSession();
+        User user = (User) session.getAttribute("PRINCIPAL");
+
+        if (user != null) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        ((HttpServletResponse) servletResponse).sendRedirect("login.jsp?login=invalid");
+
     }
 
     @Override
