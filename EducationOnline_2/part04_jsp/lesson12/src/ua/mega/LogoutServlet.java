79a59d35b0dd828fa435ae9eace80a73322078bb
Yuriy Yefremov <megadethov@gmail.com>
2017-01-19 11:24:46 +0200

    Authorization - LogoutServlet

---
+++ EducationOnline_2/part04_jsp/lesson12/src/ua/mega/LogoutServlet.java
@@ -0,0 +1,17 @@
+package ua.mega;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/logout.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
