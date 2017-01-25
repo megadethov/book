e4e2d268b8d9eb82342c9afdfa9d3c30d26200c5
Yuriy Yefremov <megadethov@gmail.com>
2017-01-25 14:33:46 +0200

    A Simple Servlet That Generates Plain Text

---
+++ tutorials/coreservlets/servlet_jsp/csajsp1/ex2_ASimpleServletThatGeneratesPlainText/src/ua/mega/servlet/HelloWorld.java
@@ -0,0 +1,19 @@
+package ua.mega.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by mega
+ */
+public class HelloWorld extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("<h1>Hello World !!!</h1>");
+    }
+}
