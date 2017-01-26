7aca09ac912b3a92e17790909628970993a5c6de
Yuriy Yefremov <megadethov@gmail.com>
2017-01-26 12:39:11 +0200

    Reading Three Request Parameters

---
+++ tutorials/coreservlets/servlet_jsp/csajsp1/ex6_ReadingFormDataInServlets/src/ua/mega/servlet/ThreeParams.java
@@ -0,0 +1,31 @@
+package ua.mega.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by mega
+ */
+public class ThreeParams extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        String title = "Reading Three Request Parameters";
+        writer.println(ServletUtilities.headWithTitle(title) +
+                "<h1 align=center>" + title + "</h1>\n" +
+                "<ul>\n" +
+                " <li><b>param1</b>: "
+                + req.getParameter("param1") + "\n" +
+                " <li><b>param2</b>: "
+                + req.getParameter("param2") + "\n" +
+                " <li><b>param3</b>: "
+                + req.getParameter("param3") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
+    }
+}
