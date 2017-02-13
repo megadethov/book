e6b3f4854a8f7b84c8a078cc6239547d54dc6d81
Yuriy Yefremov <megadethov@gmail.com>
2017-02-13 21:07:56 +0200

    SQL - CREATE Database

---
+++ tutorials/SQL/tutorialspoint/ex12_CREATE_Database/src/CREATE_Database.sql
@@ -0,0 +1,13 @@
+/*
+Basic syntax of CREATE DATABASE statement is as follows:
+CREATE DATABASE DatabaseName;
+*/
+
+[root@host]# mysql -u root -p
+Enter password:*******
+
+
+CREATE DATABASE testDB;
+
+SHOW DATABASES;
+
