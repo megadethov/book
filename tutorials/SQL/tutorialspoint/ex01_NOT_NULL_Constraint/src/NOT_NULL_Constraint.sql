78f6c66efe1f0ee70a579458d4116174b3a0241e
Yuriy Yefremov <megadethov@gmail.com>
2017-02-13 11:54:47 +0200

    SQL - NOT NULL Constraint

---
+++ tutorials/SQL/tutorialspoint/ex01_NOT_NULL_Constraint/src/NOT_NULL_Constraint.sql
@@ -0,0 +1,13 @@
+CREATE TABLE CUSTOMERS(
+  ID INT NOT NULL,
+  NAME VARCHAR(20) NOT NULL,
+  AGE INT NOT NULL,
+  ADDRESS CHAR(25),
+  SALARY DECIMAL (18, 2),
+  PRIMARY KEY (ID)
+);
+
+/*If CUSTOMERS table has already been created, then to add a NOT NULL constraint to SALARY column in Oracle and MySQL, you would write a statement similar to the following:
+ALTER TABLE CUSTOMERS
+  MODIFY SALARY  DECIMAL (18, 2) NOT NULL;*/
+
