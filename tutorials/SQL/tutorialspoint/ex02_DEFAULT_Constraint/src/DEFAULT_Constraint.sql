34efd1ed934e75db881be678a869efff0347976d
Yuriy Yefremov <megadethov@gmail.com>
2017-02-13 12:58:55 +0200

    SQL - DEFAULT Constraint

---
+++ tutorials/SQL/tutorialspoint/ex02_DEFAULT_Constraint/src/DEFAULT_Constraint.sql
@@ -0,0 +1,13 @@
+CREATE TABLE CUSTOMERS(
+  ID INT NOT NULL,
+  NAME VARCHAR(20) NOT NULL,
+  AGE INT NOT NULL,
+  ADDRESS CHAR (25),
+  SALARY DECIMAL (18, 2) DEFAULT 5000.00,
+  PRIMARY KEY (ID)
+);
+
+/*If CUSTOMERS table has already been created, then to add a DEFAULT constraint to SALARY column, you would write a statement similar to the following:
+ALTER TABLE CUSTOMERS
+  MODIFY SALARY  DECIMAL (18, 2) DEFAULT 5000.00;*/
+
