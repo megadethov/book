9dcbd30ada273322a4d5511564282453c2e6bcce
Yuriy Yefremov <megadethov@gmail.com>
2017-02-13 21:32:18 +0200

    SQL - SELECT Database, USE Statement

---
+++ tutorials/SQL/tutorialspoint/ex14_SELECT_Database_USE_Statement/src/SELECT_Database_USE_Statement.sql
@@ -0,0 +1,11 @@
+/*
+When you have multiple databases in your SQL Schema, then before starting your operation,
+you would need to select a database where all the operations would be performed.
+The SQL USE statement is used to select any existing database in SQL schema.
+Syntax:
+Basic syntax of USE statement is as follows:
+*/
+
+USE DatabaseName;
+
+
