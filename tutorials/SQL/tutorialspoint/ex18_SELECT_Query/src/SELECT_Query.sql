116cdf11f29d6535d0fd8f45bd9c6facb3d26f57
Yuriy Yefremov <megadethov@gmail.com>
2017-02-14 10:50:40 +0200

    SQL - SELECT Query

---
+++ tutorials/SQL/tutorialspoint/ex18_SELECT_Query/src/SELECT_Query.sql
@@ -0,0 +1,15 @@
+/*
+The basic syntax of SELECT statement is as follows:
+
+SELECT column1, column2, columnN FROM table_name;
+
+If you want to fetch all the fields available in the field, then you can
+use the following syntax:
+
+SELECT * FROM table_name;
+
+*/
+
+SELECT ID, NAME, SALARY FROM CUSTOMERS;
+
+SELECT * FROM CUSTOMERS;
