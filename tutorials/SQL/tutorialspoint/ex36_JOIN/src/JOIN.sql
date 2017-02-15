a5e0a57128ebb1513f958fe86480eb099ed95e9b
Yuriy Yefremov <megadethov@gmail.com>
2017-02-15 14:55:47 +0200

    SQL - Using Joins

---
+++ tutorials/SQL/tutorialspoint/ex36_JOIN/src/JOIN.sql
@@ -0,0 +1,30 @@
+/*
+The SQL Joins clause is used to combine records from two or more tables in a database. A JOIN is a means
+for combining fields from two tables by using values common to each.
+ */
+/*
+(a) CUSTOMERS table is as follows:
++----+----------+-----+-----------+----------+
+| ID | NAME     | AGE | ADDRESS   | SALARY   |
++----+----------+-----+-----------+----------+
+|  1 | Ramesh   |  32 | Ahmedabad |  2000.00 |
+|  2 | Khilan   |  25 | Delhi     |  1500.00 |
+|  3 | kaushik  |  23 | Kota      |  2000.00 |
+|  4 | Chaitali |  25 | Mumbai    |  6500.00 |
+|  5 | Hardik   |  27 | Bhopal    |  8500.00 |
+|  6 | Komal    |  22 | MP        |  4500.00 |
+|  7 | Muffy    |  24 | Indore    | 10000.00 |
++----+----------+-----+-----------+----------+
+(b) Another table is ORDERS as follows:
++-----+---------------------+-------------+--------+
+|OID  | DATE                | CUSTOMER_ID | AMOUNT |
++-----+---------------------+-------------+--------+
+| 102 | 2009-10-08 00:00:00 |           3 |   3000 |
+| 100 | 2009-10-08 00:00:00 |           3 |   1500 |
+| 101 | 2009-11-20 00:00:00 |           2 |   1560 |
+| 103 | 2008-05-20 00:00:00 |           4 |   2060 |
++-----+---------------------+-------------+--------+
+*/
+SELECT ID, NAME, AGE, AMOUNT
+FROM CUSTOMERS, ORDERS
+WHERE CUSTOMERS.ID = ORDERS.CUSTOMER_ID;
