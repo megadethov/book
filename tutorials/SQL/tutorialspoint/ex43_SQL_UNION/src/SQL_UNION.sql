a82f8b8687f53b237e175a27684a568a95cd7148
Yuriy Yefremov <megadethov@gmail.com>
2017-02-16 10:01:51 +0200

    SQL UNION

---
+++ tutorials/SQL/tutorialspoint/ex43_SQL_UNION/src/SQL_UNION.sql
@@ -0,0 +1,109 @@
+/*
+The SQL UNION clause/operator is used to combine the results of two or more SELECT statements without returning any duplicate rows.
+To use UNION, each SELECT must have the same number of columns selected, the same number of column expressions, the same data type, and have them in the same order, but they do not have to be the same length.
+The basic syntax of UNION is as follows:
+
+SELECT column1 [, column2 ]
+FROM table1 [, table2 ]
+[WHERE condition]
+
+UNION
+
+SELECT column1 [, column2 ]
+FROM table1 [, table2 ]
+[WHERE condition]
+
+(a) CUSTOMERS table is as follows:
++----+----------+-----+-----------+----------+
+| ID | NAME     | AGE | ADDRESS   | SALARY   |
++----+----------+-----+-----------+----------+
+|  1 | Ramesh   |  32 | Ahmedabad |  2000.00 |
+|  2 | Khilan   |  25 | Delhi     |  1500.00 |
+|  3 | kaushik  |  23 | Kota      |  2000.00 |
+|  4 | Chaitali |  25 | Mumbai    |  6500.00 |
+|  5 | Hardik   |  27 | Bhopal    |  8500.00 |
+|  6 | Komal    |  22 | MP        |  4500.00 |
+|  7 | Muffy    |  24 | Indore    | 10000.00 |
++----+----------+-----+-----------+----------+
+
+
+(b) Another table is ORDERS as follows:
++-----+---------------------+-------------+--------+
+|OID  | DATE                | CUSTOMER_ID | AMOUNT |
++-----+---------------------+-------------+--------+
+| 102 | 2009-10-08 00:00:00 |           3 |   3000 |
+| 100 | 2009-10-08 00:00:00 |           3 |   1500 |
+| 101 | 2009-11-20 00:00:00 |           2 |   1560 |
+| 103 | 2008-05-20 00:00:00 |           4 |   2060 |
++-----+---------------------+-------------+--------+
+
+Now, let us join these two tables in our SELECT statement as follows:
+ */
+SELECT id, name, amount, date
+FROM customers
+LEFT JOIN orders
+  ON customers.id = orders.customer_id
+UNION
+SELECT id, name, amount, date
+FROM customers
+  RIGHT JOIN orders
+    ON customers.id = orders.customer_id;
+/*
+This would produce the following result:
++------+----------+--------+---------------------+
+| ID   | NAME     | AMOUNT | DATE                |
++------+----------+--------+---------------------+
+|    1 | Ramesh   |   NULL | NULL                |
+|    2 | Khilan   |   1560 | 2009-11-20 00:00:00 |
+|    3 | kaushik  |   3000 | 2009-10-08 00:00:00 |
+|    3 | kaushik  |   1500 | 2009-10-08 00:00:00 |
+|    4 | Chaitali |   2060 | 2008-05-20 00:00:00 |
+|    5 | Hardik   |   NULL | NULL                |
+|    6 | Komal    |   NULL | NULL                |
+|    7 | Muffy    |   NULL | NULL                |
++------+----------+--------+---------------------+
+
+The UNION ALL operator is used to combine the results of two SELECT statements including duplicate rows.
+The same rules that apply to UNION apply to the UNION ALL operator.
+The basic syntax of UNION ALL is as follows:
+
+SELECT column1 [, column2 ]
+FROM table1 [, table2 ]
+[WHERE condition]
+
+UNION ALL
+
+SELECT column1 [, column2 ]
+FROM table1 [, table2 ]
+[WHERE condition]
+ */
+
+SELECT  ID, NAME, AMOUNT, DATE
+FROM CUSTOMERS
+  LEFT JOIN ORDERS
+    ON CUSTOMERS.ID = ORDERS.CUSTOMER_ID
+UNION ALL
+SELECT  ID, NAME, AMOUNT, DATE
+FROM CUSTOMERS
+  RIGHT JOIN ORDERS
+    ON CUSTOMERS.ID = ORDERS.CUSTOMER_ID;
+
+/*
+This would produce the following result:
++------+----------+--------+---------------------+
+| ID   | NAME     | AMOUNT | DATE                |
++------+----------+--------+---------------------+
+|    1 | Ramesh   |   NULL | NULL                |
+|    2 | Khilan   |   1560 | 2009-11-20 00:00:00 |
+|    3 | kaushik  |   3000 | 2009-10-08 00:00:00 |
+|    3 | kaushik  |   1500 | 2009-10-08 00:00:00 |
+|    4 | Chaitali |   2060 | 2008-05-20 00:00:00 |
+|    5 | Hardik   |   NULL | NULL                |
+|    6 | Komal    |   NULL | NULL                |
+|    7 | Muffy    |   NULL | NULL                |
+|    3 | kaushik  |   3000 | 2009-10-08 00:00:00 |
+|    3 | kaushik  |   1500 | 2009-10-08 00:00:00 |
+|    2 | Khilan   |   1560 | 2009-11-20 00:00:00 |
+|    4 | Chaitali |   2060 | 2008-05-20 00:00:00 |
++------+----------+--------+---------------------+
+ */
