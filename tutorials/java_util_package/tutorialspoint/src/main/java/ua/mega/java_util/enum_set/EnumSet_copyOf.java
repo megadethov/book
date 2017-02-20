557489c600b259d7dd8616dd88c8f15aed350599
Yuriy Yefremov <megadethov@gmail.com>
2017-02-20 11:02:26 +0200

    Java.util.EnumSet.copyOf() Method

---
+++ tutorials/java_util_package/tutorialspoint/src/main/java/ua/mega/java_util/enum_set/EnumSet_copyOf.java
@@ -0,0 +1,37 @@
+package ua.mega.java_util.enum_set;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EnumSet;
+
+/**
+ * The java.util.EnumSet.copyOf(Collection<E> c) method creates an enum set
+ * initialized from the specified collection.
+ */
+public class EnumSet_copyOf {
+
+    // create an enum
+    public enum Numbers {
+        ONE, TWO, THREE, FOUR, FIVE
+    }
+
+    public static void main(String[] args) {
+
+        // create a new collection
+        Collection<Numbers> collection = new ArrayList<>();
+
+        // print the collection
+        System.out.println("Collection: " + collection);
+
+        // add two elements in the collection
+        collection.add(Numbers.ONE);
+        collection.add(Numbers.THREE);
+
+        // create an EnumSet that is a copy of the collection
+        EnumSet<Numbers> set = EnumSet.copyOf(collection);
+
+        // print the set
+        System.out.println("Set: " + set);
+    }
+
+}
