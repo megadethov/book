83252179eedad87b4ffb2f85c3d0daad5d1339e6
Yuriy Yefremov <megadethov@gmail.com>
2017-02-26 21:20:07 +0200

    JDBC - Navigating a Result Set Example

---
+++ tutorials/JDBC/tutorialspoint/ex05_navigating-a-result-set/src/main/java/ua/mega/navigating_a_result_set/JDBCExample.java
@@ -0,0 +1,106 @@
+package ua.mega.navigating_a_result_set;
+
+//STEP 1. Import required packages
+
+import java.sql.*;
+
+public class JDBCExample {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/emp";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query to create statment with
+            // required arguments for RS example.
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String sql = "SELECT id, first, last, age FROM employees";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // Move cursor to the last row
+            System.out.println("Moving cursor to the last...");
+            rs.last();
+
+            //STEP 5: Extract data from result set
+            System.out.println("Displaying record...");
+            int id = rs.getInt("id");
+            int age = rs.getInt("age");
+            String first = rs.getString("first");
+            String last = rs.getString("last");
+
+            //Display values
+            System.out.print("ID: " + id);
+            System.out.print(", Age: " + age);
+            System.out.print(", First: " + first);
+            System.out.println(", Last: " + last);
+
+            // Move cursor to the first row.
+            System.out.println("Moving cursor to the first row...");
+            rs.first();
+
+            //STEP 6: Extract data from result set
+            System.out.println("Displaying record...");
+            id  = rs.getInt("id");
+            age = rs.getInt("age");
+            first = rs.getString("first");
+            last = rs.getString("last");
+
+            //Display values
+            System.out.print("ID: " + id);
+            System.out.print(", Age: " + age);
+            System.out.print(", First: " + first);
+            System.out.println(", Last: " + last);
+
+            System.out.println("Moving cursor to the next row...");
+            rs.next();
+
+            //STEP 7: Extract data from result set
+            System.out.println("Displaying record...");
+            id  = rs.getInt("id");
+            age = rs.getInt("age");
+            first = rs.getString("first");
+            last = rs.getString("last");
+
+            //Display values
+            System.out.print("ID: " + id);
+            System.out.print(", Age: " + age);
+            System.out.print(", First: " + first);
+            System.out.println(", Last: " + last);
+
+            //STEP 8: Clean-up environment
+        } catch (ClassNotFoundException e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } catch (SQLException e) {
+            //Handle errors for JDBC
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+    }
+}
