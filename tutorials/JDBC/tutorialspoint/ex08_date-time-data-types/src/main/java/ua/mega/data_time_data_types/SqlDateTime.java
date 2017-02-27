6b5e4e2a28c6947daccfcdab58b707b30543feb9
Yuriy Yefremov <megadethov@gmail.com>
2017-02-27 14:14:14 +0200

    JDBC - Date & Time Data Types

---
+++ tutorials/JDBC/tutorialspoint/ex08_date-time-data-types/src/main/java/ua/mega/data_time_data_types/SqlDateTime.java
@@ -0,0 +1,22 @@
+package ua.mega.data_time_data_types;
+
+public class SqlDateTime {
+    public static void main(String[] args) {
+        //Get standard date and time
+        java.util.Date javaDate = new java.util.Date();
+        long javaTime = javaDate.getTime();
+        System.out.println("The Java Date is: " + javaDate);
+
+        //Get and display SQL DATE
+        java.sql.Date sqlDate = new java.sql.Date(javaTime);
+        System.out.println(("The SQL DATE is: " + sqlDate));
+
+        //Get and display SQL TIME
+        java.sql.Time sqlTime = new java.sql.Time(javaTime);
+        System.out.println("The SQL TIME is: " + sqlTime);
+
+        //Get and display SQL TIMESTAMP
+        java.sql.Timestamp sqlTimestapm = new java.sql.Timestamp(javaTime);
+        System.out.println("The SQL TIMESTAMP is: " + sqlTimestapm);
+    }
+}
