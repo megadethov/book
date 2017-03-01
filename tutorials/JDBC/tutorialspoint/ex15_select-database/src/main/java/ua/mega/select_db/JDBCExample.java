1ab70d2200c1c16bd9005e9af992b5a21ddafd61
Yuriy Yefremov <megadethov@gmail.com>
2017-03-01 11:26:13 +0200

    JDBC - Select Database Example

---
+++ tutorials/JDBC/tutorialspoint/ex15_select-database/src/main/java/ua/mega/select_db/JDBCExample.java
@@ -0,0 +1,43 @@
+package ua.mega.select_db;
+
+//STEP 1. Import required packages
+
+import java.sql.*;
+
+public class JDBCExample {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+
+    }
+}
