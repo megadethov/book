e271d82e212aad6f6a459d56924aa5aaa8e478d6
Yuriy Yefremov <megadethov@gmail.com>
2017-03-01 12:15:00 +0200

    JDBC - Create Tables Example

---
+++ tutorials/JDBC/tutorialspoint/ex17_create-tables/src/main/java/ua/mega/create_table/JDBCExample.java
@@ -0,0 +1,61 @@
+package ua.mega.create_table;
+
+//STEP 1. Import required packages
+
+import java.sql.*;
+
+public class JDBCExample {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            stmt = conn.createStatement();
+            String sql = "CREATE TABLE registration " +
+                    "(id INTEGER NOT NULL, " +
+                    "first VARCHAR(255), " +
+                    "last VARCHAR(255), " +
+                    "age INTEGER, " +
+                    "PRIMARY KEY(id))";
+            stmt.executeUpdate(sql);
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }
+
+    }
+
+}
