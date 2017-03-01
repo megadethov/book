ded8dd7828517a641c013fc4347135dd059b8ded
Yuriy Yefremov <megadethov@gmail.com>
2017-03-01 11:44:04 +0200

    JDBC - Drop Database Example

---
+++ tutorials/JDBC/tutorialspoint/ex16_drop-database/src/main/java/ua/mega/drop_db/JDBCExample.java
@@ -0,0 +1,56 @@
+package ua.mega.drop_db;
+
+//STEP 1. Import required packages
+
+import java.sql.*;
+
+public class JDBCExample {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            stmt = conn.createStatement();
+            String sql = "DROP DATABASE students";
+            stmt.executeUpdate(sql);
+            System.out.println("Database deleted successfully...");
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+
+    }
+
+}
