69044554dfbb175222238c58a2ad41fc7c9b0914
Yuriy Yefremov <megadethov@gmail.com>
2017-03-01 11:15:02 +0200

    JDBC - Create Database Example

---
+++ tutorials/JDBC/tutorialspoint/ex14_create-database/src/main/java/ua/mega/create_database/JDBCExample.java
@@ -0,0 +1,55 @@
+package ua.mega.create_database;
+
+//STEP 1. Import required packages
+
+import java.sql.*;
+
+public class JDBCExample {
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            stmt = conn.createStatement();
+            String sql = "CREATE DATABASE students";
+            stmt.executeUpdate(sql);
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+
+    }
+
+}
