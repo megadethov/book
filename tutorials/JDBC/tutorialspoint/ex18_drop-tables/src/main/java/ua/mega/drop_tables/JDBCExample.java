a4d600fe3edeb93359e4adfe3658c97f123e7ea6
Yuriy Yefremov <megadethov@gmail.com>
2017-03-01 15:19:16 +0200

    JDBC - Drop Tables Example

---
+++ tutorials/JDBC/tutorialspoint/ex18_drop-tables/src/main/java/ua/mega/drop_tables/JDBCExample.java
@@ -0,0 +1,54 @@
+package ua.mega.drop_tables;
+
+//STEP 1. Import required packages
+
+import java.sql.*;
+
+public class JDBCExample {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            stmt = conn.createStatement();
+            String sql = "DROP TABLE registration";
+            stmt.executeUpdate(sql);
+
+            System.out.println("Table  deleted in given database...");
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+    }
+
+}
