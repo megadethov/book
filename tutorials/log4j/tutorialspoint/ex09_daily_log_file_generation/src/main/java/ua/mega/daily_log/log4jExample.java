b30874a541d7741409b1fc8465c62edf10691d5b
Yuriy Yefremov <megadethov@gmail.com>
2017-03-04 20:14:47 +0200

    ex09_daily_log_file_generation

---
+++ tutorials/log4j/tutorialspoint/ex09_daily_log_file_generation/src/main/java/ua/mega/daily_log/log4jExample.java
@@ -0,0 +1,13 @@
+package ua.mega.daily_log;
+
+import org.apache.log4j.Logger;
+
+public class log4jExample {
+    /* Get actual class name to be printed on */
+    static Logger log = Logger.getLogger(log4jExample.class);
+
+    public static void main(String[] args) {
+        log.debug("Hello this is a debug message");
+        log.info("Hello this is an info message");
+    }
+}
