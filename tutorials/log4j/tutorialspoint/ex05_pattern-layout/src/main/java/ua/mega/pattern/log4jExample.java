0526a25098a4c7312c83a3208d9db8b29145e254
Yuriy Yefremov <megadethov@gmail.com>
2017-03-04 12:42:47 +0200

    log4j - PatternLayout

---
+++ tutorials/log4j/tutorialspoint/ex05_pattern-layout/src/main/java/ua/mega/pattern/log4jExample.java
@@ -0,0 +1,13 @@
+package ua.mega.pattern;
+
+import org.apache.log4j.Logger;
+
+public class log4jExample {
+    /* Get actual class name to be printed on */
+    static Logger log = Logger.getLogger(log4jExample.class.getName());
+
+    public static void main(String[] args) {
+        log.debug("Hello this is an debug message");
+        log.info("Hello this is an info message");
+    }
+}
