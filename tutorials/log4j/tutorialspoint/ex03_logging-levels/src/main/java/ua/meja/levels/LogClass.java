60e0a7a019233f8213e1ceee271ed96eac48b757
Yuriy Yefremov <megadethov@gmail.com>
2017-03-04 10:45:51 +0200

    log4j - Logging Levels

---
+++ tutorials/log4j/tutorialspoint/ex03_logging-levels/src/main/java/ua/meja/levels/LogClass.java
@@ -0,0 +1,20 @@
+package ua.meja.levels;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class LogClass {
+    private static org.apache.log4j.Logger log = Logger.getLogger(LogClass.class);
+
+    public static void main(String[] args) {
+        log.setLevel(Level.WARN);
+
+        log.trace("Trace Message!");
+        log.debug("Debug Message!");
+        log.info("Info Message!");
+        log.warn("Warn Message!");
+        log.error("Error Message!");
+        log.fatal("Fatal Message!");
+
+    }
+}
