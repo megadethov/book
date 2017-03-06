da5df3166ef0367408e2702547b249284b9d89c6
Yuriy Yefremov <megadethov@gmail.com>
2017-03-06 14:58:33 +0200

    Spring ApplicationContext Container

---
+++ tutorials/Spring/tutorialspoint/ex03_application-context-container/src/main/java/ua/mega/MainApp.java
@@ -0,0 +1,15 @@
+package ua.mega;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new FileSystemXmlApplicationContext
+                ("d:\\java\\book\\tutorials\\Spring\\tutorialspoint\\ex03_application-context-container\\src\\main\\resources\\Beans.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
