f4222f1600e61983eb01b4ed0c946e127318e8ab
Yuriy Yefremov <megadethov@gmail.com>
2017-03-06 11:05:56 +0200

    Spring BeanFactory Container

---
+++ tutorials/Spring/tutorialspoint/ex02_bean-factory-container/src/main/java/ua/mega/MainApp.java
@@ -0,0 +1,13 @@
+package ua.mega;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class MainApp {
+    public static void main(String[] args) {
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
+
+        HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");
+        obj.getMessage();
+    }
+}
