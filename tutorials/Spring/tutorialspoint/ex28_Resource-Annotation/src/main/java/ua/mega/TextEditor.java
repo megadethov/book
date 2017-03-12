e0b8bf7294fe7fd2a95fc441b2fea60741eb7aee
Yuriy Yefremov <megadethov@gmail.com>
2017-03-12 19:26:55 +0200

    @Resource Annotation

---
+++ tutorials/Spring/tutorialspoint/ex28_Resource-Annotation/src/main/java/ua/mega/TextEditor.java
@@ -0,0 +1,22 @@
+package ua.mega;
+
+import javax.annotation.Resource;
+
+public class TextEditor {
+    private SpellChecker spellChecker;
+
+//    @Resource(name = "spellChecker")
+    @Resource
+    public void setSpellChecker(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
+    }
+
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+
+    public void spellCheck() {
+        spellChecker.checkSpelling();
+    }
+
+}
