6e9b59add6945c03ae2b309db6e359277435357b
Yuriy Yefremov <megadethov@gmail.com>
2017-03-12 13:05:44 +0200

    @Autowired on Constructors

---
+++ tutorials/Spring/tutorialspoint/ex24_autowired-annotation-on-constructors/src/main/java/ua/mega/TextEditor.java
@@ -0,0 +1,18 @@
+package ua.mega;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TextEditor {
+
+    private SpellChecker spellChecker;
+
+    @Autowired
+    public TextEditor(SpellChecker spellChecker){
+        System.out.println("Inside TextEditor constructor." );
+        this.spellChecker = spellChecker;
+    }
+
+    public void spellCheck(){
+        spellChecker.checkSpelling();
+    }
+}
