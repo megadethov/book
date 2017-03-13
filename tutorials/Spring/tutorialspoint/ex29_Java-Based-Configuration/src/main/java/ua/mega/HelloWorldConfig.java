045e6bc3be10e171e2d8d870d6b4963ff84de67c
Yuriy Yefremov <megadethov@gmail.com>
2017-03-13 16:07:45 +0200

    Injecting Bean Dependencies

---
+++ tutorials/Spring/tutorialspoint/ex29_Java-Based-Configuration/src/main/java/ua/mega/HelloWorldConfig.java
@@ -0,0 +1,13 @@
+package ua.mega;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfig {
+
+    @Bean
+    public HelloWorld helloWorld() {
+        return new HelloWorld();
+    }
+}
