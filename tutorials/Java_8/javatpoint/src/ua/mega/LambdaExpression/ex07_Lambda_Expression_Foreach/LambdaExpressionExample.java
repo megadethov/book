d8073216a603a34a22444d4601667b5407f40f40
Yuriy Yefremov <megadethov@gmail.com>
2017-03-17 09:18:52 +0200

    Java Lambda Expression Example: Foreach Loop

---
+++ tutorials/Java_8/javatpoint/src/ua/mega/LambdaExpression/ex07_Lambda_Expression_Foreach/LambdaExpressionExample.java
@@ -0,0 +1,20 @@
+package ua.mega.LambdaExpression.ex07_Lambda_Expression_Foreach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaExpressionExample {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("ankit");
+        list.add("mayank");
+        list.add("irfan");
+        list.add("jai");
+
+        list.forEach(
+                n -> System.out.println(n)
+        );
+
+    }
+}
