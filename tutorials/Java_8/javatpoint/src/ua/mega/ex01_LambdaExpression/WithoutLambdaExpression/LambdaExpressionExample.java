66785c1da1a121b1887a07e9eb3548b7f6adb1b0
Yuriy Yefremov <megadethov@gmail.com>
2017-03-17 08:03:22 +0200

    Java Example without Lambda Expression

---
+++ tutorials/Java_8/javatpoint/src/ua/mega/ex01_LambdaExpression/WithoutLambdaExpression/LambdaExpressionExample.java
@@ -0,0 +1,20 @@
+package ua.mega.ex01_LambdaExpression.WithoutLambdaExpression;
+
+interface Drawable {
+    public void draw();
+}
+
+public class LambdaExpressionExample {
+    public static void main(String[] args) {
+        int width = 10;
+
+        //without lambda, Drawable implementation using anonymous class
+        Drawable d = new Drawable() {
+            @Override
+            public void draw() {
+                System.out.println("Drawing " + width);
+            }
+        };
+        d.draw();
+    }
+}
