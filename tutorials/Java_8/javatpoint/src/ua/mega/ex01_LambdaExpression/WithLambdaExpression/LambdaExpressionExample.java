6c7ae5aa5b72f93f72915a51da9418578a415935
Yuriy Yefremov <megadethov@gmail.com>
2017-03-17 08:12:18 +0200

    Java Example with Lambda Expression

---
+++ tutorials/Java_8/javatpoint/src/ua/mega/ex01_LambdaExpression/WithLambdaExpression/LambdaExpressionExample.java
@@ -0,0 +1,18 @@
+package ua.mega.ex01_LambdaExpression.WithLambdaExpression;
+
+@FunctionalInterface // It is optional
+interface Drawable{
+    public void draw();
+}
+
+public class LambdaExpressionExample {
+    public static void main(String[] args) {
+        int width = 10;
+
+        // with lambda
+        Drawable d2 = () -> {
+            System.out.println("Drawing " + width);
+        };
+        d2.draw();
+    }
+}
