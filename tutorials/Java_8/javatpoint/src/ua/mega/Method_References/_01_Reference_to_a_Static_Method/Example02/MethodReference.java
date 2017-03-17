1e464917fa7683958fe8016812a460bc23622853
Yuriy Yefremov <megadethov@gmail.com>
2017-03-17 21:14:53 +0200

    predefined functional interface Runnable to refer static method.

--- tutorials/Java_8/javatpoint/src/ua/mega/Method_References/_01_Reference_to_a_Static_Method/Example02/MethodReference.java
+++ tutorials/Java_8/javatpoint/src/ua/mega/Method_References/_01_Reference_to_a_Static_Method/Example02/MethodReference.java
@@ -6,8 +6,22 @@ package ua.mega.Method_References._01_Reference_to_a_Static_Method.Example02;
 public class MethodReference {
     public static void main(String[] args) {
 
-        Thread t2 = new Thread(MethodReference::ThreadStatus);
+        // var-1
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                MethodReference.ThreadStatus();
+            }
+        });
+        t1.start();
+
+        // var-2
+        Thread t2 = new Thread(() -> MethodReference.ThreadStatus());
         t2.start();
+
+        // var-3
+        Thread t3 = new Thread(MethodReference::ThreadStatus);
+        t3.start();
     }
 
     public static void ThreadStatus() {
