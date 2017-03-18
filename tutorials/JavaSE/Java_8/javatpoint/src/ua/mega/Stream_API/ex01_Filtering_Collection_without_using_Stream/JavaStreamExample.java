937687a16251ac8addb0ef0a87e1952395749fa9
Yuriy Yefremov <megadethov@gmail.com>
2017-03-18 18:37:53 +0200

    Filtering Collection without using Stream

---
+++ tutorials/JavaSE/Java_8/javatpoint/src/ua/mega/Stream_API/ex01_Filtering_Collection_without_using_Stream/JavaStreamExample.java
@@ -0,0 +1,38 @@
+package ua.mega.Stream_API.ex01_Filtering_Collection_without_using_Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaStreamExample {
+    public static void main(String[] args) {
+        List<Product> productsList = new ArrayList<>();
+        //Adding Products
+        productsList.add(new Product(1,"HP Laptop",25000f));
+        productsList.add(new Product(2,"Dell Laptop",30000f));
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));
+        productsList.add(new Product(4,"Sony Laptop",28000f));
+        productsList.add(new Product(5,"Apple Laptop",90000f));
+
+        List<Float> productPriceList = new ArrayList<Float>();
+
+        for (Product product : productsList) {
+            // filtering data of list
+            if (product.price < 30000) {
+                productPriceList.add(product.price); // adding price to a productPriceList
+            }
+        }
+        System.out.println(productPriceList); // displaying data
+    }
+}
+
+class Product {
+    int id;
+    String name;
+    float price;
+
+    public Product(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
