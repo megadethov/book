82e5c43de43a607d816da2d05885eb212a1f77d8
Yuriy Yefremov <megadethov@gmail.com>
2017-03-18 09:55:39 +0200

    A functional interface can have methods of object class

--- tutorials/JavaSE/Java_8/javatpoint/src/ua/mega/Functional_Interfaces/Example02/FunctionalInterfaceExample.java
+++ tutorials/JavaSE/Java_8/javatpoint/src/ua/mega/Functional_Interfaces/Example02/FunctionalInterfaceExample.java
@@ -1,11 +1,11 @@
-package ua.mega.Functional_Interfaces.Example01;
+package ua.mega.Functional_Interfaces.Example02;
 
 /*
-Functional Interface also known as Single Abstract Method Interfaces or
-SAM Interfaces. It is a new feature in Java, which helps to achieve
-functional programming approach.
+A functional interface can have methods of object class.
+See in the following example.
  */
 public class FunctionalInterfaceExample implements Sayable {
+
     @Override
     public void say(String msg) {
         System.out.println(msg);
@@ -19,5 +19,13 @@ public class FunctionalInterfaceExample implements Sayable {
 
 @FunctionalInterface
 interface Sayable{
-    void say(String msg);
+    void say(String msg);  // abstract method
+
+    // It can contain any number of methods of Object class.
+
+    int hashCode();
+
+    String toString();
+
+    boolean equals(Object obj);
 }
