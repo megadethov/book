b4f77cac714a0bd7fa58854e76b034650c9ef8d5
Yuriy Yefremov <megadethov@gmail.com>
2017-03-19 19:53:14 +0200

    Dependency Injection by Constructor Example

---
+++ tutorials/JavaEE/Spring/javatpoint/ex01_Dependency-Injection-by-Constructor-BeanFactory/src/main/java/ua/mega/Test.java
@@ -0,0 +1,16 @@
+package ua.mega;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+    public static void main(String[] args) {
+        Resource r = new ClassPathResource("applicationContext.xml");
+        BeanFactory factory = new XmlBeanFactory(r);
+
+        Employee employee = (Employee) factory.getBean("e");
+        employee.show();
+    }
+}
