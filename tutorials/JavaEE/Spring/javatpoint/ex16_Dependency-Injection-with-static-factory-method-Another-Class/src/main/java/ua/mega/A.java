3a9edc00e5ea87aab767e2c64841ba8f8897730b
Yuriy Yefremov <megadethov@gmail.com>
2017-03-20 18:00:51 +0200

    A static factory method that returns instance of another class

---
+++ tutorials/JavaEE/Spring/javatpoint/ex16_Dependency-Injection-with-static-factory-method-Another-Class/src/main/java/ua/mega/A.java
@@ -0,0 +1,8 @@
+package ua.mega;
+
+public class A implements Printable {
+    @Override
+    public void print() {
+        System.out.println("hello a");
+    }
+}
---
+++ tutorials/JavaEE/Spring/javatpoint/ex16_Dependency-Injection-with-static-factory-method-Another-Class/src/main/java/ua/mega/B.java
@@ -0,0 +1,8 @@
+package ua.mega;
+
+public class B implements Printable {
+    @Override
+    public void print() {
+        System.out.println("hello b");
+    }
+}
