6a64287176a7cb5bc26a30ebcd0768a81646573e
Yuriy Yefremov <megadethov@gmail.com>
2017-03-20 18:26:53 +0200

    A non-static factory method that returns instance of another class

---
+++ tutorials/JavaEE/Spring/javatpoint/ex17_Dependency-Injection-with-non-static-factory-method-Another-Class/src/main/java/ua/mega/PrintableFactory.java
@@ -0,0 +1,10 @@
+package ua.mega;
+
+public class PrintableFactory {
+
+    //non-static factory method
+    public Printable getPrintable() {
+        return new A(); //return any one instance, either A or B
+//        return new B();
+    }
+}
