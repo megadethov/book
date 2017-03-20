1002219f20f383b15208e03f94ca8269b137109b
Yuriy Yefremov <megadethov@gmail.com>
2017-03-20 16:57:32 +0200

    Autowiring in Spring

---
+++ tutorials/JavaEE/Spring/javatpoint/ex14_Autowiring/src/main/java/ua/mega/B.java
@@ -0,0 +1,11 @@
+package ua.mega;
+
+public class B {
+    public B() {
+        System.out.println("b is created");
+    }
+
+    void print() {
+        System.out.println("hello b");
+    }
+}
---
+++ tutorials/JavaEE/Spring/javatpoint/ex14_Autowiring/src/main/java/ua/mega/Test.java
@@ -0,0 +1,13 @@
+package ua.mega;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        A a = (A) context.getBean("a");
+
+        a.display();
+    }
+}
