611f7658fe82fa826b7751c1d9d6ea5e3daadc77
Yuriy Yefremov <megadethov@gmail.com>
2017-03-22 17:20:33 +0200

    AOP-After-Returning-Advice-spring12-old-style

---
+++ tutorials/JavaEE/Spring/javatpoint/ex20_AOP-After-Returning-Advice-spring12-old-style/src/main/java/ua/mega/AfterAdvisor.java
@@ -0,0 +1,12 @@
+package ua.mega;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+public class AfterAdvisor implements AfterReturningAdvice {
+    @Override
+    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+        System.out.println("additional concern after returning advice");
+    }
+}
