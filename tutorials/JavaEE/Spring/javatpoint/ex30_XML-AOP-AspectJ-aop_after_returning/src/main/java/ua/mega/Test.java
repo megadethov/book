551004bfc7e2994706dbaa811b7fb5fb02477139
Yuriy Yefremov <megadethov@gmail.com>
2017-03-23 16:39:47 +0200

    XML-AOP-AspectJ-aop_after_returning

---
+++ tutorials/JavaEE/Spring/javatpoint/ex30_XML-AOP-AspectJ-aop_after_returning/src/main/java/ua/mega/Test.java
@@ -0,0 +1,17 @@
+package ua.mega;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Operation operation = (Operation) context.getBean("opBean");
+
+        System.out.println("calling m...");
+        operation.m();
+        System.out.println("calling k...");
+        operation.k();
+    }
+}
