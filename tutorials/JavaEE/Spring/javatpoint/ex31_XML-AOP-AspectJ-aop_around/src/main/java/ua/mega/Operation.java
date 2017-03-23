b6f43d2ddbb9e074ae4bc752f24ad25bdfaf3864
Yuriy Yefremov <megadethov@gmail.com>
2017-03-23 17:18:34 +0200

    XML-AOP-AspectJ-aop_around

---
+++ tutorials/JavaEE/Spring/javatpoint/ex31_XML-AOP-AspectJ-aop_around/src/main/java/ua/mega/Operation.java
@@ -0,0 +1,6 @@
+package ua.mega;
+
+public class Operation {
+    public void msg(){System.out.println("msg() is invoked");}
+    public void display(){System.out.println("display() is invoked");}
+}
---
+++ tutorials/JavaEE/Spring/javatpoint/ex31_XML-AOP-AspectJ-aop_around/src/main/java/ua/mega/TrackOperation.java
@@ -0,0 +1,13 @@
+package ua.mega;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class TrackOperation {
+
+    public Object myAdvice(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("Additional Concern Before calling actual method");
+        Object obj = pjp.proceed();
+        System.out.println("Additional Concern After calling actual method");
+        return obj;
+    }
+}
