16e14be6e13b0e89e28ef7209d807088548a2a11
Yuriy Yefremov <megadethov@gmail.com>
2017-04-06 09:50:29 +0300

    Add User to the DB

--- tutorials/JavaEE/Hibernate/Pluralsight/Introduction_To_Hibernate/m1_ProteinTracker/src/main/java/ua/mega/Program.java
+++ tutorials/JavaEE/Hibernate/Pluralsight/Introduction_To_Hibernate/m1_ProteinTracker/src/main/java/ua/mega/Program.java
@@ -7,7 +7,15 @@ public class Program {
         System.out.println("Hello World");
         Session session = HibernateUtilities.getSessionFactory().openSession();
 
+        session.beginTransaction();
+        User user = new User();
+        user.setName("Joe");
+        user.setGoal(250);
+        session.save(user);
+        session.getTransaction().commit();
+
         session.close();
+        HibernateUtilities.getSessionFactory().close();
 
     }
 }
