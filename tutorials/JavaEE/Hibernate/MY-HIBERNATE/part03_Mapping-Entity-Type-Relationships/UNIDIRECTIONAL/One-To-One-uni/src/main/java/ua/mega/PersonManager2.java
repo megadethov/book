52b297a52b00273e9c6e06678e26bc22f9426f43
Yuriy Yefremov <megadethov@gmail.com>
2017-04-10 18:10:28 +0300

    UNIDIRECT One - To - One

---
+++ tutorials/JavaEE/Hibernate/MY-HIBERNATE/part03_Mapping-Entity-Type-Relationships/UNIDIRECTIONAL/One-To-One-uni/src/main/java/ua/mega/PersonManager2.java
@@ -0,0 +1,28 @@
+package ua.mega;
+
+import org.hibernate.Session;
+import ua.mega.domain.Address;
+import ua.mega.domain.Person;
+import ua.mega.util.HibernateUtil;
+
+public class PersonManager2 {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        Address address = new Address();
+        address.setCity("CITY-1");
+        session.save(address);
+        Person person1 = new Person();
+        person1.setName("NAME-1");
+        person1.setAddress(address);
+        session.save(person1);
+//        Person person2 = new Person();
+//        person2.setName("NAME-2");
+//        person2.setAddress(address);
+//        session.save(person2);
+        session.getTransaction().commit();
+
+        HibernateUtil.getSessionFactory().close();
+    }
+}
