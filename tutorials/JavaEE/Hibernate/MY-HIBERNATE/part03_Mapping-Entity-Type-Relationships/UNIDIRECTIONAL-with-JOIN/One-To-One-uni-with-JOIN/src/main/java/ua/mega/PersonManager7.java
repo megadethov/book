ee5cb14f8b7921215e39b46c5110b13ba5761e4a
Yuriy Yefremov <megadethov@gmail.com>
2017-04-11 10:50:27 +0300

    UNIDIRECT One-To-One with JOIN

---
+++ tutorials/JavaEE/Hibernate/MY-HIBERNATE/part03_Mapping-Entity-Type-Relationships/UNIDIRECTIONAL-with-JOIN/One-To-One-uni-with-JOIN/src/main/java/ua/mega/PersonManager7.java
@@ -0,0 +1,26 @@
+package ua.mega;
+
+import org.hibernate.Session;
+import ua.mega.domain.Address;
+import ua.mega.domain.Person;
+import ua.mega.util.HibernateUtil;
+
+public class PersonManager7 {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Address address = new Address();
+        address.setCity("CITY-1");
+        Person person1 = new Person();
+        person1.setName("NAME-1");
+        person1.setAddress(address);
+        session.save(address);
+        session.save(person1);
+
+        session.getTransaction().commit();
+
+        HibernateUtil.getSessionFactory().close();
+    }
+}
