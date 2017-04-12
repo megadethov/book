411d67238ef75bb8a6394a8c289742c2b3a40216
Yuriy Yefremov <megadethov@gmail.com>
2017-04-12 20:34:40 +0300

    WHERE, LIKE, IN

--- tutorials/JavaEE/SQL/Lynda/SQL_Essential_Training/src/main/resources/scripts/ex02_Basic.sql
+++ tutorials/JavaEE/SQL/Lynda/SQL_Essential_Training/src/main/resources/scripts/ex02_Basic.sql
@@ -98,3 +98,15 @@ INSERT INTO test (a, b) VALUES ( 10, 'a' );
 INSERT INTO test (a, b) VALUES ( 11, 'b' );
 INSERT INTO test (a, b) VALUES ( 12, 'c' );
 SELECT * FROM test;
+
+-- 12 WHERE, LIKE, and IN-----------------------------------------------------------------------------------------------
+-- world.db
+
+SELECT * FROM Country;
+SELECT Name, Continent, Population FROM Country WHERE Population < 100000 ORDER BY Population DESC;
+SELECT Name, Continent, Population FROM Country WHERE Population < 100000 OR Population IS NULL ORDER BY Population DESC;
+SELECT Name, Continent, Population FROM Country WHERE Population < 100000 AND Continent = 'Oceania' ORDER BY Population DESC;
+SELECT Name, Continent, Population FROM Country WHERE Name LIKE '%island%' ORDER BY Name;
+SELECT Name, Continent, Population FROM Country WHERE Name LIKE '%island' ORDER BY Name;
+SELECT Name, Continent, Population FROM Country WHERE Name LIKE '_a%' ORDER BY Name;
+SELECT Name, Continent, Population FROM Country WHERE Continent IN ( 'Europe', 'Asia' ) ORDER BY Name;
