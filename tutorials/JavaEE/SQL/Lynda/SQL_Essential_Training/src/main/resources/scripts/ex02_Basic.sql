377c69d87a6fede9d054b92d33b0c8409a969b7d
Yuriy Yefremov <megadethov@gmail.com>
2017-04-12 15:15:02 +0300

    SQL - DELETE TABLE

--- tutorials/JavaEE/SQL/Lynda/SQL_Essential_Training/src/main/resources/scripts/ex02_Basic.sql
+++ tutorials/JavaEE/SQL/Lynda/SQL_Essential_Training/src/main/resources/scripts/ex02_Basic.sql
@@ -1,5 +1,4 @@
--- 04 CREATE TABLE
--- :memory:
+-- 04 CREATE TABLE-------------------------------------
 
 CREATE TABLE test (
   a INTEGER,
@@ -10,3 +9,11 @@ INSERT INTO test VALUES ( 1, 'a' );
 INSERT INTO test VALUES ( 2, 'b' );
 INSERT INTO test VALUES ( 3, 'c' );
 SELECT * FROM test;
+
+-- 05 DELETE TABLE-------------------------------------
+
+CREATE TABLE test ( a TEXT, b TEXT );
+INSERT INTO test VALUES ( 'one', 'two' );
+SELECT * FROM test;
+DROP TABLE test;
+DROP TABLE IF EXISTS test;
