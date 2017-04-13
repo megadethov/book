63128bebd6f4a2fd285f079e8e2aa7c7c98f1b60
Yuriy Yefremov <megadethov@gmail.com>
2017-04-13 10:45:27 +0300

    SQL - SUBSTR

--- tutorials/JavaEE/SQL/Lynda/SQL_Essential_Training/src/main/resources/scripts/ex04_String.sql
+++ tutorials/JavaEE/SQL/Lynda/SQL_Essential_Training/src/main/resources/scripts/ex04_String.sql
@@ -4,3 +4,15 @@
 SELECT LENGTH('string');
 SELECT Name, LENGTH(Name) AS Len FROM City ORDER BY Len DESC;
 SELECT Name, LENGTH(Name) AS Len FROM City ORDER BY Len DESC, Name;
+
+-- 03 SUBSTR---------------------------------------------------------------------------------
+-- album.db
+
+SELECT SUBSTR('this string', 6);
+SELECT released,
+  SUBSTR(released, 1, 4) AS year,
+  SUBSTR(released, 6, 2) AS month,
+  SUBSTR(released, 9, 2) AS day
+FROM album
+ORDER BY released
+;
