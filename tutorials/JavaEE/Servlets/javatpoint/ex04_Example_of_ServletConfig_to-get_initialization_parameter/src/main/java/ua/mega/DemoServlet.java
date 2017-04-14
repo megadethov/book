d7502d2277c91d6c2bf89eea5102c931f6254b28
Yuriy Yefremov <megadethov@gmail.com>
2017-04-14 20:33:21 +0300

    Example_of_ServletConfig_to-get_initialization_parameter

---
+++ tutorials/JavaEE/Servlets/javatpoint/ex04_Example_of_ServletConfig_to-get_initialization_parameter/src/main/java/ua/mega/DemoServlet.java
@@ -0,0 +1,23 @@
+package ua.mega;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter pw = resp.getWriter();
+
+        ServletConfig config = getServletConfig();
+        String driver = config.getInitParameter("driver");
+        pw.print("Driver is: " + driver);
+
+        pw.close();
+    }
+}
