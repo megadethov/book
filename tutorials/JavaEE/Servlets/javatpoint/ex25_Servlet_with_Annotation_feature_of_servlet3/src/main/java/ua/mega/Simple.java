91f4e2decb84139c9d4d76a5959d8dfc2a784023
Yuriy Yefremov <megadethov@gmail.com>
2017-04-21 09:48:18 +0300

    Servlet with Annotation (feature of servlet3)

---
+++ tutorials/JavaEE/Servlets/javatpoint/ex25_Servlet_with_Annotation_feature_of_servlet3/src/main/java/ua/mega/Simple.java
@@ -0,0 +1,22 @@
+package ua.mega;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/simple")
+public class Simple extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out=resp.getWriter();
+
+        out.print("<html><body>");
+        out.print("<h3>Hello Servlet</h3>");
+        out.print("</body></html>");
+    }
+}
