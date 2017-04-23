0a778becb51c0ddfda072b76618891a16b254717
Yuriy Yefremov <megadethov@gmail.com>
2017-04-23 20:50:55 +0300

    Example of Login Form in Servlet Tutorial

---
+++ tutorials/JavaEE/Servlets/javatpoint/ex33_Example_of_Login_Form_in_Servlet_Tutorial/src/main/java/ua/mega/WelcomeServlet.java
@@ -0,0 +1,21 @@
+package ua.mega;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String n=req.getParameter("username");
+        out.print("Welcome "+n);
+
+        out.close();
+    }
+}
