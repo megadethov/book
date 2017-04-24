9a2108e1b8eb4c429db93214daf7dde67b529f8d
Yuriy Yefremov <megadethov@gmail.com>
2017-04-24 23:19:28 +0300

    java bean class

---
+++ tutorials/JavaEE/JSP/javatpoint/ex23_Java_bean_class/src/main/java/ua/mega/bean/Test.java
@@ -0,0 +1,9 @@
+package ua.mega.bean;
+
+public class Test {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("Aaron");
+        System.out.println(employee.getName());
+    }
+}
