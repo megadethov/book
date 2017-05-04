e7ad4232b8e5fee9a1713274948ffc3ee9ad93ed
Yuriy Yefremov <megadethov@gmail.com>
2017-05-04 12:23:40 +0300

    Bad style JSP

--- tutorials/JavaEE/WebDevelopment/VP/ch08_MVC/src/main/java/ua/mega/servlets/AddNewBookServlet.java
+++ tutorials/JavaEE/WebDevelopment/VP/ch08_MVC/src/main/java/ua/mega/servlets/AddNewBookServlet.java
@@ -3,9 +3,7 @@ package ua.mega.servlets;
 import ua.mega.domain.Book;
 import ua.mega.services.BookService;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,9 +14,8 @@ public class AddNewBookServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        PrintWriter out = res.getWriter();
-        res.setContentType("text/html");
 
+        // do the work and render a response.
         String isbn = req.getParameter("isbn");
         String title = req.getParameter("title");
         String author = req.getParameter("author");
@@ -29,11 +26,9 @@ public class AddNewBookServlet extends HttpServlet {
         BookService service = BookService.getService();
         service.registerNewBook(newBook);
 
-        // render response
-        out.print("<html><head><title>Add new Book</title>");
-        out.print("<body><h1>Book " + title + " has added</h1>");
-        out.print("</body></html>");
-
-        out.close();
+        // Forward to a JSP page to inform the user all is well
+        ServletContext context = req.getServletContext();
+        RequestDispatcher dispatcher = context.getRequestDispatcher("/book-added.jsp");
+        dispatcher.forward(req, res);
     }
 }
--- tutorials/JavaEE/WebDevelopment/VP/ch08_MVC/src/main/java/ua/mega/servlets/DisplayAllBooksServlet.java
+++ tutorials/JavaEE/WebDevelopment/VP/ch08_MVC/src/main/java/ua/mega/servlets/DisplayAllBooksServlet.java
@@ -15,33 +15,12 @@ import java.util.List;
 public class DisplayAllBooksServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        resp.setContentType("text/html");
-        HttpSession session = req.getSession(); // need to encodeURL()
 
         // do some work
         BookService bookService = BookService.getService();
         List<Book> allBooks = bookService.getEntireCatalogue();
-        // output the result page
-        out.print("<html><head><title>Our entire catalogue</title>");
-        out.print("<body><h1>Our entire catalogue</h1>");
-        out.print("<ul>");
-        for (Book next : allBooks) {
-            out.print("<li>");
-            out.print(next.getTitle());
-            // add to Card
-            String url = "AddToCart.html";
-            url = resp.encodeURL(url); // if cookie is disabled (plan B)
 
-            out.print("<form method='get' action='" + url + "'>");
-            out.print("<input type='hidden' name='id' value='" + next.getId() + "'/>");
-            out.print("<input type='submit' value='Add Book to Cart'/>");
-            out.print("</form>");
-            out.print("</li>");
-        }
-        out.print("</ul>");
-        out.print("</body></html>");
+        // forward to the displayAllBooks.jsp page to display the results
 
-        out.close();
     }
 }
