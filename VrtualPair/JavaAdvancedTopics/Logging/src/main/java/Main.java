3c292e8c6cc8d0a515f2ceefb6e0934a7f01600e
Yuriy Yefremov <megadethov@gmail.com>
2017-05-05 17:25:05 +0300

    log4j with default config

---
+++ VrtualPair/JavaAdvancedTopics/Logging/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    static Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.log(Level.ERROR, "Error event");
+
+        AnotherClass ac = new AnotherClass();
+        ac.testMethod();
+
+    }
+}
