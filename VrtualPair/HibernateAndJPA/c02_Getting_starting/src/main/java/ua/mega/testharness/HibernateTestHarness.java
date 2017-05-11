03d86bb178f55e922dbef4f2264ac736fa81032b
Yuriy Yefremov <megadethov@gmail.com>
2017-05-11 14:35:38 +0300

    Workspase for HQL

--- VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/testharness/HibernateTestHarness.java
+++ VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/testharness/HibernateTestHarness.java
@@ -1,6 +1,7 @@
 package ua.mega.testharness;
 
 import ua.mega.domain.Student;
+import ua.mega.domain.Subject;
 import ua.mega.domain.Tutor;
 
 import javax.persistence.EntityManager;
@@ -10,24 +11,58 @@ import javax.persistence.Persistence;
 
 public class HibernateTestHarness {
 
-    public static void main(String[] args) {
+    public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
+    public static void main(String[] args)
+    {
+        setUpData();
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
-
         tx.begin();
 
-        Tutor t1 = new Tutor("adc123", "Aaron", 18314243);
+        // let's do some queries!
+
+        tx.commit();
+        em.close();
+    }
+
+    public static void setUpData()
+    {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // Some subjects
+        Subject mathematics = new Subject("Mathematics", 2);
+        Subject science = new Subject("Science", 2);
+        Subject history = new Subject("History", 3);
+        em.persist(mathematics);
+        em.persist(science);
+        em.persist(history);
+
+        // This tutor will be very busy, with lots of students
+        Tutor t1 = new Tutor("ABC123", "David Banks", 2939393);
+        t1.addSubjectToQualifications(mathematics);
+        t1.addSubjectToQualifications(science);
+
+        // This tutor is new and has no students
+        // But he will be able to teach science and mathematics
+        Tutor t2 = new Tutor("DEF456", "Alan Bridges", 0);
+        t2.addSubjectToQualifications(mathematics);
+        t2.addSubjectToQualifications(science);
+
+        // This tutor is the only tutor who can teach History
+        Tutor t3 = new Tutor("GHI678", "Linda Histroia", 0);
+        t3.addSubjectToQualifications(history);
+
         em.persist(t1);
+        em.persist(t2);
+        em.persist(t3);
 
-        t1.createNewStudentAndAddToSupervisorGroup("Student-1", "1-STD", "1 The Street", "Anytown-1", "1112233");
-        t1.createNewStudentAndAddToSupervisorGroup("Student-2", "2-STD", "2 The Street", "Anytown-2", "23624786");
-        t1.createNewStudentAndAddToSupervisorGroup("Student-3", "3-STD", "2 The Street", "Anytown-2", "23624786");
-        t1.createNewStudentAndAddToSupervisorGroup("Student-4", "4-STD", "2 The Street", "Anytown-2", "23624786");
-        t1.createNewStudentAndAddToSupervisorGroup("Student-5", "5-STD", "2 The Street", "Anytown-2", "23624786");
-
-
+        // this only works because we are cascading from tutor to student
+        t1.createNewStudentAndAddToSupervisorGroup("Marco Fortes", "1-FOR-2010", "1 The Street", "Anytown", "484848");
+        t1.createNewStudentAndAddToSupervisorGroup("Kath Grainer", "2-GRA-2009", "2 Kaths Street", "Georgia", "939393");
+        t3.createNewStudentAndAddToSupervisorGroup("Sandra Perkins", "3-PER-2009", "4 The Avenue", "Georgia", "939393");
 
         tx.commit();
         em.close();
