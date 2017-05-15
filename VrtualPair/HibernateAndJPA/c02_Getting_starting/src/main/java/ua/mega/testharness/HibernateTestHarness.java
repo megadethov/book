49eae27c27337fff4ce81b164db1f6c1b6c84d92
Yuriy Yefremov <megadethov@gmail.com>
2017-05-15 16:48:53 +0300

    @Version - if version != OptimisticLockException + Handle ex

--- VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/testharness/HibernateTestHarness.java
+++ VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/testharness/HibernateTestHarness.java
@@ -4,6 +4,8 @@ import ua.mega.domain.Student;
 import ua.mega.domain.Tutor;
 import ua.mega.services.TutorManagement;
 
+import javax.persistence.OptimisticLockException;
+
 public class HibernateTestHarness {
     public static void main(String[] args) {
         TutorManagement tutorManagement = new TutorManagement();
@@ -15,8 +17,13 @@ public class HibernateTestHarness {
 
         newTutor.setName("Clara Smith");
 
-        // STEP 2
-        tutorManagement.updateTutor(newTutor);
+        // STEP 2 - because there's been, check for a stale object
+        try {
+            tutorManagement.updateTutor(newTutor);
+        } catch (OptimisticLockException e) {
+            // deal with the problem
+            System.out.println("Sorry, that tutor was updated while you where thinking. Please try again!");
+        }
 
     }
 }
