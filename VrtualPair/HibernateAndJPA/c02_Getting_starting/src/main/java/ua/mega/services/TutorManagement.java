da7d6289d1d65fe58e9ff50876e8294e6576a55b
Yuriy Yefremov <megadethov@gmail.com>
2017-05-15 19:46:33 +0300

    Pessimistic Lock - generateReport() impl

--- VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/services/TutorManagement.java
+++ VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/services/TutorManagement.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 /*
 * This is not production standard!
@@ -36,6 +37,19 @@ public class TutorManagement {
         return newTutor;
     }
 
+    public Tutor findTutorById(int id) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Tutor foundTutor = em.find(Tutor.class, id);
+
+        tx.commit();
+        em.close();
+
+        return foundTutor;
+    }
+
     // STEP 2
     public Tutor updateTutor(Tutor tutorToUpdate) {
         EntityManager em = emf.createEntityManager();
@@ -51,5 +65,25 @@ public class TutorManagement {
         tx.commit();
         em.close();
         return tutorToUpdate;
+    }
+
+    // this method will give the name and salarys of every tutor in the college, with their salary
+    // a summary at the end with the total salary bill for the whole college.
+    public void generateReport() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        List<Object[]> namesAndSalarys = em.createQuery("select tutor.name, tutor.salary from Tutor as tutor").getResultList();
+
+        for (Object[] next : namesAndSalarys) {
+            System.out.println(next[0] + " : " + next[1]);
+        }
+
+        long totalSalary = (long) em.createQuery("select sum(tutor.salary) from Tutor as tutor").getSingleResult();
+        System.out.println("Total college salary is: " + totalSalary);
+
+        tx.commit();
+        em.close();
     }
 }
--- VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/testharness/HibernateTestHarness.java
+++ VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/testharness/HibernateTestHarness.java
@@ -5,17 +5,22 @@ import ua.mega.domain.Tutor;
 import ua.mega.services.TutorManagement;
 
 import javax.persistence.OptimisticLockException;
+import java.util.Scanner;
 
 public class HibernateTestHarness {
     public static void main(String[] args) {
         TutorManagement tutorManagement = new TutorManagement();
 
         // STEP 1
-        Tutor newTutor = tutorManagement.createNewTutor("1234567", "Clara Jones", 10000);
+        Tutor newTutor = tutorManagement.findTutorById(1);
 
         // client will now sit and wait....
 
-        newTutor.setName("Clara Smith");
+        newTutor.setName("Process-1 name");
+
+        System.out.println("Press enter to continue");
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
 
         // STEP 2 - because there's been, check for a stale object
         try {
