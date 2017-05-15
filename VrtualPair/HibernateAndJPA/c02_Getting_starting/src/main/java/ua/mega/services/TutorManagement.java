b07adf8c348aca1c10e57fa41b6323e593b6600e
Yuriy Yefremov <megadethov@gmail.com>
2017-05-15 09:38:55 +0300

    Extend tutorManagemet

--- VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/services/TutorManagement.java
+++ VrtualPair/HibernateAndJPA/c02_Getting_starting/src/main/java/ua/mega/services/TutorManagement.java
@@ -15,18 +15,31 @@ import javax.persistence.Persistence;
 
 public class TutorManagement {
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
+    EntityManager em;
 
     public Tutor createNewTutor(String staffId, String name, int salary) {
-        EntityManager em = emf.createEntityManager();
+        em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         Tutor newTutor = new Tutor(staffId, name, salary);
         em.persist(newTutor);
 
+        // commit release any database connection
+        tx.commit();
+
+        return newTutor;
+    }
+
+    public Tutor updateTutor(Tutor tutorToUpdate) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // tutorToUpdate will be dirty
+
         tx.commit();
         em.close();
 
-        return newTutor;
+        return tutorToUpdate;
     }
 }
