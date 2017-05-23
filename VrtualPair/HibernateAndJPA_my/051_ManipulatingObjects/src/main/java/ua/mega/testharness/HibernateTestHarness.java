c684831e9df6742b1afb23cd2fdb544fa04eb714
Yuriy Yefremov <megadethov@gmail.com>
2017-05-23 17:32:03 +0300

    HibernateAndJPA_my - session.get()

---
+++ VrtualPair/HibernateAndJPA_my/051_ManipulatingObjects/src/main/java/ua/mega/testharness/HibernateTestHarness.java
@@ -0,0 +1,41 @@
+package ua.mega.testharness;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import ua.mega.domain.Student;
+
+public class HibernateTestHarness {
+
+    private static SessionFactory sessionFactory = null;
+
+    public static void main(String[] args) {
+
+        SessionFactory sf = getSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+        Student myStudent = (Student) session.get(Student.class, 7);
+        System.out.println(myStudent);
+
+        tx.commit();
+        session.close();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ServiceRegistry serviceRegistry = new
+                    ServiceRegistryBuilder().applySettings(configuration.getProperties())
+                    .buildServiceRegistry();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+}
