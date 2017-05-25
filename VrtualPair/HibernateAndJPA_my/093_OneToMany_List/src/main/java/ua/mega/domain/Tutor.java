8ac46547a74029fccf5200fe981080fca434eea3
Yuriy Yefremov <megadethov@gmail.com>
2017-05-25 22:08:29 +0300

    HibernateAndJPA_my - @OneToMany - List

---
+++ VrtualPair/HibernateAndJPA_my/093_OneToMany_List/src/main/java/ua/mega/domain/Tutor.java
@@ -0,0 +1,43 @@
+package ua.mega.domain;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Tutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String stuffId;
+    private String name;
+    private int salary;
+
+    @OneToMany
+    @JoinColumn(name = "TUTOR_FK")
+    private List<Student> supervisionGroup;
+
+    public Tutor() {
+    }
+
+    public Tutor(String stuffId, String name, int salary) {
+        this.stuffId = stuffId;
+        this.name = name;
+        this.salary = salary;
+        supervisionGroup = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addStudentToSupervisionGroup(Student addStudent) {
+        supervisionGroup.add(addStudent);
+    }
+
+    public List<Student> getSupervisionGroup() {
+        List<Student> unmodifiableGroup = Collections.unmodifiableList(this.supervisionGroup);
+        return unmodifiableGroup;
+    }
+}
---
+++ VrtualPair/HibernateAndJPA_my/093_OneToMany_List/src/main/java/ua/mega/testharness/HibernateTestHarness.java
@@ -0,0 +1,75 @@
+package ua.mega.testharness;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import ua.mega.domain.Student;
+import ua.mega.domain.Tutor;
+
+import java.util.List;
+import java.util.Map;
+
+public class HibernateTestHarness {
+
+    private static SessionFactory sessionFactory = null;
+
+    public static void main(String[] args) {
+
+        SessionFactory sf = getSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+
+       /* Student myStudent = new Student("StudentName-2");
+        Tutor myTutor = new Tutor("222-STF", "TutorName-2", 10000);
+
+        session.save(myStudent);
+        session.save(myTutor);
+
+        // make the student be supervised by the tutor
+        myStudent.allocateSupervisor(myTutor);
+        // sout the Tutor name
+        System.out.println("Tutor name is: " + myStudent.getSupervisorName());*/
+
+       /* Tutor tutor = new Tutor("111-STF", "nameTut-1", 10000);
+
+        Student student1 = new Student("nameSt-1", "101-ENRID");
+        Student student2 = new Student("nameSt-2", "102-ENRID");
+        Student student3 = new Student("nameSt-3", "103-ENRID");
+
+        session.save(student1);
+        session.save(student2);
+        session.save(student3);
+        session.save(tutor);
+
+        tutor.addStudentToSupervisionGroup(student1);
+        tutor.addStudentToSupervisionGroup(student2);
+        tutor.addStudentToSupervisionGroup(student3);*/
+
+        Tutor foundTutor = (Tutor) session.get(Tutor.class, 1);
+        List<Student> students = foundTutor.getSupervisionGroup();
+
+        for (Student next : students) {
+            System.out.println(next);
+        }
+
+        tx.commit(); // SQL execute here
+        session.close();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ServiceRegistry serviceRegistry = new
+                    ServiceRegistryBuilder().applySettings(configuration.getProperties())
+                    .buildServiceRegistry();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
+    }
+}
