621e0b566b780919bbca894a1dbeb32eb62668c1
Yuriy Yefremov <megadethov@gmail.com>
2017-05-26 10:31:21 +0300

    HibernateAndJPA_my - Bidirectional - problem FK=null

---
+++ VrtualPair/HibernateAndJPA_my/101_Bidirectional/src/main/java/ua/mega/domain/Tutor.java
@@ -0,0 +1,44 @@
+package ua.mega.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
+public class Tutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String stuffId;
+    private String name;
+    private int salary;
+
+    @OneToMany(mappedBy = "supervisor")
+    private List<Student> supervisionGroup;
+
+    public Tutor() {
+    }
+
+    public Tutor(String stuffId, String name, int salary) {
+        this.stuffId = stuffId;
+        this.name = name;
+        this.salary = salary;
+        supervisionGroup = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addStudentToSupervisionGroup(Student addStudent) {
+        supervisionGroup.add(addStudent);
+    }
+
+    public List<Student> getSupervisionGroup() {
+        List<Student> unmodifiableGroup = Collections.unmodifiableList(this.supervisionGroup);
+        return unmodifiableGroup;
+    }
+}
