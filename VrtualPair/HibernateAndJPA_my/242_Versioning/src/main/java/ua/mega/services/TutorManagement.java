89925fefd9d4fd976d3e3841b1d1aa61f039e2aa
Yuriy Yefremov <megadethov@gmail.com>
2017-06-02 22:48:51 +0300

    HibernateAndJPA_my - Versioning

---
+++ VrtualPair/HibernateAndJPA_my/242_Versioning/src/main/java/ua/mega/services/TutorManagement.java
@@ -0,0 +1,57 @@
+package ua.mega.services;
+
+import ua.mega.domain.Tutor;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * not fo production style
+ */
+
+public class TutorManagement {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
+
+    // STEP1
+    public Tutor createTutor(String stuffId, String name, int salary) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Tutor myTutor = new Tutor(stuffId, name, salary);
+
+        // Lock the Tutor
+        em.persist(myTutor);
+
+        // commit realise any DB conn
+        tx.commit();
+        // close will detach the tutor
+        em.close();
+
+        return myTutor;
+    }
+
+    // STEP2
+    public Tutor updateTutor(Tutor tutorToUpdate) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // make the object be dirty checked again
+        tutorToUpdate = em.merge(tutorToUpdate); // don't forget re-assign!!!
+
+        // dirty check
+
+        // Unlock the Tutor
+        tx.commit();
+        em.close();
+
+        return tutorToUpdate;
+    }
+
+}
+
+
---
+++ VrtualPair/HibernateAndJPA_my/242_Versioning/src/main/java/ua/mega/testharness/HibernateTestHarness.java
@@ -0,0 +1,30 @@
+package ua.mega.testharness;
+
+import ua.mega.domain.Student;
+import ua.mega.domain.Tutor;
+import ua.mega.services.TutorManagement;
+
+import javax.persistence.OptimisticLockException;
+
+public class HibernateTestHarness {
+
+    public static void main(String[] args) {
+
+        TutorManagement tutorManagement = new TutorManagement();
+
+        // Step 1
+        Tutor tutor = tutorManagement.createTutor("12345", "Venya", 12000);
+
+        // client seat and wait...
+        tutor.setName("Clara Cetcin");
+
+        // Step 2 Because there is been a pause, check for a stale object
+        try {
+            tutorManagement.updateTutor(tutor);
+        } catch (OptimisticLockException e) {
+            // deal with the problem
+            System.out.println("Sorry, that tutor was updated while you are thinking! Please try again.");
+        }
+
+    }
+}
