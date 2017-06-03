7d3adaf037078d8a86185e65434536bd78a56b82
Yuriy Yefremov <megadethov@gmail.com>
2017-06-03 15:23:46 +0300

    HibernateAndJPA_my - Lazy Init - start workspase

---
+++ VrtualPair/HibernateAndJPA_my/261_Lazy_Initialization/src/main/java/ua/mega/services/TutorManagement.java
@@ -0,0 +1,108 @@
+package ua.mega.services;
+
+import ua.mega.domain.Tutor;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * not to production style
+ */
+
+public class TutorManagement {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
+
+    // STEP1
+    public Tutor createTutor(String stuffId, String name, int salary) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Tutor myTutor = new Tutor(stuffId, name, salary);
+
+        // Lock the Tutor
+        em.persist(myTutor);
+
+        // commit realise any DB conn
+        tx.commit();
+        // close will detach the tutor
+        em.close();
+
+        return myTutor;
+    }
+
+    public Tutor findTutorById(int id) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Tutor tutor = em.find(Tutor.class, id);
+
+        tx.commit();
+        em.close();
+
+        return tutor;
+    }
+
+    // STEP2
+    public Tutor updateTutor(Tutor tutorToUpdate) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // make the object be dirty checked again
+        tutorToUpdate = em.merge(tutorToUpdate); // don't forget re-assign!!!
+
+        // dirty check
+
+        // Unlock the Tutor
+        tx.commit();
+        em.close();
+
+        return tutorToUpdate;
+    }
+
+    public void generateReport() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        /*List<Object[]> result = em.createQuery("select tutor.name, tutor.salary from Tutor as tutor").getResultList();
+        for (Object[] next : result) {
+            System.out.println(next[0] + "---" + next[1]);
+        }*/
+
+        List<Object[]> result = em.createQuery("select tutor.name, tutor.salary from Tutor as tutor")
+                .setLockMode(LockModeType.PESSIMISTIC_READ)
+                .getResultList();
+        for (Object[] next : result) {
+            System.out.println(next[0] + "---" + next[1]);
+        }
+
+        long totalSalary = (Long) em.createQuery("select sum(tutor.salary) from Tutor as tutor").getSingleResult();
+
+        System.out.println("Total salary is: " + totalSalary);
+
+        tx.commit();
+        em.close();
+    }
+
+    public void inMemoryQuery(int id) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Tutor tutor = em.find(Tutor.class, id);
+
+        // if here other process something change in DB
+        // This does not concern us, because we work with in memory Object
+
+        tutor.setSalary(20000);
+
+        tx.commit();em.close();
+    }
+
+}
+
+
---
+++ VrtualPair/HibernateAndJPA_my/261_Lazy_Initialization/src/main/java/ua/mega/testharness/PerformanceTesting.java
@@ -0,0 +1,68 @@
+package ua.mega.testharness;
+
+import ua.mega.domain.Subject;
+import ua.mega.domain.Tutor;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class PerformanceTesting {
+    public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("myDatabaseConfig");
+
+    public static void main(String[] args)
+    {
+        setUpData();
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // let's do some queries!
+
+        tx.commit();
+        em.close();
+    }
+
+    public static void setUpData()
+    {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // Some subjects
+        Subject mathematics = new Subject("Mathematics", 2);
+        Subject science = new Subject("Science", 2);
+        Subject history = new Subject("History", 3);
+        em.persist(mathematics);
+        em.persist(science);
+        em.persist(history);
+
+        // This tutor will be very busy, with lots of students
+        Tutor t1 = new Tutor("ABC123", "David Banks", 2939393);
+        t1.addSubjectToTutor(mathematics);
+        t1.addSubjectToTutor(science);
+
+        // This tutor is new and has no students
+        // But he will be able to teach science and mathematics
+        Tutor t2 = new Tutor("DEF456", "Alan Bridges", 0);
+        t2.addSubjectToTutor(mathematics);
+        t2.addSubjectToTutor(science);
+
+        // This tutor is the only tutor who can teach History
+        Tutor t3 = new Tutor("GHI678", "Linda Histroia", 0);
+        t3.addSubjectToTutor(history);
+
+        em.persist(t1);
+        em.persist(t2);
+        em.persist(t3);
+
+        // this only works because we are cascading from tutor to student
+        t1.createStudentAndAddToSupervisionGroup("Marco Fortes", "1-FOR-2010", "1 The Street", "Anytown", "484848");
+        t1.createStudentAndAddToSupervisionGroup("Kath Grainer", "2-GRA-2009", "2 Kaths Street", "Georgia", "939393");
+        t3.createStudentAndAddToSupervisionGroup("Sandra Perkins", "3-PER-2009", "4 The Avenue", "Georgia", "939393");
+
+        tx.commit();
+        em.close();
+    }
+}
