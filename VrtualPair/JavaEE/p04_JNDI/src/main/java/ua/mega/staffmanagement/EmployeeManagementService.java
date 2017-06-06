7652a263cfcf664906546e51bfaabc4b0fd6b444
Yuriy Yefremov <megadethov@gmail.com>
2017-06-06 21:00:14 +0300

    VP JavaEE - JNDI - sw

---
+++ VrtualPair/JavaEE/p04_JNDI/src/main/java/ua/mega/staffmanagement/EmployeeManagementService.java
@@ -0,0 +1,15 @@
+package ua.mega.staffmanagement;
+
+import ua.mega.staffmanagement.domain.Employee;
+
+import javax.ejb.Remote;
+import java.util.List;
+
+@Remote
+public interface EmployeeManagementService {
+    public void registerEmployee(Employee newEmployee);
+
+    public List<Employee> getAllEmployees();
+
+    public List<Employee> searchBySurname(String surname);
+}
---
+++ VrtualPair/JavaEE/p04_JNDI/src/main/java/ua/mega/staffmanagement/EmployeeManagementServiceImplementation.java
@@ -0,0 +1,29 @@
+package ua.mega.staffmanagement;
+
+import ua.mega.staffmanagement.domain.Employee;
+
+import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateless
+public class EmployeeManagementServiceImplementation implements EmployeeManagementService {
+    @Override
+    public void registerEmployee(Employee newEmployee) {
+
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        List<Employee> tempList = new ArrayList<>();
+        tempList.add(new Employee("temp", "temp", "temp", 100));
+        return tempList;
+    }
+
+    @Override
+    public List<Employee> searchBySurname(String surname) {
+        List<Employee> tempList = new ArrayList<>();
+        tempList.add(new Employee("temp", "temp", "temp", 100));
+        return tempList;
+    }
+}
