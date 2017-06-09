7b38868ad7db0209e2feacde10d94bcd1be4563e
Yuriy Yefremov <megadethov@gmail.com>
2017-06-09 09:30:34 +0300

    VP JavaEE JPA - create Entity

--- VrtualPair/JavaEE/p061_JPA/src/main/java/ua/mega/staffmanagement/domain/Employee.java
+++ VrtualPair/JavaEE/p061_JPA/src/main/java/ua/mega/staffmanagement/domain/Employee.java
@@ -1,12 +1,24 @@
 package ua.mega.staffmanagement.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int Id;
+
     private String firstName;
     private String surname;
     private String jobRole;
     private int salary;
+
+    public Employee() {
+    }
 
     public Employee(String firstName, String surname, String jobRole, int salary) {
         this.firstName = firstName;
