aac3bc071d9cecc9efee17fc36e5a1cdfb147e1d
Yuriy Yefremov <megadethov@gmail.com>
2017-06-11 08:02:15 +0300

    VP JavaEE JPQL Binding Dynamic Parameters

--- VrtualPair/JavaEE/p091_JPQL/src/main/java/ua/mega/test/TestHarness.java
+++ VrtualPair/JavaEE/p091_JPQL/src/main/java/ua/mega/test/TestHarness.java
@@ -19,7 +19,11 @@ public class TestHarness {
 
 //        Query q = em.createQuery("select employee from Employee employee");
 //        Query q = em.createQuery("select employee from Employee employee where employee.surname='xurname-7'");
-        Query q = em.createQuery("select employee from Employee employee where employee.surname like 's%'");
+//        Query q = em.createQuery("select employee from Employee employee where employee.surname like 's%'");
+
+        String requiredName = "xurname-7";
+        Query q = em.createQuery("select employee from Employee employee where employee.surname=:name");
+        q.setParameter("name", requiredName);
         List<Employee> employees = q.getResultList();
         for (Employee next : employees) {
             System.out.println(next);
