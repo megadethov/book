ee04128538ffc9a47c6ddc30d6abd5ed8ae378ef
Yuriy Yefremov <megadethov@gmail.com>
2017-06-12 08:25:06 +0300

    VP JavaEE Rollback sw

---
+++ VrtualPair/JavaEE/p121_Rollback/src/main/java/ua/mega/client/ClientApplicationTest.java
@@ -0,0 +1,34 @@
+package ua.mega.client;
+
+import ua.mega.staffmanagement.EmployeeManagementService;
+import ua.mega.staffmanagement.domain.Employee;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.List;
+
+public class ClientApplicationTest {
+    public static void main(String[] args) {
+        try {
+            Context jndi = new InitialContext();
+
+//            EmployeeManagementService service = (EmployeeManagementService) jndi.lookup("ua.mega.staffmanagement.EmployeeManagementService");
+            EmployeeManagementService service = (EmployeeManagementService) jndi.lookup("java:global/Employee/EmployeeManagementServiceImplementation");
+
+            /*service.registerEmployee(new Employee("Richard", "Chesterwood", "Programmer", 0));
+            List<Employee> employees = service.getAllEmployees();
+            for (Employee next : employees) {
+                System.out.println(next);
+            }*/
+
+            List<Employee> employees = service.searchBySurname("Chesterwood");
+            for (Employee next : employees) {
+                System.out.println(next);
+            }
+
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+}
