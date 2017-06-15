455fcb04db262712724253f617067d91fd9bd029
Yuriy Yefremov <megadethov@gmail.com>
2017-06-15 12:27:20 +0300

    VP JavaEE JSF components - Navigating Tables

--- VrtualPair/JavaEE/p151_JSF_Components/src/main/java/ua/mega/backingbeans/AllEmployeesPageBean.java
+++ VrtualPair/JavaEE/p151_JSF_Components/src/main/java/ua/mega/backingbeans/AllEmployeesPageBean.java
@@ -5,6 +5,7 @@ import ua.mega.staffmanagement.domain.Employee;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIData;
 import java.util.List;
 
 @ManagedBean(name = "allEmployeesPageBean")
@@ -13,7 +14,33 @@ public class AllEmployeesPageBean {
     @EJB
     private EmployeeManagementServiceLocal employeeService;
 
+    private Employee selectedEmployee;
+
+    private UIData dataTable;
+
+    public String showEmployee() {
+        selectedEmployee = (Employee) dataTable.getRowData();
+        return "employeeDetail";
+    }
+
+    public Employee getSelectedEmployee() {
+        return selectedEmployee;
+    }
+
+    public void setSelectedEmployee(Employee selectedEmployee) {
+        this.selectedEmployee = selectedEmployee;
+    }
+
+    public UIData getDataTable() {
+        return dataTable;
+    }
+
+    public void setDataTable(UIData dataTable) {
+        this.dataTable = dataTable;
+    }
+
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
 }
