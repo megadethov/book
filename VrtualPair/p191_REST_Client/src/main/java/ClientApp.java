bf4b09fa4b8b28fd325fd2cc3ec4c659c155ada9
Yuriy Yefremov <megadethov@gmail.com>
2017-06-21 08:19:18 +0300

    VP JavaEE REST resolve issue with generic type in REST

--- VrtualPair/p191_REST_Client/src/main/java/ClientApp.java
+++ VrtualPair/p191_REST_Client/src/main/java/ClientApp.java
@@ -1,5 +1,8 @@
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+
+import java.util.List;
 
 public class ClientApp {
     public static void main(String[] args) {
@@ -8,5 +11,15 @@ public class ClientApp {
         Employee employee = web.get(Employee.class);
 
         System.out.println("Employee name is: " + employee.getFirstName() + " " + employee.getSurname());
+
+//        ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        web = client.resource("http://localhost:8080/webapp/webservice/employees");
+        List<Employee> allEmployees = web.get(new GenericType<List<Employee>>() {});
+
+        for (Employee next : allEmployees) {
+            System.out.println(next.getFirstName() + " " + next.getSurname());
+        }
+
     }
 }
