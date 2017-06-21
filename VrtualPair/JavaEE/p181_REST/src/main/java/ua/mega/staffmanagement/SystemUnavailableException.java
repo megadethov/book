6cac90f2060c0783eeeb5be719ebaf3e405f3432
Yuriy Yefremov <megadethov@gmail.com>
2017-06-21 10:23:58 +0300

    VP JavaEE REST UniformInterfaceException

--- VrtualPair/JavaEE/p181_REST/src/main/java/ua/mega/staffmanagement/SystemUnavailableException.java
+++ VrtualPair/JavaEE/p181_REST/src/main/java/ua/mega/staffmanagement/SystemUnavailableException.java
@@ -2,7 +2,7 @@ package ua.mega.staffmanagement;
 
 import javax.ejb.ApplicationException;
 
-//@ApplicationException(rollback = true)
+//@ApplicationException(rollback = true) // need only for checked Exception!!!
 public class SystemUnavailableException extends RuntimeException {
 
 }
