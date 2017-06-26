f7a9258d11c6b28d370aa0dc2855a7f0517d14ef
Yuriy Yefremov <megadethov@gmail.com>
2017-06-26 09:03:11 +0300

    JavaEE_JMS - Sending in Batches - Session.AUTO_ACKNOWLEDGE=0

--- VrtualPair/JavaEE_JMS_MessageSender/src/main/java/MessageSender2.java
+++ VrtualPair/JavaEE_JMS_MessageSender/src/main/java/MessageSender2.java
@@ -12,7 +12,8 @@ public class MessageSender2 {
             ConnectionFactory cf = (ConnectionFactory) ctx.lookup("jms/ConnectionFactory");
             connection = cf.createConnection();
 //            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE); // true=batch
+//            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE); // true=batch
+            Session session = connection.createSession(true, 0); // true=batch
             MessageProducer messageProducer = session.createProducer(queue);
 
             MapMessage message = session.createMapMessage();
@@ -20,11 +21,11 @@ public class MessageSender2 {
             message.setStringProperty("surname", "Pupkin");
             message.setInt("salary", 100);
 
-            messageProducer.send(message, DeliveryMode.PERSISTENT, 7, 10000);
-            messageProducer.send(message, DeliveryMode.PERSISTENT, 7, 10000);
-            messageProducer.send(message, DeliveryMode.PERSISTENT, 7, 10000);
-            messageProducer.send(message, DeliveryMode.PERSISTENT, 7, 10000);
-            messageProducer.send(message, DeliveryMode.PERSISTENT, 7, 10000);
+            messageProducer.send(message);
+            messageProducer.send(message);
+            messageProducer.send(message);
+            messageProducer.send(message);
+            messageProducer.send(message);
 
             session.commit();
 
