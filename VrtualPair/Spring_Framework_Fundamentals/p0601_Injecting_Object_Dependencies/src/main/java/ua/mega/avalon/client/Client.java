1100e2bed14d0bba3d6cb1c23059c86cfb636f7f
Yuriy Yefremov <megadethov@gmail.com>
2017-07-01 21:52:50 +0300

    VP Spring Fundamental - Provide PurchasingService

--- VrtualPair/Spring_Framework_Fundamentals/p0601_Injecting_Object_Dependencies/src/main/java/ua/mega/avalon/client/Client.java
+++ VrtualPair/Spring_Framework_Fundamentals/p0601_Injecting_Object_Dependencies/src/main/java/ua/mega/avalon/client/Client.java
@@ -5,19 +5,26 @@ import java.util.List;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.mega.avalon.domain.Book;
 import ua.mega.avalon.services.BookService;
+import ua.mega.avalon.services.PurchasingService;
 
-public class Client
-{
-	public static void main(String[] args)
-	{
-		ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
-		
-		BookService service = container.getBean(BookService.class);
+public class Client {
+    public static void main(String[] args) {
 
+        System.out.println("Testing buying a book....");
+        String requiredIsbn = "ISBN1"; // we know this isbn is present in the mock
+
+        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
+
+		/*BookService service = container.getBean(BookService.class);
 		List<Book> allBooks = service.getEntireCatalogue();
 		for (Book next : allBooks)
 		{
 			System.out.println(next);
-		}
-	}
+		}*/
+
+        PurchasingService purchasingService = container.getBean("purchasingService", PurchasingService.class);
+        purchasingService.buyBook(requiredIsbn);
+
+        container.close();
+    }
 }
---
+++ VrtualPair/Spring_Framework_Fundamentals/p0601_Injecting_Object_Dependencies/src/main/java/ua/mega/avalon/services/PurchasingServiceImpl.java
@@ -0,0 +1,33 @@
+package ua.mega.avalon.services;
+
+import ua.mega.avalon.domain.Book;
+
+public class PurchasingServiceImpl implements PurchasingService {
+    private AccountsService accounts;
+    private BookService books;
+
+    public PurchasingServiceImpl(AccountsService accountsService, BookService booksService) {
+        this.accounts = accountsService;
+        this.books = booksService;
+    }
+
+    public PurchasingServiceImpl() {
+    }
+
+    public void setAccounts(AccountsService accounts) {
+        this.accounts = accounts;
+    }
+
+    public void setBooks(BookService books) {
+        this.books = books;
+    }
+
+    @Override
+    public void buyBook(String isbn) {
+        // find the correct book
+        Book requiredBook = books.getBookByIsbn(isbn);
+
+        // now raise the invoice
+        accounts.raiseInvoice(requiredBook);
+    }
+}
