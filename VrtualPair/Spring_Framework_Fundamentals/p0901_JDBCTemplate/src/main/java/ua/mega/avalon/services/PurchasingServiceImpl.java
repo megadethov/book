4c60266dd1cc59cb700a6ed3eb158af95b273423
Yuriy Yefremov <megadethov@gmail.com>
2017-07-02 15:52:58 +0300

    VP Spring Fundamental - 09 - BookDaoHorridJdbcImpl

---
+++ VrtualPair/Spring_Framework_Fundamentals/p0901_JDBCTemplate/src/main/java/ua/mega/avalon/services/PurchasingServiceImpl.java
@@ -0,0 +1,33 @@
+package ua.mega.avalon.services;
+
+import ua.mega.avalon.domain.Book;
+
+public class PurchasingServiceImpl implements PurchasingService {
+    private AccountsService accounts;
+    private BookService books;
+
+    public PurchasingServiceImpl(AccountsService accounts, BookService books) {
+        this.accounts = accounts;
+        this.books = books;
+    }
+
+   /* public PurchasingServiceImpl() {
+        System.out.println("Cteate instance of PurchasingServiceImpl..." + this);
+    }*/
+
+   /* public void setAccounts(AccountsService accounts) {
+        this.accounts = accounts;
+    }
+    public void setBooks(BookService books) {
+        this.books = books;
+    }*/
+
+    @Override
+    public void buyBook(String isbn) {
+        // find the correct book
+        Book requiredBook = books.getBookByIsbn(isbn);
+
+        // now raise the invoice
+        accounts.raiseInvoice(requiredBook);
+    }
+}
