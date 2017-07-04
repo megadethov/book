1215b9c20617aa36fa058c9c2ef8063d92c2eb42
Yuriy Yefremov <megadethov@gmail.com>
2017-07-04 17:32:51 +0300

    VP Spring Fundamental - p1201_CRM_System sw

---
+++ VrtualPair/Spring_Framework_Fundamentals/p1201_CRM_System/src/main/java/ua/mega/domain/Call.java
@@ -0,0 +1,75 @@
+package ua.mega.domain;
+
+import java.util.Date;
+
+/**
+ * Represents a call made by a customer.
+ * <p/>
+ * For example : "at 4:30pm on the 2nd December, Martha from Acme Corp called
+ * asking if we are interested in buying their Giant Rubber
+ * Bands (for tripping road runners)"
+ *
+ * @author Richard Chesterwood
+ *
+ */
+public class Call {
+    /**
+     * database neutral id: used for JPA, ignore until then
+     */
+    private int id;
+
+    /**
+     * Records the time and date of the call. If you want
+     * to change the type for something better (like Calendar or even
+     * something better like http://joda-time.sourceforge.net/)
+     */
+    private Date timeAndDate;
+
+    /**
+     * General notes about the call
+     */
+    private String notes;
+
+    /**
+     * Constructor for Call
+     * @param notes eg "Martha from Acme Corp Called etc"
+     */
+    public Call(String notes)
+    {
+        // this defaults to a timestamp of "now"
+        this (notes, new Date());
+    }
+
+    /**
+     * Constructor for Call if the automatic timestamp needs to be overridden
+     */
+    public Call(String notes, Date timestamp)
+    {
+        // this defaults to a timestamp of "now"
+        this.timeAndDate = timestamp;
+        this.notes = notes;
+    }
+    public String toString()
+    {
+        return this.timeAndDate + " : " + this.notes;
+    }
+
+    public Date getTimeAndDate() {
+        return timeAndDate;
+    }
+
+    public void setTimeAndDate(Date timeAndDate) {
+        this.timeAndDate = timeAndDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    // needed for JPA - ignore until then
+    public Call() {}
+}
---
+++ VrtualPair/Spring_Framework_Fundamentals/p1201_CRM_System/src/main/java/ua/mega/services/customers/CustomerManagementServiceImpl.java
@@ -0,0 +1,67 @@
+package ua.mega.services.customers;
+
+import ua.mega.domain.Call;
+import ua.mega.domain.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerManagementServiceImpl implements CustomerManagementService {
+
+    private Map<String, Customer> customers;
+
+    public CustomerManagementServiceImpl() {
+        customers = new HashMap<>();
+        customers.put("C2040", new Customer("C2040", "Disney", "Note-1"));
+        customers.put("C2041", new Customer("C2041", "Home", "Note-2"));
+        customers.put("C2042", new Customer("C2042", "Sclep", "Note-3"));
+    }
+
+    @Override
+    public void newCustomer(Customer newCustomer) {
+        this.customers.put(newCustomer.getCustomerId(), newCustomer);
+    }
+
+    @Override
+    public void updateCustomer(Customer changedCustomer) {
+        this.customers.put(changedCustomer.getCustomerId(), changedCustomer);
+    }
+
+    @Override
+    public void deleteCustomer(Customer oldCustomer) {
+        this.customers.remove(oldCustomer.getCustomerId());
+    }
+
+    @Override
+    public Customer findCustomerById(String customerId) throws CustomerNotFoundException {
+        if (this.customers.get(customerId) == null) throw new CustomerNotFoundException();
+        return this.customers.get(customerId);
+    }
+
+    @Override
+    public List<Customer> findCustomersByName(String name) {
+        ArrayList<Customer> foundCustomers = new ArrayList<>();
+        for (Customer next : this.customers.values()) {
+            if (next.getCompanyName().equals(name)) foundCustomers.add(next);
+        }
+        return foundCustomers;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return new ArrayList<>(this.customers.values());
+    }
+
+    @Override
+    public Customer getFullCustomerDetail(String customerId) throws CustomerNotFoundException {
+        return this.findCustomerById(customerId);
+    }
+
+    @Override
+    public void recordCall(String customerId, Call callDetails) throws CustomerNotFoundException {
+        Customer customer = this.getFullCustomerDetail(customerId);
+        customer.addCall(callDetails);
+    }
+}
