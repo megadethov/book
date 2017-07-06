3e9948dbaf1bbe70f8f1ed567e410109dc50a139
Yuriy Yefremov <megadethov@gmail.com>
2017-07-06 15:34:32 +0300

    VP Spring Fundamental - p1402_SpringAOP AfterReturningAdvice

--- VrtualPair/Spring_Framework_Fundamentals/p1402_SpringAOP/src/main/java/ua/mega/avalon/advice/SimpleLoggingAdvice.java
+++ VrtualPair/Spring_Framework_Fundamentals/p1402_SpringAOP/src/main/java/ua/mega/avalon/advice/SimpleLoggingAdvice.java
@@ -4,10 +4,18 @@ import org.springframework.aop.AfterReturningAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
 public class SimpleLoggingAdvice implements AfterReturningAdvice {
     @Override
     public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+
+        // HACK returnValue
+        if (returnValue instanceof List) {
+            List hackReturningValue = (List) returnValue;
+            hackReturningValue.clear();
+        }
+
         System.out.println("METHOD is done - " + method.getName());
         System.out.println("Target method return: " + returnValue);
     }
