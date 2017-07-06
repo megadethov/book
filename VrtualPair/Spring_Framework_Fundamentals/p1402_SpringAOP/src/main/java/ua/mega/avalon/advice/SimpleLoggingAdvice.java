6aef69f6eb863e7c7b57df45ee07170c1fd54a93
Yuriy Yefremov <megadethov@gmail.com>
2017-07-06 13:01:00 +0300

    VP Spring Fundamental - p1402_SpringAOP - AfterAdvice

--- VrtualPair/Spring_Framework_Fundamentals/p1402_SpringAOP/src/main/java/ua/mega/avalon/advice/SimpleLoggingAdvice.java
+++ VrtualPair/Spring_Framework_Fundamentals/p1402_SpringAOP/src/main/java/ua/mega/avalon/advice/SimpleLoggingAdvice.java
@@ -1,12 +1,14 @@
 package ua.mega.avalon.advice;
 
+import org.springframework.aop.AfterReturningAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 
 import java.lang.reflect.Method;
 
-public class SimpleLoggingAdvice implements MethodBeforeAdvice {
+public class SimpleLoggingAdvice implements AfterReturningAdvice {
     @Override
-    public void before(Method method, Object[] args, Object target) throws Throwable {
-        System.out.println("NOW IN METHOD " + method.getName());
+    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+        System.out.println("METHOD is done - " + method.getName());
+        System.out.println("Target method return: " + returnValue);
     }
 }
