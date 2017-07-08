2e23818fdf4f5b8428e3cbdb76228a7b140e7f38
Yuriy Yefremov <megadethov@gmail.com>
2017-07-08 18:23:11 +0300

    VP Spring Fundamental - AOP Annotations config sw

---
+++ VrtualPair/Spring_Framework_Fundamentals/p1502_AspectJ_Pointcut_Syntax/src/main/java/ua/mega/avalon/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,32 @@
+package ua.mega.avalon.advice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class PerformanceTimingAdvice {
+
+    public Object performTimingMeasurement(ProceedingJoinPoint method) throws Throwable {
+
+        // before
+        long startTime = System.nanoTime();
+
+        try {
+            // proceed to Target
+            Object returnValue = method.proceed();
+            return returnValue;
+        } finally {
+            // after
+            long endTime = System.nanoTime();
+            long timer = (endTime - startTime);
+            System.out.println("The method " + method.getSignature().getName() + " took " + timer + " nanoseconds");
+        }
+    }
+
+    //    public void beforeAdviceLogging() {
+    public void beforeAdviceLogging(JoinPoint jp) {
+//        System.out.println("Start the method ...");
+        System.out.println("Start the method ..." + jp.getSignature().getName());
+    }
+}
