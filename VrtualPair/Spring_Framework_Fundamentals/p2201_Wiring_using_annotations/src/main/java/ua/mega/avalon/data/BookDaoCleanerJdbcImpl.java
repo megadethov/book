c50919e432e1ca5b29621bfbb7722aab3950bc64
Yuriy Yefremov <megadethov@gmail.com>
2017-07-12 11:50:32 +0300

    VP Spring Fundamental - p2201_Wiring_using_annotations - @Repository

--- VrtualPair/Spring_Framework_Fundamentals/p2201_Wiring_using_annotations/src/main/java/ua/mega/avalon/data/BookDaoCleanerJdbcImpl.java
+++ VrtualPair/Spring_Framework_Fundamentals/p2201_Wiring_using_annotations/src/main/java/ua/mega/avalon/data/BookDaoCleanerJdbcImpl.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ua.mega.avalon.domain.Book;
 
 import javax.annotation.PostConstruct;
@@ -13,7 +14,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Component("bookDao")
+@Repository("bookDao")
 public class BookDaoCleanerJdbcImpl implements BookDao {
 
     // SQL
