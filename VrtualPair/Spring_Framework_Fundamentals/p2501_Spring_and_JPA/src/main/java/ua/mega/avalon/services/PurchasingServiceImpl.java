ba6b45a2439c332eb745cdb6f84fc6a7ebd9f648
Yuriy Yefremov <megadethov@gmail.com>
2017-07-13 14:16:10 +0300

    VP Spring Fundamental - p2401_Spring_and_JPA sw

---
+++ VrtualPair/Spring_Framework_Fundamentals/p2501_Spring_and_JPA/src/main/java/ua/mega/avalon/services/PurchasingServiceImpl.java
@@ -0,0 +1,33 @@
+package ua.mega.avalon.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import ua.mega.avalon.data.BookNotFoundException;
+import ua.mega.avalon.domain.Book;
+
+@Transactional
+@Service("purchasingService")
+public class PurchasingServiceImpl implements PurchasingService {
+
+    @Autowired
+    private AccountsService accounts;
+    @Autowired
+    private BookService books;
+
+    @Transactional(rollbackFor = {CustomerCreditExcededException.class, BookNotFoundException.class}, timeout = 10, isolation = Isolation.SERIALIZABLE)
+    @Override
+    public void buyBook(String isbn) throws BookNotFoundException, CustomerCreditExcededException {
+        // find the correct book
+        Book requiredBook = books.getBookByIsbn(isbn);
+
+        // delete the book from stock
+        books.deleteFromStock(requiredBook);
+
+        // now raise the invoice
+        accounts.raiseInvoice(requiredBook);
+    }
+}
