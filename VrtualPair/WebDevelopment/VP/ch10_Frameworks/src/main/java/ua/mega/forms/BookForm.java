7b14d8392ff1b198bd56765feea4e9ffe1542a32
Yuriy Yefremov <megadethov@gmail.com>
2017-07-20 17:33:51 +0300

    VP WebDevelopment - ch10_Frameworks - add Bookform

---
+++ VrtualPair/WebDevelopment/VP/ch10_Frameworks/src/main/java/ua/mega/forms/BookForm.java
@@ -0,0 +1,43 @@
+package ua.mega.forms;
+
+import org.apache.struts.action.ActionForm;
+
+public class BookForm extends ActionForm {
+
+    private String isbn;
+    private String title;
+    private String author;
+    private String price;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
