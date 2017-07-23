3ed236578e50a070627035aff796d7cd9858e188
Yuriy Yefremov <megadethov@gmail.com>
2017-07-23 19:19:57 +0300

    VP SpringMVC - Session Handling - approach 3

--- VrtualPair/SpringMVC_and_WebFlow/p05_Parameters_and_Sessions_4/src/main/java/ua/mega/control/CartManagementController.java
+++ VrtualPair/SpringMVC_and_WebFlow/p05_Parameters_and_Sessions_4/src/main/java/ua/mega/control/CartManagementController.java
@@ -17,12 +17,13 @@ import ua.mega.domain.ShoppingCart;
 import ua.mega.services.BookService;
 
 @Controller
-@Scope("session")
+@Scope("request")
 public class CartManagementController {
     @Autowired
     @Qualifier("bookService")
     private BookService bookService;
-    private ShoppingCart cart = new ShoppingCart();
+    @Autowired
+    private ShoppingCart cart;
 
     @RequestMapping("/addToCart")
     public ModelAndView addToCart(@RequestParam("id") int id) {
