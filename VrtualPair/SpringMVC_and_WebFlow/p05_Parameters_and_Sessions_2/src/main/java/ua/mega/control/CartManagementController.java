ce2d449bddb64855174d051bd59ffa9a3c6c71e8
Yuriy Yefremov <megadethov@gmail.com>
2017-07-23 15:33:54 +0300

    VP SpringMVC - Session Handling - approach 1

--- VrtualPair/SpringMVC_and_WebFlow/p05_Parameters_and_Sessions_2/src/main/java/ua/mega/control/CartManagementController.java
+++ VrtualPair/SpringMVC_and_WebFlow/p05_Parameters_and_Sessions_2/src/main/java/ua/mega/control/CartManagementController.java
@@ -1,4 +1,3 @@
-/*
 package ua.mega.control;
 
 import java.util.List;
@@ -17,20 +16,32 @@ import ua.mega.domain.ShoppingCart;
 import ua.mega.services.BookService;
 
 @Controller
-public class CartManagementController
-{
-	@Autowired
-	private BookService bookService;
+public class CartManagementController {
+    @Autowired
+    private BookService bookService;
 
-	@RequestMapping("/addToCart")
-	public ModelAndView addToCart(@RequestParam("id") int id)
-	{
-		Book requiredBook = bookService.getBookById(id);
+    @RequestMapping("/addToCart")
+    public ModelAndView addToCart(@RequestParam("id") int id, HttpSession session) {
 
-		// BUT HOW DO WE ADD IT TO THE USER'S SESSION?
+        Book requiredBook = bookService.getBookById(id);
 
-		return new ModelAndView("/bookAddedToCart.jsp", "title", requiredBook.getTitle());
-	}
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+        if (cart == null) {
+            cart = new ShoppingCart();
+        }
+        cart.addItem(requiredBook);
+        session.setAttribute("cart", cart);
+
+        return new ModelAndView("/bookAddedToCart.jsp", "title", requiredBook.getTitle());
+    }
+
+    @RequestMapping("/viewCart")
+    public ModelAndView viewCart(HttpSession session) {
+
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+        List<Book> allItems = cart.getAllItems();
+
+        return new ModelAndView("/cartContents.jsp", "cart", allItems);
+    }
 
 }
-*/
