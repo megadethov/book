2f75673a5c2edea8afde5f71a046f98b45965357
Yuriy Yefremov <megadethov@gmail.com>
2017-07-27 14:09:40 +0300

    VP SpringMVC - p10_View_Resolvers sw

---
+++ VrtualPair/SpringMVC_and_WebFlow/p10_View_Resolvers/src/main/java/ua/mega/control/CartManagementController.java
@@ -0,0 +1,41 @@
+package ua.mega.control;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import ua.mega.domain.Book;
+import ua.mega.domain.ShoppingCart;
+import ua.mega.services.BookService;
+
+@Controller
+@Scope("request")
+public class CartManagementController {
+    @Autowired
+    @Qualifier("bookService")
+    private BookService bookService;
+    @Autowired
+    private ShoppingCart cart;
+
+    @RequestMapping("/addToCart")
+    public ModelAndView addToCart(@RequestParam("id") int id) {
+        Book requiredBook = bookService.getBookById(id);
+        cart.addItem(requiredBook);
+        return new ModelAndView("/bookAddedToCart.jsp", "title", requiredBook.getTitle());
+    }
+
+    @RequestMapping("/viewCart")
+    public ModelAndView viewCart() {
+        List<Book> allItems = cart.getAllItems();
+        return new ModelAndView("/cartContents.jsp", "cart", allItems);
+    }
+
+}
---
+++ VrtualPair/SpringMVC_and_WebFlow/p10_View_Resolvers/src/main/java/ua/mega/domain/Book.java
@@ -0,0 +1,80 @@
+package ua.mega.domain;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import ua.mega.validation.ISBN;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
+public class Book implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+    private static int nextId = 1;
+    private int id;
+    @NotEmpty
+    @ISBN
+    private String isbn;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    private String author;
+    @Min(value = 0)
+    @Digits(integer = 3, fraction = 2)
+    private double price;
+
+    public Book(String isbn, String title, String author, double price) {
+        this.id = nextId++;
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return this.title + " by " + this.author;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
