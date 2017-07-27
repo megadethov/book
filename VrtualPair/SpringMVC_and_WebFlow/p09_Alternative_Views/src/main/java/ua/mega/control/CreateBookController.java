04c7eb4a23a75e8422220ae65b19c7ee753911b7
Yuriy Yefremov <megadethov@gmail.com>
2017-07-27 09:30:29 +0300

    VP SpringMVC - p09_Alternative_Views ws

---
+++ VrtualPair/SpringMVC_and_WebFlow/p09_Alternative_Views/src/main/java/ua/mega/control/CreateBookController.java
@@ -0,0 +1,36 @@
+package ua.mega.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import ua.mega.domain.Book;
+import ua.mega.services.BookService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/addNewBook")
+public class CreateBookController {
+
+    @Autowired
+    BookService bookService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView show() {
+        return new ModelAndView("/add-new-book.jsp", "book", new Book());
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ModelAndView processForm(@Valid Book newBook, Errors result) {
+
+        if (result.hasErrors()) {
+            return new ModelAndView("/add-new-book.jsp", "book", newBook);
+        }
+        bookService.registerNewBook(newBook);
+        return new ModelAndView("/book-added.jsp", "title", newBook.getTitle());
+    }
+
+}
---
+++ VrtualPair/SpringMVC_and_WebFlow/p09_Alternative_Views/src/main/java/ua/mega/data/BookNotFoundException.java
@@ -0,0 +1,5 @@
+package ua.mega.data;
+
+public class BookNotFoundException extends Exception {
+	private static final long serialVersionUID = 4631445786801082925L;
+}
