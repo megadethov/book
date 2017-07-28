24676f350ba4b7048efd9cccaca8b47145c98369
Yuriy Yefremov <megadethov@gmail.com>
2017-07-28 15:53:12 +0300

    VP SpringMVC - p11_Ajax_and_JSON sw

---
+++ VrtualPair/SpringMVC_and_WebFlow/p11_Ajax_and_JSON/src/main/java/ua/mega/control/BookManagementController.java
@@ -0,0 +1,47 @@
+package ua.mega.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import ua.mega.domain.Book;
+import ua.mega.services.BookService;
+import ua.mega.views.BookReportExcel;
+import ua.mega.views.BookReportPdf;
+
+import java.util.List;
+
+@Controller
+public class BookManagementController {
+
+    @Autowired
+    @Qualifier("bookService")
+    private BookService bookService;
+
+    @RequestMapping("/viewAllBooks")
+    public ModelAndView viewAllBooks() {
+        List<Book> allBooks = bookService.getEntireCatalogue();
+        return new ModelAndView("displayAllBooks", "allBooks", allBooks);
+    }
+
+    @RequestMapping("/findByAuthor")
+    public ModelAndView findByAuthor(/*@RequestParam("author")*/ String author) {
+        List<Book> allBooks = bookService.getAllBooksByAuthor(author);
+        return new ModelAndView("displayAllBooks", "allBooks", allBooks);
+    }
+
+    @RequestMapping("/viewAllBooksPDF")
+    public ModelAndView viewAllBooksPDF()
+    {
+        List<Book> allBooks = bookService.getEntireCatalogue();
+        return new ModelAndView("BookReportPdf","allBooks",allBooks);
+    }
+
+    @RequestMapping("/viewAllBooksExcel")
+    public ModelAndView viewAllBooksExcel()
+    {
+        List<Book> allBooks = bookService.getEntireCatalogue();
+        return new ModelAndView("BookReportExcel","allBooks",allBooks);
+    }
+}
