97d0c4e9c16469a0405afa69e0672f1778612439
Yuriy Yefremov <megadethov@gmail.com>
2017-08-04 19:34:01 +0300

    VP-Spring_Remoting_and_Webservices - RMI on the Client

--- VrtualPair/Spring_Remoting_and_Webservices/p0301_RMI_on_the_Client/src/main/java/ua/mega/client/SimpleClientTest.java
+++ VrtualPair/Spring_Remoting_and_Webservices/p0301_RMI_on_the_Client/src/main/java/ua/mega/client/SimpleClientTest.java
@@ -37,35 +37,35 @@ public class SimpleClientTest {
 				System.out.println("Sorry, that customer doesn't exisit");
 			}
 			
-//			// begin
-//			customerService.newCustomer(new Customer("CS03939", "Acme", "Good Customer"));
-//			// commit
-//
-//			Call newCall = new Call("Larry Wall called from Acme Corp");
-//			Action action1 = new Action("Call back Larry to ask how things are going", new GregorianCalendar(2016, 0, 0), "rac");
-//			Action action2 = new Action("Check our sales dept to make sure Larry is being tracked", new GregorianCalendar(2016, 0, 0), "rac");		
-//
-//			List<Action> actions = new ArrayList<Action>();
-//			actions.add(action1);
-//			actions.add(action2);
-//
-//			// begin
-//			try
-//			{
-//				callService.recordCall("CS03939", newCall, actions);
-//			}
-//			catch (CustomerNotFoundException e)
-//			{
-//				System.out.println("That customer doesn't exist");
-//			}
-//			// commit
-//
-//			System.out.println("Here are the outstanding actions:");
-//			Collection<Action> incompleteActions = diaryService.getAllIncompleteActions("rac");
-//			for (Action next: incompleteActions)
-//			{
-//				System.out.println(next);
-//			}
+			// begin
+			customerService.newCustomer(new Customer("CS03939", "Acme", "Good Customer"));
+			// commit
+
+			Call newCall = new Call("Larry Wall called from Acme Corp");
+			Action action1 = new Action("Call back Larry to ask how things are going", new GregorianCalendar(2016, 0, 0), "rac");
+			Action action2 = new Action("Check our sales dept to make sure Larry is being tracked", new GregorianCalendar(2016, 0, 0), "rac");
+
+			List<Action> actions = new ArrayList<Action>();
+			actions.add(action1);
+			actions.add(action2);
+
+			// begin
+			try
+			{
+				callService.recordCall("CS03939", newCall, actions);
+			}
+			catch (CustomerNotFoundException e)
+			{
+				System.out.println("That customer doesn't exist");
+			}
+			// commit
+
+			System.out.println("Here are the outstanding actions:");
+			Collection<Action> incompleteActions = diaryService.getAllIncompleteActions("rac");
+			for (Action next: incompleteActions)
+			{
+				System.out.println(next);
+			}
 		}
 		finally
 		{
--- VrtualPair/Spring_Remoting_and_Webservices/p0301_RMI_on_the_Client/src/main/java/ua/mega/dataaccess/CustomerDaoJpaImpl.java
+++ VrtualPair/Spring_Remoting_and_Webservices/p0301_RMI_on_the_Client/src/main/java/ua/mega/dataaccess/CustomerDaoJpaImpl.java
@@ -11,70 +11,67 @@ import ua.mega.domain.Customer;
 
 public class CustomerDaoJpaImpl implements CustomerDao {
 
-	@PersistenceContext
-	private EntityManager em;
-	
-	@Override
-	public void create(Customer customer) {
-		em.persist(customer);
-	}
+    @PersistenceContext
+    private EntityManager em;
 
-	@Override
-	public Customer getById(String customerId) throws RecordNotFoundException {
-		try
-		{
-			return (Customer)em.createQuery("select customer from Customer as customer where customer.customerId=:customerId")
-				.setParameter("customerId", customerId)
-				.getSingleResult();
-		}
-		catch (NoResultException e)
-		{
-			throw new RecordNotFoundException();
-		}
-	}
+    @Override
+    public void create(Customer customer) {
+        em.persist(customer);
+    }
 
-	@Override
-	public List<Customer> getByName(String name) {
-		return em.createQuery("select customer from Customer where customer.companyName=:companyName")
-				 .setParameter("companyName", name)
-				 .getResultList();
-	}
+    @Override
+    public Customer getById(String customerId) throws RecordNotFoundException {
+        try {
+            return (Customer) em.createQuery("select customer from Customer as customer where customer.customerId=:customerId")
+                    .setParameter("customerId", customerId)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            throw new RecordNotFoundException();
+        }
+    }
 
-	@Override
-	public void update(Customer customerToUpdate)
-			throws RecordNotFoundException 
-	{
-		em.merge(customerToUpdate);
-	}
+    @Override
+    public List<Customer> getByName(String name) {
+        return em.createQuery("select customer from Customer as customer where customer.companyName=:companyName")
+                .setParameter("companyName", name)
+                .getResultList();
+    }
 
-	@Override
-	public void delete(Customer oldCustomer) throws RecordNotFoundException 
-	{
-		oldCustomer = em.merge(oldCustomer);
-		em.remove(oldCustomer);
-	}
+    @Override
+    public void update(Customer customerToUpdate)
+            throws RecordNotFoundException {
+        em.merge(customerToUpdate);
+    }
 
-	@Override
-	public List<Customer> getAllCustomers() 
-	{
-		return em.createQuery("select customer from Customer as customer").getResultList();
-	}
+    @Override
+    public void delete(Customer oldCustomer) throws RecordNotFoundException {
+        oldCustomer = em.merge(oldCustomer);
+        em.remove(oldCustomer);
+    }
 
-	@Override
-	public Customer getFullCustomerDetail(String customerId)
-			throws RecordNotFoundException 
-	{
-		return (Customer)em.createQuery("select customer from Customer as customer left join fetch customer.calls where customer.customerId=:customerId")
-				.setParameter("customerId", customerId)
-				.getSingleResult();
-	}
+    @Override
+    public List<Customer> getAllCustomers() {
+        return em.createQuery("select customer from Customer as customer").getResultList();
+    }
 
-	@Override
-	public void addCall(Call newCall, String customerId)
-			throws RecordNotFoundException 
-	{
-		Customer customer = em.find(Customer.class, customerId);
-		customer.addCall(newCall);
-	}
+    @Override
+    public Customer getFullCustomerDetail(String customerId)
+            throws RecordNotFoundException {
+        try {
+            return (Customer) em.createQuery("select customer from Customer as customer left join fetch customer.calls where customer.customerId=:customerId")
+                    .setParameter("customerId", customerId)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            throw new RecordNotFoundException();
+        }
+
+    }
+
+    @Override
+    public void addCall(Call newCall, String customerId)
+            throws RecordNotFoundException {
+        Customer customer = em.find(Customer.class, customerId);
+        customer.addCall(newCall);
+    }
 
 }
