075249657ff770ea0f94b62f2f8758be6b48d817
Yuriy Yefremov <megadethov@gmail.com>
2017-08-05 07:46:40 +0300

    VP-Spring_Remoting_and_Webservices - p0401_HttpInvoker sw

---
+++ VrtualPair/Spring_Remoting_and_Webservices/p0401_HttpInvoker/src/main/java/ua/mega/server/RmiServer.java
@@ -0,0 +1,22 @@
+package ua.mega.server;
+
+import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class RmiServer {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("remoting-server.xml");
+
+        System.out.println("Server Running! Please press enter to stop!");
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.nextLine();
+
+        container.close();
+        sc.close();
+    }
+
+}
