817960d604ed0b1caf2bb35089dd13ea17a19922
Yuriy Yefremov <megadethov@gmail.com>
2017-08-08 12:03:42 +0300

    VP-RMI WS - p0801_Spring_SOAP_Clients_SERVER

---
+++ VrtualPair/Spring_Remoting_and_Webservices/p0801_Spring_SOAP_Clients_SERVER/src/main/java/ua/mega/webservices/CustomerEndpoint.java
@@ -0,0 +1,61 @@
+package ua.mega.webservices;
+
+import ua.mega.domain.Call;
+import ua.mega.domain.Customer;
+import ua.mega.services.customers.CustomerManagementService;
+import ua.mega.services.customers.CustomerNotFoundException;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.util.List;
+
+@WebService(serviceName = "customerService")
+public class CustomerEndpoint implements CustomerManagementService {
+
+    CustomerManagementService service;
+
+    @WebMethod(exclude = true)
+    public void setService(CustomerManagementService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void newCustomer(Customer newCustomer) {
+        service.newCustomer(newCustomer);
+    }
+
+    @Override
+    public void updateCustomer(Customer changedCustomer) throws CustomerNotFoundException {
+        service.updateCustomer(changedCustomer);
+    }
+
+    @Override
+    public void deleteCustomer(Customer oldCustomer) throws CustomerNotFoundException {
+        deleteCustomer(oldCustomer);
+    }
+
+    @Override
+    public Customer findCustomerById(String customerId) throws CustomerNotFoundException {
+        return service.findCustomerById(customerId);
+    }
+
+    @Override
+    public List<Customer> findCustomersByName(String name) {
+        return service.findCustomersByName(name);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return service.getAllCustomers();
+    }
+
+    @Override
+    public Customer getFullCustomerDetail(String customerId) throws CustomerNotFoundException {
+        return service.getFullCustomerDetail(customerId);
+    }
+
+    @Override
+    public void recordCall(String customerId, Call callDetails) throws CustomerNotFoundException {
+        service.recordCall(customerId, callDetails);
+    }
+}
