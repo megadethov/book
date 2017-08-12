124e0da365b8bce75520ef3f63ca7ec9dc34cbab
Yuriy Yefremov <megadethov@gmail.com>
2017-08-12 07:51:39 +0300

    VP-RMI WS - p1102_XPath_CLIENT sw

---
+++ VrtualPair/Spring_Remoting_and_Webservices/p1101_XPath/src/main/java/ua/mega/webservices/CustomerEndpoint.java
@@ -0,0 +1,48 @@
+package ua.mega.webservices;
+
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import ua.mega.domain.Customer;
+import ua.mega.services.customers.CustomerManagementService;
+import ua.mega.services.customers.CustomerNotFoundException;
+
+import javax.annotation.Resource;
+
+@Endpoint
+public class CustomerEndpoint {
+
+    public static final String NAMESPACE = "http://www.mega.ua/crm";
+
+    @Resource(name="customerService")
+    CustomerManagementService service;
+
+    @PayloadRoot(namespace= NAMESPACE, localPart="getCustomerByIdRequest")
+    @ResponsePayload
+    public Element fetchTheCustomerDetailsJDomVersion(@RequestPayload Element incoming) throws CustomerNotFoundException {
+        Element idElement = incoming.getChild("id");
+        String id = idElement.getText();
+
+        Customer found = service.findCustomerById(id);
+
+        Element outgoing = new Element("getCustomerByIdResponse");
+        outgoing.addNamespaceDeclaration(Namespace.getNamespace("tns", NAMESPACE));
+
+        Element customer = new Element("customer");
+
+        customer.addContent(new Element("customerId").setText(found.getCustomerId()));
+        customer.addContent(new Element("companyName").setText(found.getCompanyName()));
+        customer.addContent(new Element("email").setText(found.getEmail()));
+        if (found.getNotes() != null) customer.addContent(new Element("notes").setText(found.getNotes()));
+        if (found.getTelephone() != null) customer.addContent(new Element("phone").setText(found.getTelephone()));
+
+        outgoing.addContent(customer);
+
+        return outgoing;
+    }
+}
