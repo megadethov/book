0d7181dee9ad7629d52f97e53b8938f5b5d8db29
Yuriy Yefremov <megadethov@gmail.com>
2017-08-15 21:30:39 +0300

    VP-RMI WS - RestTemplate

--- VrtualPair/Spring_Remoting_and_Webservices/p1702_REST_CLIENT/src/main/java/RestClient.java
+++ VrtualPair/Spring_Remoting_and_Webservices/p1702_REST_CLIENT/src/main/java/RestClient.java
@@ -1,16 +1,27 @@
+import org.springframework.web.client.RestTemplate;
+
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class RestClient {
     public static void main(String[] args) throws IOException {
-        URL url = new URL("http://localhost:8080/mywebapp/customer/102");
+
+        //IO approach
+/*        URL url = new URL("http://localhost:8080/mywebapp/customer/102");
         InputStream is = url.openStream();
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
-
         System.out.println(br.readLine());
+        br.close();*/
 
-        br.close();
+        RestTemplate template = new RestTemplate();
+
+        String response = template.getForObject("http://localhost:8080/mywebapp/customer/102", String.class);
+        System.out.println(response);
+
+        Customer customer = template.getForObject("http://localhost:8080/mywebapp/customer/102", Customer.class);
+        System.out.println(customer.getCustomerId());
+
     }
 }
