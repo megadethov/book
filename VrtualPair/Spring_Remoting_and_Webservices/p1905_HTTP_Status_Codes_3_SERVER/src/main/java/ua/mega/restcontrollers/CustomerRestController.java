37f56bfac97a851bea86e7ad793c98021dd1547a
Yuriy Yefremov <megadethov@gmail.com>
2017-08-17 21:31:15 +0300

    VP-RMI WS - p1901_HTTP_Status_Codes_3

--- VrtualPair/Spring_Remoting_and_Webservices/p1905_HTTP_Status_Codes_3_SERVER/src/main/java/ua/mega/restcontrollers/CustomerRestController.java
+++ VrtualPair/Spring_Remoting_and_Webservices/p1905_HTTP_Status_Codes_3_SERVER/src/main/java/ua/mega/restcontrollers/CustomerRestController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ua.mega.domain.Customer;
 import ua.mega.services.customers.CustomerManagementService;
 import ua.mega.services.customers.CustomerNotFoundException;
@@ -16,6 +14,12 @@ public class CustomerRestController {
 
     @Autowired
     private CustomerManagementService customerService;
+
+    @ResponseStatus(value= HttpStatus.NOT_FOUND, reason="The customer wasn't found")
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public void rulesForCustomerNotFound() {
+
+    }
 
     @RequestMapping(value = "/customer/{id}")
     public Customer findCustomerById(@PathVariable String id) throws CustomerNotFoundException {
--- VrtualPair/Spring_Remoting_and_Webservices/p1905_HTTP_Status_Codes_3_SERVER/src/main/java/ua/mega/services/customers/CustomerNotFoundException.java
+++ VrtualPair/Spring_Remoting_and_Webservices/p1905_HTTP_Status_Codes_3_SERVER/src/main/java/ua/mega/services/customers/CustomerNotFoundException.java
@@ -3,7 +3,7 @@ package ua.mega.services.customers;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="The customer wasn't found")
+
 public class CustomerNotFoundException extends Exception {
     // I've added this just to stop the annoying warning in Eclipse.
     private static final long serialVersionUID = 1L;
