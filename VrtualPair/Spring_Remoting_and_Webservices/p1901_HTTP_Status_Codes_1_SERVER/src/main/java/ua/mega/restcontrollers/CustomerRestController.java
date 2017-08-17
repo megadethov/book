80a506abdb23067a80396cdf336f72879a6bd24d
Yuriy Yefremov <megadethov@gmail.com>
2017-08-17 19:15:24 +0300

    VP-RMI WS - p1901_HTTP_Status_Codes_1 sw

---
+++ VrtualPair/Spring_Remoting_and_Webservices/p1901_HTTP_Status_Codes_1_SERVER/src/main/java/ua/mega/restcontrollers/CustomerRestController.java
@@ -0,0 +1,27 @@
+package ua.mega.restcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.mega.domain.Customer;
+import ua.mega.services.customers.CustomerManagementService;
+import ua.mega.services.customers.CustomerNotFoundException;
+
+@RestController
+public class CustomerRestController {
+
+    @Autowired
+    private CustomerManagementService customerService;
+
+    @RequestMapping(value="/customer/{id}")
+    public Customer findCustomerById(@PathVariable String id) {
+        try {
+            return customerService.getFullCustomerDetail(id);
+        } catch (CustomerNotFoundException e) {
+            throw new RuntimeException();
+        }
+    }
+
+}
