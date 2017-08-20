ac6d37808b95f846d94276a64b5f929c312aa665
Yuriy Yefremov <megadethov@gmail.com>
2017-08-20 08:29:46 +0300

    VP-RMI WS - p2104_Ranges_CLIENT sw

---
+++ VrtualPair/Spring_Remoting_and_Webservices/p2103_Ranges_SERVER/src/main/java/ua/mega/restcontrollers/CustomerRestController.java
@@ -0,0 +1,44 @@
+package ua.mega.restcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ua.mega.domain.Customer;
+import ua.mega.services.customers.CustomerManagementService;
+import ua.mega.services.customers.CustomerNotFoundException;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@RestController
+public class CustomerRestController {
+
+    @Autowired
+    private CustomerManagementService customerService;
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ClientErrorInformation> rulesForCustomerNotFound() {
+        ClientErrorInformation error = new ClientErrorInformation("The customer wasn't found");
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value = "/customer/{id}")
+    public Customer findCustomerById(@PathVariable String id) throws CustomerNotFoundException {
+
+        Customer foundCustomer = customerService.getFullCustomerDetail(id);
+        return customerService.getFullCustomerDetail(id);
+
+    }
+
+    @RequestMapping(value = "/customers")
+    public CustomerCollectionRepresentation returnAllCustomers() {
+        List<Customer> allCustomers = customerService.getAllCustomers();
+        for (Customer next : allCustomers) {
+            next.setCalls(null);
+        }
+        return new CustomerCollectionRepresentation(allCustomers);
+    }
+
+}
---
+++ VrtualPair/Spring_Remoting_and_Webservices/p2104_Ranges_CLIENT/src/main/java/RestClient.java
@@ -0,0 +1,34 @@
+import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestClient {
+    public static void main(String[] args) throws IOException {
+
+        RestTemplate template = new RestTemplate();
+        template.setErrorHandler(new CustomExceptionHandler(template));
+
+        HttpHeaders headers = new HttpHeaders();
+        List<MediaType> acceptableMediaTypes = new ArrayList<MediaType>();
+        acceptableMediaTypes.add(MediaType.IMAGE_JPEG);
+        acceptableMediaTypes.add(MediaType.APPLICATION_XML);
+//        acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
+        headers.setAccept(acceptableMediaTypes);
+
+        HttpEntity requestEntity = new HttpEntity(headers);
+
+        try {
+            HttpEntity response = template.exchange("http://localhost:8080/mywebapp/customer/122132341", HttpMethod.GET, requestEntity, String.class);
+            System.out.println(response.getBody());
+        } catch (ResourceNotFoundException e) { // definitely a 404
+            System.out.println("Sorry, the customer was not found...");
+            System.out.println("The message returned back was " + e.getErrorObject().getMessage());
+        }
+    }
+}
