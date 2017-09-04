15e5b6c0c03358b5894d083b9cd7152ba79950cd
Yuriy Yefremov <megadethov@gmail.com>
2017-09-04 08:29:41 +0300

    LavrJS2015 L01 - p03_dom

---
+++ JS/Lavr_JS_2015/Lesson_01/src/main/webapp/p03_dom/script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+	
+	alert(document.body.innerHTML);
+	//document.body.innerHTML = '<h1>Ура</h1>';
+	
+	//document.body.children[0].innerHTML = 'a';
+	
+	// var div1 = document.body.children[0];
+	// alert(div1.children.length);
+	
+	//var div2 = document.body.children[1];
+	// div2.children[0].innerHTML = 'w';
+	
+	//div2.parentNode.innerHTML = 'q';
+}
