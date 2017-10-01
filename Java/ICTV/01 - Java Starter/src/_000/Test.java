06f309c9202b1c8e963b72b351acf7c0fdb97c30
Yuriy Yefremov <megadethov@gmail.com>
2017-10-01 12:17:45 +0300

    ICTV 01 - Java Starter - 003

---
+++ Java/ICTV/01 - Java Starter/src/_000/Test.java
@@ -0,0 +1,11 @@
+package _000;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        char[] arr = new char[5];
+        Arrays.fill(arr, 'A');
+        System.out.println(Arrays.toString(arr));
+    }
+}
