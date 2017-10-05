88b0b43fb859682b9897d17bf363342be273cea1
Yuriy Yefremov <megadethov@gmail.com>
2017-10-05 13:07:12 +0300

    _06_Atomic

---
+++ Java/TCACH/_01-Concurrency/src/main/java/ua/mega/concurrency/_06_Atomic/Account.java
@@ -0,0 +1,35 @@
+package ua.mega.concurrency._06_Atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Account {
+    private int balance;
+    private final Lock lock = new ReentrantLock();
+    private AtomicInteger failCounter = new AtomicInteger(0); // атомик инкремент
+
+    public Account(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public Lock getLock() {
+        return lock;
+    }
+
+    public AtomicInteger getFailCounter() {
+        return failCounter;
+    }
+}
