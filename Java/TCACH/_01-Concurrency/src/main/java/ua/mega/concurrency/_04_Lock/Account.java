aa3a2184ae388bccceed8117a769b750317c251b
Yuriy Yefremov <megadethov@gmail.com>
2017-10-05 12:25:27 +0300

    _04_Lock

---
+++ Java/TCACH/_01-Concurrency/src/main/java/ua/mega/concurrency/_04_Lock/Account.java
@@ -0,0 +1,29 @@
+package ua.mega.concurrency._04_Lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Account {
+    private int balance;
+    private final Lock lock = new ReentrantLock();
+
+    public Account(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public Lock getLock() {
+        return lock;
+    }
+}
---
+++ Java/TCACH/_01-Concurrency/src/main/java/ua/mega/concurrency/_04_Lock/Operations.java
@@ -0,0 +1,47 @@
+package ua.mega.concurrency._04_Lock;
+
+import javax.naming.InsufficientResourcesException;
+import java.util.concurrent.TimeUnit;
+
+public class Operations {
+    public static void main(String[] args) throws InsufficientResourcesException {
+        final Account a = new Account(1000);
+        final Account b = new Account(2000);
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    transfer(a, b, 500);
+                } catch (InsufficientResourcesException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        transfer(b, a, 300);
+    }
+
+    static void transfer(Account acc1, Account acc2, int amount) throws InsufficientResourcesException {
+        if (acc1.getBalance() < amount) {
+            throw new InsufficientResourcesException();
+        }
+        // пытаемся захватить 1-й ресурс в течении 1 сек
+        if (acc1.getLock().tryLock(1, TimeUnit.SECONDS)) { // требует обработки InterruptedException
+            try {
+                // пытаемся захватить 2-й ресурс в течении 1 сек
+                if (acc2.getLock().tryLock(1, TimeUnit.SECONDS)) { // требует обработки InterruptedException
+                    try {
+                        acc1.withdraw(amount);
+                        acc2.deposit(amount);
+                    } finally {
+                        acc2.getLock().unlock();
+                    }
+                }
+            } finally {
+                acc1.getLock().unlock();
+            }
+        } else {
+            System.out.println("Sorry - не удалось выполнить трансфер");
+        }
+    }
+}
