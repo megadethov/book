4c7cdb9f45561a1072aace95b9f0156d86048fc5
Yuriy Yefremov <megadethov@gmail.com>
2017-10-06 16:24:21 +0300

    TCACH Generics - _05_Generic_inheritance

---
+++ Java/TCACH/_03_Generics/src/main/java/_05_Generic_inheritance.java
@@ -0,0 +1,42 @@
+public class _05_Generic_inheritance {
+    public static void main(String[] args) {
+
+        // В такой контейнер можно пихать любой Product
+        Container<Product> c1 = new Container<>(); // Мы можем создать контейнер для любого типа. Например - Container<String>
+        c1.setItem(new Product());
+        c1.setItem(new Camera());
+        c1.setItem(new Phone());
+
+        c1.getItem();
+
+        // Если же мы хотим, чтобы в контейнере хранились только Product и наследники (String контейнер нельзя создать) - нужен BOUNDED
+        // Создадим другой контейнер только для Product и его наследников
+    }
+}
+
+class Product {
+    private String name;
+    private int price;
+}
+
+class Camera extends Product {
+    private int pixel;
+}
+
+class Phone extends Product {
+    private String model;
+}
+
+
+class Container<T> {
+    private T item;
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+}
+
