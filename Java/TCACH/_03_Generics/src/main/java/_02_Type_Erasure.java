9e8a7c975635aa0815d7c97fc2f6e39e9632ae45
Yuriy Yefremov <megadethov@gmail.com>
2017-10-06 14:27:35 +0300

    TCACH Collections - _02_Type_Erasure

---
+++ Java/TCACH/_03_Generics/src/main/java/_02_Type_Erasure.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class _02_Type_Erasure {
+    public static void main(String[] args) {
+        // Какой из методов будет вызван?
+        SomeType someType = new SomeType();
+        List<String> list = Arrays.asList("value");
+//        someType.test(list); // ClassCastException - тк сырой тип и вызывается наиболее подходящий метод - List
+
+        // Решение - генерализируй хоть чем нибудь - <?>, <Object>
+        SomeType<?> someType2 = new SomeType<>();
+        someType2.test(list); // value - теперрь вызовется метод - Collection<E>
+    }
+    // Если ничего не указать - компилятор будеть игнорить все дженерики.
+    // Не юзай сырой тип - указывай хоть что-нибудь.
+}
+
+class SomeType<T> {
+    public <E> void test(Collection<E> collection) {
+        for (E e : collection) {
+            System.out.println(e);
+        }
+    }
+
+    public void test(List<Integer> integerList) {
+        for (Integer i : integerList) {
+            System.out.println(i);
+        }
+    }
+}
