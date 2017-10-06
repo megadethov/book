328616d0af8dd301c260a1443d56acfb5d509920
Yuriy Yefremov <megadethov@gmail.com>
2017-10-06 15:42:58 +0300

    TCACH Generics - _03_Inheritance_and_Generics

---
+++ Java/TCACH/_03_Generics/src/main/java/_03_Inheritance_and_Generics.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _03_Inheritance_and_Generics {
+    public static void main(String[] args) {
+        List<String> lstr = new ArrayList<>();
+//        List<Object> lobj = lstr; // compile error
+
+        // String является Object
+        // List<String> не является List<Object>
+        // List<String>, List<Object> являются Object
+
+        // По одному типу наследование допускается:
+        // ArrayList<String> является List<String> является Collection<String>
+
+    }
+}
