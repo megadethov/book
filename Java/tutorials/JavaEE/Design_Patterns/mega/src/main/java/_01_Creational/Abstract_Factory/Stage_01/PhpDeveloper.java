f0c871e189589302a8e7c053c3df92f33674de16
Yuriy Yefremov <megadethov@gmail.com>
2017-10-07 17:18:19 +0300

    Design Patterns - Abstract_Factory - Stage_01

---
+++ Java/tutorials/JavaEE/Design_Patterns/mega/src/main/java/_01_Creational/Abstract_Factory/Stage_01/PhpDeveloper.java
@@ -0,0 +1,7 @@
+package _01_Creational.Abstract_Factory.Stage_01;
+
+public class PhpDeveloper {
+    public void writeCode() {
+        System.out.println("PHP developer write PHP code...");
+    }
+}
---
+++ Java/tutorials/JavaEE/Design_Patterns/mega/src/main/java/_01_Creational/Abstract_Factory/Stage_01/ProjectManager.java
@@ -0,0 +1,7 @@
+package _01_Creational.Abstract_Factory.Stage_01;
+
+public class ProjectManager {
+    public void manageProject() {
+        System.out.println("PM manages website project...");
+    }
+}
