a7218a22168ffb6a9d47abd8832e8da0a0dc19b3
Yuriy Yefremov <megadethov@gmail.com>
2017-12-02 21:00:18 +0200

    GWT - p08_Widgets_Event_Handlers - add button handlers

--- Java/tutorials/JavaEE/GWT/[YT-EN] -  GWT/p08_Widgets_Event_Handlers/src/ua/mega/home/client/Home.java
+++ Java/tutorials/JavaEE/GWT/[YT-EN] -  GWT/p08_Widgets_Event_Handlers/src/ua/mega/home/client/Home.java
@@ -1,6 +1,9 @@
 package ua.mega.home.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.DOM;
@@ -10,9 +13,41 @@ import com.google.gwt.event.dom.client.ClickEvent;
 public class Home implements EntryPoint {
 
     private Button btn;
+    private Button btn2;
 
     public void onModuleLoad() {
+//        btn = new Button("Click me!");
         btn = new Button("Click me!");
+        btn2 = new Button("=')");
+
+/*        btn.addMouseOverHandler(new MouseOverHandler() {
+            @Override
+            public void onMouseOver(MouseOverEvent event) {
+                Window.alert("Mouse Over Event");
+            }
+        });*/
+
+/*        btn.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                Window.alert("Button click Event");
+            }
+        });*/
+
+        btn.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                btn2.setText("='(");
+            }
+        });
+        btn2.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                btn2.setText("=')");
+            }
+        });
+
         RootPanel.get().add(btn);
+        RootPanel.get().add(btn2);
     }
 }
