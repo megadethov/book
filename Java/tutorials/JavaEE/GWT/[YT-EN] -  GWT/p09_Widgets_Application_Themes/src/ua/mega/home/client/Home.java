2c892083aa2414a79f15b2aec1fcd5e3b6a0d6e2
Yuriy Yefremov <megadethov@gmail.com>
2017-12-02 22:40:41 +0200

    GWT - p09_Widgets_Application_Themes - add sub menu

--- Java/tutorials/JavaEE/GWT/[YT-EN] -  GWT/p09_Widgets_Application_Themes/src/ua/mega/home/client/Home.java
+++ Java/tutorials/JavaEE/GWT/[YT-EN] -  GWT/p09_Widgets_Application_Themes/src/ua/mega/home/client/Home.java
@@ -6,7 +6,6 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class Home implements EntryPoint {
 
@@ -14,15 +13,22 @@ public class Home implements EntryPoint {
 
     public void onModuleLoad() {
 
-//        menu = new MenuBar(); // horizontal menu
-        menu = new MenuBar(true); // vertical menu
+        menu = new MenuBar(); // horizontal menu
+//        menu = new MenuBar(true); // vertical menu
         Command cmd = new Command() {
             @Override
             public void execute() {
                 Window.alert("Clicked");
             }
         };
-        menu.addItem(new MenuItem("File", cmd));
+
+        // add sub menu to the item - File
+        MenuBar menuFile = new MenuBar(true);
+        menuFile.addItem(new MenuItem("New", cmd));
+        menuFile.addItem(new MenuItem("Open", cmd));
+        menuFile.addItem(new MenuItem("Close", cmd));
+
+        menu.addItem(new MenuItem("File", menuFile));
         menu.addItem(new MenuItem("Edit", cmd));
         menu.addItem(new MenuItem("Refactor", cmd));
 
