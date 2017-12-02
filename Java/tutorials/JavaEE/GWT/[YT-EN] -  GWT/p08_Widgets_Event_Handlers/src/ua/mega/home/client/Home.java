f727384496431a5dfcddc803f6907b753bb0de55
Yuriy Yefremov <megadethov@gmail.com>
2017-12-02 19:39:15 +0200

    GWT - p08_Widgets_Event_Handlers - start ws

---
+++ Java/tutorials/JavaEE/GWT/[YT-EN] -  GWT/p08_Widgets_Event_Handlers/src/ua/mega/home/client/Home.java
@@ -0,0 +1,15 @@
+package ua.mega.home.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+
+public class Home implements EntryPoint {
+
+    public void onModuleLoad() {
+
+    }
+}
