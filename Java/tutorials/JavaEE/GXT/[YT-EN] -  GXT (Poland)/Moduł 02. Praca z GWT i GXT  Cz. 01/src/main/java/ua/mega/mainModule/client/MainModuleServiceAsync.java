b50e3a7f84962c4dac7f49b353fc0f22adaf2e7b
Yuriy Yefremov <megadethov@gmail.com>
2017-12-09 13:40:56 +0200

    tech commit

--- Java/tutorials/JavaEE/GXT/[YT-EN] -  GXT (Poland)/Moduł 02. Praca z GWT i GXT  Cz. 01/src/main/java/ua/mega/mainModule/client/MainModuleServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package ua.mega.mainModule.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface MainModuleServiceAsync {
-    void getMessage(String msg, AsyncCallback<String> async);
-}
