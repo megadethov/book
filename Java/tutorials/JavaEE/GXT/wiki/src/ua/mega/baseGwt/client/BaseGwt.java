b8db63f8c820bf629c31fded722260db350e087e
Yuriy Yefremov <megadethov@gmail.com>
2017-12-09 15:30:36 +0200

    GXT-wiki - Basic GXT project

--- Java/tutorials/JavaEE/GXT/wiki/src/ua/mega/baseGwt/client/BaseGwt.java
+++ Java/tutorials/JavaEE/GXT/wiki/src/ua/mega/baseGwt/client/BaseGwt.java
@@ -1,11 +1,12 @@
 package ua.mega.baseGwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.widget.core.client.box.AlertMessageBox;
+import com.sencha.gxt.widget.core.client.box.MessageBox;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>
@@ -16,41 +17,16 @@ public class BaseGwt implements EntryPoint {
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        final Button button = new Button("Click me");
-        final Label label = new Label();
+        TextButton b = new TextButton("Нажми меня...");
+        b.addSelectHandler(new SelectHandler() {
 
-        button.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                if (label.getText().equals("")) {
-                    BaseGwtService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
-                } else {
-                    label.setText("");
-                }
+            @Override
+            public void onSelect(SelectEvent event) {
+                AlertMessageBox d = new AlertMessageBox("Нажата кнопка", "Вы нажали кнопку");
+                d.setIcon(MessageBox.ICONS.info());
+                d.show();
             }
         });
-
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot1").add(button);
-        RootPanel.get("slot2").add(label);
-    }
-
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
-
-        public MyAsyncCallback(Label label) {
-            this.label = label;
-        }
-
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
-
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
+        RootPanel.get().add(b);
     }
 }
--- Java/tutorials/JavaEE/GXT/wiki/src/ua/mega/baseGwt/client/BaseGwtServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package ua.mega.baseGwt.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface BaseGwtServiceAsync {
-    void getMessage(String msg, AsyncCallback<String> async);
-}
