7e0b084aabcb53fae6939acc65dfbfc6b5fda72e
Yuriy Yefremov <megadethov@gmail.com>
2018-09-25 21:24:49 +0300

    PosgreSQL - DISTINCT

---
+++ Java/tutorials/JavaEE/SQL/[Udemy] - PostgreSQL - Advanced SQL Queries and Data Analysis/03 Some PostgreSQL Operators/027 EXCEPT Operator.sql
@@ -0,0 +1,20 @@
+﻿-- выбрать все film_id из film = 1000
+SELECT film_id, title 
+FROM film 
+ORDER BY title;
+
+-- выбрать все film_id из inventory приджойнить из film, искл повторы = 958
+SELECT DISTINCT inventory.film_id, title 
+FROM inventory
+INNER JOIN film ON film.film_id = inventory.film_id
+ORDER BY title;
+
+-- выбрать все film_id из film (исключенные) = 42
+SELECT film_id, title 
+FROM film 
+EXCEPT
+ SELECT
+ DISTINCT inventory.film_id, title
+ FROM inventory
+ INNER JOIN film ON film.film_id = inventory.film_id
+ORDER BY title;
