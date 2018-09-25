7c828b964836f6a3ee763e4a25357d217a739752
Yuriy Yefremov <megadethov@gmail.com>
2018-09-25 22:27:44 +0300

    PosgreSQL - IN

---
+++ Java/tutorials/JavaEE/SQL/[Udemy] - PostgreSQL - Advanced SQL Queries and Data Analysis/03 Some PostgreSQL Operators/028 IN Operator.sql
@@ -0,0 +1,10 @@
+﻿-- фильтрация по множественному значению
+SELECT customer_id, rental_id, return_date
+FROM rental
+WHERE customer_id IN (1, 2)
+ORDER BY return_date ASC;
+
+SELECT customer_id, rental_id, return_date
+FROM rental
+WHERE rental_id IN (320, 76, 2128, 5636)
+ORDER BY return_date DESC;
